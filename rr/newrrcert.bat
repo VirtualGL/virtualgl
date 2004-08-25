3ccabfa2b3c15a2335a2c29fc3b8f21df9c61911
DRC <dcommander@users.sourceforge.net>
2004-08-25 04:17:42 +0000

    Pause at the end
    git-svn-id: svn+ssh://svn.code.sf.net/p/virtualgl/code/trunk@19 799e4f7b-5fd2-41f6-823c-2ecc41bc7f0b

--- rr/newrrcert.bat
+++ rr/newrrcert.bat
@@ -3,3 +3,4 @@ set CERTFILE="%USERPROFILE%\rrcert.pem"
 if /i "%1"=="root" set CERTFILE=rrcert.pem
 openssl req -new -x509 -days 365 -nodes -config rrcert.cnf -out %CERTFILE% -keyout %CERTFILE%
 openssl x509 -subject -dates -fingerprint -noout -in %CERTFILE%
+pause
