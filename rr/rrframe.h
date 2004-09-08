9790bd7ed9a4bca05140dae5570069ba8f9c13ea
DRC <dcommander@users.sourceforge.net>
2004-09-08 01:11:02 +0000

    New, improved rrframe classes
    git-svn-id: svn+ssh://svn.code.sf.net/p/virtualgl/code/trunk@47 799e4f7b-5fd2-41f6-823c-2ecc41bc7f0b

--- rr/rrframe.h
+++ rr/rrframe.h
@@ -14,17 +14,25 @@
 #ifndef __RRFRAME_H
 #define __RRFRAME_H
 
-#include "rrcommon.h"
 #include "rr.h"
 #ifdef _WIN32
 #define XDK
 #endif
 #include "fbx.h"
 #include "hpjpeg.h"
+#include "rrmutex.h"
+#include "rrlog.h"
+#include "rrutil.h"
 #include <string.h>
-#include <pthread.h>
 
 static int jpegsub[RR_SUBSAMP]={HPJ_444, HPJ_422, HPJ_411};
+
+// Bitmap flags
+#define RRBMP_BOTTOMUP 1  // Bottom-up bitmap (as opposed to top-down)
+#define RRBMP_BGR      2  // BGR or BGRA pixel order
+#define RRBMP_EOLPAD   4  // Each input scanline is padded to the next 32-bit boundary
+#define RRBMP_FORCE    8  // Send this frame, even if identical to last.  Normally,
+                          // only a refresh packet is sent for identical frames
 
 // Uncompressed bitmap
 
@@ -32,48 +40,78 @@ class rrframe
 {
 	public:
 
-	rrframe(void)
+	rrframe(bool _primary=true) : bits(NULL), pitch(0), pixelsize(0), flags(0),
+		primary(_primary)
 	{
 		memset(&h, 0, sizeof(rrframeheader));
-		bits=NULL;
-		tryunix(pthread_mutex_init(&_ready, NULL));  tryunix(pthread_mutex_lock(&_ready));
-		tryunix(pthread_mutex_init(&_complete, NULL));
-		pixelsize=0;
+		_ready.lock();
 	}
 
 	virtual ~rrframe(void)
 	{
-		pthread_mutex_unlock(&_ready);  pthread_mutex_destroy(&_ready);
-		pthread_mutex_unlock(&_complete);  pthread_mutex_destroy(&_complete);
-		if(bits) delete [] bits;
+		if(bits && primary) delete [] bits;
 	}
 
-	void init(rrframeheader *hnew, int pixelsize)
+	void init(rrframeheader *hnew, int pixelsize, int flags=RRBMP_BGR)
 	{
-		if(!hnew) _throw("Invalid argument to rrframe::init()");
+		if(!hnew) throw(rrerror("rrframe::init", "Invalid argument"));
+		this->flags=flags;
 		hnew->size=hnew->winw*hnew->winh*pixelsize;
-		if(!checkheader(hnew, pixelsize)) _throw("Invalid argument to rrframe::init()");
+		checkheader(hnew);
 		if(pixelsize<3 || pixelsize>4) _throw("Only true color bitmaps are supported");
 		if(hnew->winw!=h.winw || hnew->winh!=h.winh || this->pixelsize!=pixelsize
 		|| !bits)
 		{
 			if(bits) delete [] bits;
 			errifnot(bits=new unsigned char[hnew->winw*hnew->winh*pixelsize]);
-			this->pixelsize=pixelsize;
+			this->pixelsize=pixelsize;  pitch=pixelsize*hnew->winw;
 		}
 		memcpy(&h, hnew, sizeof(rrframeheader));
 	}
 
-	void zero(void)
+	rrframe *getstrip(int startline, int endline)
 	{
-		if(!h.winw || !h.winh || !pixelsize) return;
-		memset(bits, 0, h.winw*h.winh*pixelsize);
+		rrframe *f;
+		if(!bits || !pitch || !pixelsize) _throw("Frame not initialized");
+		if(startline<0 || startline>h.bmph-1 || endline<=0 || endline>h.bmph)
+			_throw("Invalid argument");
+		errifnot(f=new rrframe(false));
+		f->h=h;
+		f->h.bmph=endline-startline;
+		f->h.bmpy+=startline;
+		f->pixelsize=pixelsize;
+		f->flags=flags;
+		f->pitch=pitch;
+		bool bu=(flags&RRBMP_BOTTOMUP);
+		f->bits=&bits[pitch*(bu? h.bmph-endline:startline)];
+		return f;
 	}
 
-	void ready(void) {pthread_mutex_unlock(&_ready);}
-	void waituntilready(void) {tryunix(pthread_mutex_lock(&_ready));}
-	void complete(void) {pthread_mutex_unlock(&_complete);}
-	void waituntilcomplete(void) {tryunix(pthread_mutex_lock(&_complete));}
+	void zero(void)
+	{
+		if(!h.winh || !pitch) return;
+		memset(bits, 0, pitch*h.winh);
+	}
+
+	bool stripequals(rrframe *last, int startline, int endline)
+	{
+		bool bu=(flags&RRBMP_BOTTOMUP);
+		if(last && h.bmpw==last->h.bmpw && h.bmph==last->h.bmph
+		&& h.winw==last->h.winw && h.winh==last->h.winh
+		&& h.qual==last->h.qual && h.subsamp==last->h.subsamp
+		&& pixelsize==last->pixelsize && h.winid==last->h.winid
+		&& h.dpynum==last->h.dpynum && bits && last->pitch==pitch
+		&& startline>=0 && startline<=h.bmph-1 && endline>0 && endline<=h.bmph
+		&& last->bits && !memcmp(&bits[pitch*(bu? h.bmph-endline:startline)],
+			&last->bits[pitch*(bu? h.bmph-endline:startline)],
+			pitch*(endline-startline))) return true;
+		return false;
+	}
+
+	void ready(void) {_ready.unlock();}
+	void waituntilready(void) {_ready.lock();}
+	void complete(void) {_complete.unlock();}
+	void waituntilcomplete(void) {_complete.lock();}
 
 	rrframe& operator= (rrframe& f)
 	{
@@ -90,38 +128,36 @@ class rrframe
 
 	rrframeheader h;
 	unsigned char *bits;
+	int pitch, pixelsize, flags;
 
 	protected:
 
 	void dumpheader(rrframeheader *h)
 	{
-		hpprintf("h->size    = %lu\n", h->size);
-		hpprintf("h->winid   = 0x%.8x\n", h->winid);
-		hpprintf("h->winw    = %d\n", h->winw);
-		hpprintf("h->winh    = %d\n", h->winh);
-		hpprintf("h->bmpw    = %d\n", h->bmpw);
-		hpprintf("h->bmph    = %d\n", h->bmph);
-		hpprintf("h->bmpx    = %d\n", h->bmpx);
-		hpprintf("h->bmpy    = %d\n", h->bmpy);
-		hpprintf("h->qual    = %d\n", h->qual);
-		hpprintf("h->subsamp = %d\n", h->subsamp);
-		hpprintf("h->eof     = %d\n", h->eof);
+		rrout.print("h->size    = %lu\n", h->size);
+		rrout.print("h->winid   = 0x%.8x\n", h->winid);
+		rrout.print("h->winw    = %d\n", h->winw);
+		rrout.print("h->winh    = %d\n", h->winh);
+		rrout.print("h->bmpw    = %d\n", h->bmpw);
+		rrout.print("h->bmph    = %d\n", h->bmph);
+		rrout.print("h->bmpx    = %d\n", h->bmpx);
+		rrout.print("h->bmpy    = %d\n", h->bmpy);
+		rrout.print("h->qual    = %d\n", h->qual);
+		rrout.print("h->subsamp = %d\n", h->subsamp);
+		rrout.print("h->eof     = %d\n", h->eof);
 	}
 
-	int pixelsize;
-
-	int checkheader(rrframeheader *h, int pixelsize)
+	void checkheader(rrframeheader *h)
 	{
-		if((h->size<1 && !h->eof)
-		|| h->winw<1 || h->winh<1 || h->bmpw<1 || h->bmph<1
-		|| h->bmpx+h->bmpw>h->winw || h->bmpy+h->bmph>h->winh
-		|| h->qual>100 || h->subsamp>RR_SUBSAMP-1)
-			return 0;
-		return 1;
+		if(!h || h->winw<1 || h->winh<1 || h->bmpw<1 || h->bmph<1
+		|| h->bmpx+h->bmpw>h->winw || h->bmpy+h->bmph>h->winh)
+			throw(rrerror("rrframe::checkheader", "Invalid header"));
 	}
 
-	pthread_mutex_t _ready;
-	pthread_mutex_t _complete;
+	rrmutex _ready;
+	rrmutex _complete;
+	friend class rrjpeg;
+	bool primary;
 };
 
 // Compressed JPEG
@@ -130,7 +166,7 @@ class rrjpeg : public rrframe
 {
 	public:
 
-	rrjpeg(void) : rrframe()
+	rrjpeg(void) : rrframe(), hpjhnd(NULL)
 	{
 		if(!(hpjhnd=hpjInitCompress())) _throw(hpjGetErrorStr());
 		pixelsize=3;
@@ -138,21 +174,23 @@ class rrjpeg : public rrframe
 
 	~rrjpeg(void)
 	{
-		hpjDestroy(hpjhnd);
+		if(hpjhnd) hpjDestroy(hpjhnd);
 	}
 
-	rrjpeg& operator= (rrbmp& b)
+	rrjpeg& operator= (rrframe& b)
 	{
 		int hpjflags=0;
 		if(!b.bits) _throw("Bitmap not initialized");
 		if(b.pixelsize<3 || b.pixelsize>4) _throw("Only true color bitmaps are supported");
-		b.h.size=b.h.winw*b.h.winh*b.pixelsize;
+		if(b.h.qual>100 || b.h.subsamp>RR_SUBSAMP-1)
+			throw(rrerror("JPEG compressor", "Invalid argument"));
 		init(&b.h);
-		if(b.flags&RRBMP_EOLPAD) hpjflags|=HPJ_EOLPAD;
+		int pitch=b.h.bmpw*b.pixelsize;
+		if(b.flags&RRBMP_EOLPAD) pitch=HPJPAD(pitch);
 		if(b.flags&RRBMP_BOTTOMUP) hpjflags|=HPJ_BOTTOMUP;
 		if(b.flags&RRBMP_BGR) hpjflags|=HPJ_BGR;
 		unsigned long size;
-		hpj(hpjCompress(hpjhnd, b.bits, b.h.bmpw, b.h.bmph, b.pixelsize,
+		hpj(hpjCompress(hpjhnd, b.bits, b.h.bmpw, pitch, b.h.bmph, b.pixelsize,
 			bits, &size, jpegsub[b.h.subsamp], b.h.qual, hpjflags));
 		h.size=(unsigned int)size;
 		return *this;
@@ -165,12 +203,12 @@ class rrjpeg : public rrframe
 
 	void init(rrframeheader *hnew)
 	{
-		if(!hnew || !checkheader(hnew, pixelsize)) _throw("Invalid argument to rrjpeg::init()");
+		checkheader(hnew);
 		if(hnew->winw!=h.winw || hnew->winh!=h.winh || !bits)
 		{
 			if(bits) delete [] bits;
-			// Dest. buffer for IJL must be big enough to hold 16 x 16 x 3 MCU, and I
-			// like to give it a wide berth
+			// Dest. buffer must be big enough to hold JPEG headers, quant. tables,
+			// etc., and I like to give it a wide berth
 			errifnot(bits=new unsigned char[HPJBUFSIZE(hnew->winw, hnew->winh)]);
 		}
 		memcpy(&h, hnew, sizeof(rrframeheader));
@@ -179,105 +217,97 @@ class rrjpeg : public rrframe
 	private:
 
 	hpjhandle hpjhnd;
-	friend class rrbitmap;
+	friend class rrfb;
 };
-
-//#ifdef _WIN32
-//#define fblock() rrlock l(mutex)
-//#else
-#define fblock()
-//#endif
-#define fbunlock()
 
 // Bitmap created from shared graphics memory
 
-class rrbitmap : public rrframe
+class rrfb : public rrframe
 {
 	public:
 
-	rrbitmap(Display *dpy, Window win) : rrframe()
+	rrfb(Display *dpy, Window win) : rrframe()
 	{
-		if(!dpy || !win) _throw("Invalid argument to rrbitmap constructor");
+		if(!dpy || !win) throw(rrerror("rrfb::rrfb", "Invalid argument"));
 		XFlush(dpy);
 		init(DisplayString(dpy), win);
 	}
 
-	rrbitmap(char *dpystring, Window win) : rrframe()
+	rrfb(char *dpystring, Window win) : rrframe()
 	{
 		init(dpystring, win);
 	}
 
 	void init(char *dpystring, Window win)
 	{
-		if(!dpystring || !win) _throw("Invalid argument to rrbitmap constructor");
-		wh.dpy=XOpenDisplay(dpystring);  wh.win=win;
+		hpjhnd=NULL;
 		memset(&fb, 0, sizeof(fbx_struct));
-		if(!(hpjhnd=hpjInitDecompress())) _throw(hpjGetErrorStr());
-//		#ifdef _WIN32
-//		if(!mutexinit) {tryunix(pthread_mutex_init(&mutex, NULL));  mutexinit=true;}
-//		#endif
+		if(!dpystring || !win) throw(rrerror("rrfb::init", "Invalid argument"));
+		wh.dpy=XOpenDisplay(dpystring);  wh.win=win;
 	}
 
-	~rrbitmap(void)
+	~rrfb(void)
 	{
-		fblock();
-		if(fb.bits) fbx_term(&fb);
-		hpjDestroy(hpjhnd);
+		if(fb.bits) fbx_term(&fb);  if(bits) bits=NULL;
+		if(hpjhnd) hpjDestroy(hpjhnd);
 		if(wh.dpy) XCloseDisplay(wh.dpy);
 	}
 
 	void init(rrframeheader *hnew)
 	{
-		if(!hnew || !checkheader(hnew)) _throw("Invalid argument to rrbitmap::init()");
-		fblock();
+		checkheader(hnew);
 		fbx(fbx_init(&fb, wh, hnew->winw, hnew->winh, 1));
 		if(hnew->winw>fb.width || hnew->winh>fb.height)
 		{
 			XSync(wh.dpy, False);
 			fbx(fbx_init(&fb, wh, hnew->winw, hnew->winh, 1));
 			if(hnew->winw>fb.width || hnew->winh>fb.height)
-				hpprintf("Window size mismatch.  Server=%dx%d  Client=%dx%d\n", hnew->winw, hnew->winh, fb.width, fb.height);
+				rrout.print("Window size mismatch.  Server=%dx%d  Client=%dx%d\n", hnew->winw, hnew->winh, fb.width, fb.height);
 		}
+		if(fb.ps<3 || fb.ps>4) _throw("Display must be 24-bit or 32-bit true color");
 		memcpy(&h, hnew, sizeof(rrframeheader));
+		pixelsize=fb.ps;  pitch=fb.xi->bytes_per_line;  bits=(unsigned char *)fb.bits;
+		if(fb.bgr) flags|=RRBMP_BGR;
+		if(fb.xi->bytes_per_line!=fb.width*fb.ps) flags|=RRBMP_EOLPAD;
 	}
 
-	rrbitmap& operator= (rrjpeg& f)
+	rrfb& operator= (rrjpeg& f)
 	{
 		int hpjflags=0;
 		if(!f.bits || f.h.size<1)
 			_throw("JPEG not initialized");
 		init(&f.h);
 		if(!fb.xi) _throw("Bitmap not initialized");
-		fblock();
-		if(fb.xi->bytes_per_line%fb.width!=0) hpjflags|=HPJ_EOLPAD;
 		if(fb.bgr) hpjflags|=HPJ_BGR;
 		if(f.h.bmpw<=fb.width && f.h.bmpy+f.h.bmph<=fb.height)
 		{
-		hpj(hpjDecompress(hpjhnd, f.bits, f.h.size, (unsigned char *)&fb.bits[fb.xi->bytes_per_line*f.h.bmpy],
-			f.h.bmpw, f.h.bmph, fb.ps, hpjflags));
+			if(!hpjhnd)
+			{
+				if((hpjhnd=hpjInitDecompress())==NULL) throw(rrerror("rrfb::decompressor", hpjGetErrorStr()));
+			}
+			hpj(hpjDecompress(hpjhnd, f.bits, f.h.size, (unsigned char *)&fb.bits[fb.xi->bytes_per_line*f.h.bmpy],
+				f.h.bmpw, fb.xi->bytes_per_line, f.h.bmph, fb.ps, hpjflags));
 		}
 		return *this;
 	}
 
 	void redraw(void)
 	{
-		fblock();
 		fbx(fbx_write(&fb, 0, 0, 0, 0, fb.width, fb.height));
 	}
 
 	void draw(void)
 	{
 		int _w=h.bmpw, _h=h.bmph;
-		fblock();
 		XWindowAttributes xwa;
 		if(!XGetWindowAttributes(wh.dpy, wh.win, &xwa))
 		{
-			hpprintf("Failed to get window attributes\n");
+			rrout.print("Failed to get window attributes\n");
 			return;
 		}
 		if(h.bmpx+h.bmpw>xwa.width || h.bmpy+h.bmph>xwa.height)
 		{
-			hpprintf("WARNING: bitmap (%d,%d) at (%d,%d) extends beyond window (%d,%d)\n",
+			rrout.print("WARNING: bitmap (%d,%d) at (%d,%d) extends beyond window (%d,%d)\n",
 				h.bmpw, h.bmph, h.bmpx, h.bmpy, xwa.width, xwa.height);
 			_w=min(h.bmpw, xwa.width-h.bmpx);
 			_h=min(h.bmph, xwa.height-h.bmpy);
@@ -286,20 +316,16 @@ class rrbitmap : public rrframe
 		fbx(fbx_write(&fb, h.bmpx, h.bmpy, h.bmpx, h.bmpy, _w, _h));
 	}
 
-	private:
-
-	int checkheader(rrframeheader *h)
+	void drawstrip(int startline, int endline)
 	{
-		if(h->winw<1 || h->winh<1 || h->bmpw<1 || h->bmph<1
-		|| h->bmpx+h->bmpw>h->winw || h->bmpy+h->bmph>h->winh
-		|| h->qual>100 || h->subsamp>RR_SUBSAMP-1)
-			return 0;
-		return 1;
+		if(startline<0 || startline>h.winh-1 || endline<0 || endline>h.winh)
+			return;
+		fbx(fbx_awrite(&fb, 0, startline, 0, startline, 0, endline-startline));
 	}
 
-//	#ifdef _WIN32
-//	static pthread_mutex_t mutex;  static bool mutexinit;
-//	#endif
+	void sync(void) {fbx(fbx_sync(&fb));}
+
+	private:
 
 	fbx_wh wh;
 	fbx_struct fb;
--- rr/rrframeut.cpp
+++ rr/rrframeut.cpp
@@ -11,7 +11,9 @@
  * wxWindows Library License for more details.
  */
 
+#include "rrthread.h"
 #include "rrframe.h"
+#include "rrtimer.h"
 
 #define ITER 50
 #define NB 2
@@ -20,214 +22,289 @@
 #define BORDER 0
 #define NUMWIN 3
 
-class rrframeut
+class blitter : public Runnable
 {
 	public:
 
-	rrframeut(Display *dpy, int myid)
+	blitter(Display *_dpy, Window _win) : indx(0), deadyet(false), dpy(_dpy),
+		win(_win), t(NULL)
 	{
-		int i;  buf=1;
-		w=-1;  h=-1;
-		this->dpy=dpy;  this->myid=myid;
-		cthnd=dthnd=bthnd=0;
-		deadyet=0;
-		errifnot(win=XCreateSimpleWindow(dpy, DefaultRootWindow(dpy), myid*(MINW+BORDER*2), 0, MINW+BORDER, MINW+BORDER, 0,
-			WhitePixel(dpy, DefaultScreen(dpy)), BlackPixel(dpy, DefaultScreen(dpy))));
-		errifnot(XMapRaised(dpy, win));
-
-		for(i=0; i<NB; i++)
-		{
-			errifnot(b[i]=new rrbitmap(dpy, win));
-			memset(&bmp[i], 0, sizeof(rrbmp));
-			tryunix(pthread_mutex_init(&_ready[i], NULL));  tryunix(pthread_mutex_lock(&_ready[i]));
-			tryunix(pthread_mutex_init(&_complete[i], NULL));
-		}
-
-		tryunix(pthread_create(&cthnd, NULL, compressor, this));
-		tryunix(pthread_create(&dthnd, NULL, decompressor, this));
-		tryunix(pthread_create(&bthnd, NULL, blitter, this));
+		for(int i=0; i<NB; i++) errifnot(fb[i]=new rrfb(dpy, win));
+		errifnot(t=new Thread(this));
+		t->start();
 	}
 
-	~rrframeut(void)
+	virtual ~blitter(void)
 	{
 		shutdown();
+		for(int i=0; i<NB; i++) {if(fb[i]) {delete fb[i];  fb[i]=NULL;}}
 	}
 
-	void dotest(int w, int h)
+	rrfb *get(void)
 	{
-		buf=(buf+1)%NB;
-		tryunix(pthread_mutex_lock(&_complete[buf]));
-		#if 0
-		if(w!=this->w || h!=this->h)
-		{
-			XLockDisplay(dpy);
-			XWindowChanges xwc;
-			xwc.width=w;  xwc.height=h;  xwc.x=(w+BORDER*2)*myid;  xwc.y=0;
-			XConfigureWindow(dpy, win, CWWidth|CWHeight|CWX|CWY, &xwc);
-			XFlush(dpy);
-			XSync(dpy, False);
-			XWindowAttributes xwa;
-			do XGetWindowAttributes(dpy, win, &xwa); while(xwa.width<w || xwa.height<h);
-			this->w=w;  this->h=h;
-			XUnlockDisplay(dpy);
-		}
-		#endif
-		allocbmp(buf, w, h, 3);
-		bmp[buf].h.winid=win;
-		bmp[buf].h.winw=w+BORDER;
-		bmp[buf].h.winh=h+BORDER;
-		bmp[buf].h.bmpx=BORDER;
-		bmp[buf].h.bmpy=BORDER;
-		bmp[buf].h.qual=80;
-		bmp[buf].h.subsamp=RR_422;
-		bmp[buf].flags=0;
-		initbmp(buf);
-		pthread_mutex_unlock(&_ready[buf]);
+		rrfb *b=fb[indx];  indx=(indx+1)%NB;
+		if(t) t->checkerror();  b->waituntilcomplete();  if(t) t->checkerror();
+		return b;
+	}
+
+	void put(rrfb *b)
+	{
+		if(t) t->checkerror();
+		b->ready();
+	}
+
+	void shutdown(void)
+	{
+		deadyet=true;
+		int i;
+		for(i=0; i<NB; i++) fb[i]->ready();  // Release my thread
+		if(t) {t->stop();  delete t;  t=NULL;}
+		for(i=0; i<NB; i++) fb[i]->complete();  // Release decompressor
 	}
 
 	private:
 
-	void allocbmp(int buf, int w, int h, int pixelsize)
+	void run(void)
 	{
-		if(buf<0 || buf>NB-1 || w<1 || h<1 || pixelsize<3 || pixelsize>4)
-			throw("Invalid argument to allocbmp()");
-		if(bmp[buf].h.bmpw==w && bmp[buf].h.bmph==h && bmp[buf].pixelsize==pixelsize
-			&& bmp[buf].bits) return;
-		if(bmp[buf].bits) {free(bmp[buf].bits);  memset(&bmp[buf], 0, sizeof(rrbmp));}
-		errifnot(bmp[buf].bits=(unsigned char *)malloc(w*h*pixelsize));
-		bmp[buf].h.bmpw=w;  bmp[buf].h.bmph=h;
-		bmp[buf].pixelsize=pixelsize;
+		rrtimer timer;
+		double mpixels=0., totaltime=0.;
+		int buf=0;  rrfb *b;
+		try
+		{
+			while(!deadyet)
+			{
+				b=fb[buf];  buf=(buf+1)%NB;
+				b->waituntilready();  if(deadyet) break;
+				timer.start();
+				b->redraw();
+				mpixels+=(double)(b->h.bmpw*b->h.bmph)/1000000.;
+				totaltime+=timer.elapsed();
+				b->complete();
+			}
+			fprintf(stderr, "Average blitter performance = %f Mpixels/sec\n", mpixels/totaltime);
+		}
+		catch (...)
+		{
+			for(int i=0; i<NB; i++) if(fb[i]) fb[i]->complete();  throw;
+		}
+		fprintf(stderr, "Blitter exiting ...\n");
 	}
 
-	void initbmp(int buf)
+	int indx;  bool deadyet;  
+	rrfb *fb[NB];
+	Display *dpy;  Window win;
+	Thread *t;
+};
+
+class decompressor : public Runnable
+{
+	public:
+
+	decompressor(blitter *_bobj, Display *_dpy, Window _win, int _myid) : bobj(_bobj),
+		indx(0), deadyet(false), dpy(_dpy), win(_win), myid(_myid), t(NULL)
 	{
-		int i, j, pitch=bmp[buf].h.bmpw*bmp[buf].pixelsize;
-		for(i=0; i<bmp[buf].h.bmph; i++)
+		errifnot(t=new Thread(this));
+		t->start();
+	}
+
+	virtual ~decompressor(void) {shutdown();}
+
+	rrjpeg &get(void)
+	{
+		rrjpeg &j=jpg[indx];  indx=(indx+1)%NB;
+		if(deadyet) return j;
+		if(t) t->checkerror();  j.waituntilcomplete();  if(t) t->checkerror();
+		return j;
+	}
+
+	void put(rrjpeg &j)
+	{
+		if(t) t->checkerror();
+		j.ready();
+	}
+
+	void shutdown(void)
+	{
+		deadyet=true;
+		int i;
+		for(i=0; i<NB; i++) jpg[i].ready();  // Release my thread
+		if(t) {t->stop();  delete t;  t=NULL;}
+		for(i=0; i<NB; i++) jpg[i].complete();  // Release compressor
+	}
+
+	private:
+
+	void run(void)
+	{
+		int buf=0;  rrfb *b=NULL;
+		try
 		{
-			for(j=0; j<bmp[buf].h.bmpw; j++)
+			while(!deadyet)
 			{
-				bmp[buf].bits[i*pitch+j*bmp[buf].pixelsize]=i%256;
-				bmp[buf].bits[i*pitch+j*bmp[buf].pixelsize+1]=j%256;
-				bmp[buf].bits[i*pitch+j*bmp[buf].pixelsize+2]=(i+j)%256;
+				rrjpeg &j=jpg[buf];  buf=(buf+1)%NB;
+				j.waituntilready();  if(deadyet) break;
+				b=bobj->get();  if(deadyet) break;
+				XWindowAttributes xwa;
+				int w=j.h.bmpw, h=j.h.bmph;
+				XGetWindowAttributes(dpy, win, &xwa);
+				if(w!=xwa.width || h!=xwa.height)
+				{
+					XLockDisplay(dpy);
+					XWindowChanges xwc;
+					xwc.width=w;  xwc.height=h;
+					xwc.x=(w+BORDER*2)*myid;  xwc.y=0;
+					XConfigureWindow(dpy, win, CWWidth|CWHeight|CWX|CWY, &xwc);
+					XFlush(dpy);
+					XSync(dpy, False);
+					XWindowAttributes xwa;
+					do XGetWindowAttributes(dpy, win, &xwa); while(xwa.width<w || xwa.height<h);
+					XUnlockDisplay(dpy);
+				}
+				(*b)=j;
+				bobj->put(b);
+				j.complete();
+			}
+		}
+		catch (...)
+		{
+			for(int i=0; i<NB; i++) jpg[i].complete();  throw;
+		}
+		fprintf(stderr, "Decompressor exiting ...\n");
+	}
+
+	blitter *bobj;
+	int indx;  bool deadyet;
+	Display *dpy;  Window win;
+	rrjpeg jpg[NB];
+	int myid;  Thread *t;
+};
+
+class compressor : public Runnable
+{
+	public:
+
+	compressor(decompressor *_dobj) : indx(0), deadyet(false), t(NULL), dobj(_dobj)
+	{
+		errifnot(t=new Thread(this));
+		t->start();
+	}
+
+	virtual ~compressor(void)
+	{
+		if(t) t->stop();
+	}
+
+	rrframe &get(int w, int h)
+	{
+		rrframe &f=bmp[indx];  indx=(indx+1)%NB;
+		if(t) t->checkerror();  f.waituntilcomplete();  if(t) t->checkerror();
+		rrframeheader hdr;
+		hdr.winw=hdr.bmpw=w+BORDER;
+		hdr.winh=hdr.bmph=h+BORDER;
+		hdr.bmpx=hdr.bmpy=BORDER;
+		hdr.qual=80;
+		hdr.subsamp=RR_422;
+		f.init(&hdr, 3);
+		return f;
+	}
+
+	void put(rrframe &f)
+	{
+		if(t) t->checkerror();
+		f.ready();
+	}
+
+	void shutdown(void)
+	{
+		deadyet=true;
+		int i;
+		for(i=0; i<NB; i++) bmp[i].ready();  // Release my thread
+		if(t) {t->stop();  delete t;  t=NULL;}
+		for(i=0; i<NB; i++) bmp[i].complete();  // Release main thread
+	}
+
+	private:
+
+	void run(void)
+	{
+		int buf=0;
+		try
+		{
+			while(!deadyet)
+			{
+				rrframe &f=bmp[buf];  buf=(buf+1)%NB;
+				f.waituntilready();  if(deadyet) break;
+				rrjpeg &j=dobj->get();  if(deadyet) break;
+				j=f;
+				dobj->put(j);
+				f.complete();
+			}
+		}
+		catch (...)
+		{
+			for(int i=0; i<NB; i++) bmp[i].complete();  throw;
+		}
+		fprintf(stderr, "Compressor exiting ...\n");
+	}
+
+	int indx;  bool deadyet;
+	Thread *t;
+	rrframe bmp[NB];
+	decompressor *dobj;
+};
+
+class rrframeut
+{
+	public:
+
+	rrframeut(Display *dpy, int myid) : c(NULL), d(NULL), b(NULL)
+	{
+		Window win;
+		errifnot(win=XCreateSimpleWindow(dpy, DefaultRootWindow(dpy), myid*(MINW+BORDER*2), 0, MINW+BORDER, MINW+BORDER, 0,
+			WhitePixel(dpy, DefaultScreen(dpy)), BlackPixel(dpy, DefaultScreen(dpy))));
+		errifnot(XMapRaised(dpy, win));
+
+		errifnot(b=new blitter(dpy, win));
+		errifnot(d=new decompressor(b, dpy, win, myid));
+		errifnot(c=new compressor(d));
+	}
+
+	~rrframeut(void) {shutdown();}
+
+	void dotest(int w, int h)
+	{
+		rrframe &f=c->get(w, h);
+		initbmp(f);
+		c->put(f);
+	}
+
+	private:
+
+	void initbmp(rrframe &f)
+	{
+		int i, j, pitch=f.pitch;
+		for(i=0; i<f.h.bmph; i++)
+		{
+			for(j=0; j<f.h.bmpw; j++)
+			{
+				f.bits[i*pitch+j*f.pixelsize]=i%256;
+				f.bits[i*pitch+j*f.pixelsize+1]=j%256;
+				f.bits[i*pitch+j*f.pixelsize+2]=(i+j)%256;
 			}	
 		}
 	}
 
 	void shutdown(void)
 	{
-		int i;
-		deadyet=1;
-		for(i=0; i<NB; i++) {if(b[i]) b[i]->ready();}
-		if(bthnd) {pthread_join(bthnd, NULL);  bthnd=0;}
-
-		for(i=0; i<NB; i++) {if(b[i]) b[i]->complete();  j[i].ready();}
-		if(dthnd) {pthread_join(dthnd, NULL);  dthnd=0;}
-
-		for(i=0; i<NB; i++) {j[i].complete();  pthread_mutex_unlock(&_ready[i]);}
-		if(cthnd) {pthread_join(cthnd, NULL);  cthnd=0;}
-
-		for(i=0; i<NB; i++) {delete b[i];  b[i]=NULL;}
+		fprintf(stderr, "Shutting down....\n");  fflush(stderr);
+		if(b) b->shutdown();  if(d) d->shutdown();  if(c) c->shutdown();
+		if(b) {delete b;  b=NULL;}
+		if(d) {delete d;  d=NULL;}
+		if(c) {delete c;  c=NULL;}
 	}
 
-	FILE *f;
-	Display *dpy;
-	int myid, buf, w, h;
-	rrbmp bmp[NB];
-	pthread_mutex_t _ready[NB], _complete[NB];
-	rrjpeg j[NB];
-	rrbitmap *b[NB];
-	int deadyet;
-	pthread_t cthnd, dthnd, bthnd;
-	Window win;
-	friend void *compressor(void *);
-	friend void *decompressor(void *);
-	friend void *blitter(void *);
+	compressor *c;  decompressor *d;  blitter *b;
 };
-
-void *compressor(void *p)
-{
-	rrframeut *test=(rrframeut *)p;
-	int buf=1;
-	try
-	{
-		while(!test->deadyet)
-		{
-			buf=(buf+1)%NB;
-			tryunix(pthread_mutex_lock(&test->_ready[buf]));
-			test->j[buf].waituntilcomplete();
-			test->j[buf]=test->bmp[buf];
-			pthread_mutex_unlock(&test->_complete[buf]);
-			test->j[buf].ready();
-		}
-	}
-	catch (char *e) {hpprintf("Compressor- %s\n", e);  test->cthnd=0;  exit(1);}
-	catch (const char *e) {hpprintf("Compressor- %s\n", e);  test->cthnd=0;  exit(1);}
-	hpprintf("Compressor exiting ...\n");
-	return 0;
-}
-
-void *decompressor(void *p)
-{
-	rrframeut *test=(rrframeut *)p;
-	int buf=1, w=-1, h=-1;
-	try
-	{
-		while(!test->deadyet)
-		{
-			buf=(buf+1)%NB;
-			test->j[buf].waituntilready();
-			test->b[buf]->waituntilcomplete();
-			w=test->j[buf].h.bmpw;  h=test->j[buf].h.bmph;
-		if(w!=test->w || h!=test->h)
-		{
-			XLockDisplay(test->dpy);
-			XWindowChanges xwc;
-			xwc.width=w;  xwc.height=h;  xwc.x=(w+BORDER*2)*test->myid;  xwc.y=0;
-			XConfigureWindow(test->dpy, test->win, CWWidth|CWHeight|CWX|CWY, &xwc);
-			XFlush(test->dpy);
-			XSync(test->dpy, False);
-			XWindowAttributes xwa;
-			do XGetWindowAttributes(test->dpy, test->win, &xwa); while(xwa.width<w || xwa.height<h);
-			test->w=w;  test->h=h;
-			XUnlockDisplay(test->dpy);
-		}
-			*(test->b[buf])=test->j[buf];
-			test->j[buf].complete();
-			test->b[buf]->ready();
-		}
-	}
-	catch (char *e) {hpprintf("Decompressor- %s\n", e);  test->dthnd=0;  exit(1);}
-	catch (const char *e) {hpprintf("Decompressor- %s\n", e);  test->dthnd=0;  exit(1);}
-	hpprintf("Decompressor exiting ...\n");
-	return 0;
-}
-
-void *blitter(void *p)
-{
-	rrframeut *test=(rrframeut *)p;
-	hptimer_init();
-	double mpixels=0., totaltime=0.;
-	int iter=0;
-	int buf=1;
-	try
-	{
-		while(!test->deadyet)
-		{
-			buf=(buf+1)%NB;
-			test->b[buf]->waituntilready();
-			double _time=hptime();
-			test->b[buf]->draw();
-			mpixels+=(double)(test->w*test->h)/1000000.;
-			totaltime+=hptime()-_time;
-			test->b[buf]->complete();
-		}
-		hpprintf("Average blitter performance = %f Mpixels/sec\n", mpixels/totaltime);
-	}
-	catch (char *e) {hpprintf("Blitter- %s\n", e);  test->bthnd=0;  exit(1);}
-	catch (const char *e) {hpprintf("Blitter- %s\n", e);  test->bthnd=0;  exit(1);}
-	hpprintf("Blitter exiting ...\n");
-	return 0;
-}
 
 int main(void)
 {
@@ -235,11 +312,10 @@ int main(void)
 	rrframeut *test[NUMWIN];
 	int i, j, w, h;
 
-	hputil_log(1, 0);
 	try
 	{
 		errifnot(XInitThreads());
-		if(!(dpy=XOpenDisplay(0))) {hpprintf("Could not open display %s\n", XDisplayName(0));  exit(1);}
+		if(!(dpy=XOpenDisplay(0))) {fprintf(stderr, "Could not open display %s\n", XDisplayName(0));  exit(1);}
 
 		for(i=0; i<NUMWIN; i++)
 		{
@@ -248,14 +324,38 @@ int main(void)
 
 		for(w=MINW; w<=MAXW; w+=33)
 		{
-			h=w;
-			hpprintf("%.4d x %.4d: ", w, h);
+			h=1;
+			fprintf(stderr, "%.4d x %.4d: ", w, h);
 			for(i=0; i<ITER; i++)
 			{
-				hpprintf(".");
+				fprintf(stderr, ".");
 				for(j=0; j<NUMWIN; j++) test[j]->dotest(w, h);
 			}
-			hpprintf("\n");
+			fprintf(stderr, "\n");
+		}
+
+		for(h=MINW; h<=MAXW; h+=33)
+		{
+			w=1;
+			fprintf(stderr, "%.4d x %.4d: ", w, h);
+			for(i=0; i<ITER; i++)
+			{
+				fprintf(stderr, ".");
+				for(j=0; j<NUMWIN; j++) test[j]->dotest(w, h);
+			}
+			fprintf(stderr, "\n");
+		}
+
+		for(w=MINW; w<=MAXW; w+=33)
+		{
+			h=w;
+			fprintf(stderr, "%.4d x %.4d: ", w, h);
+			for(i=0; i<ITER; i++)
+			{
+				fprintf(stderr, ".");
+				for(j=0; j<NUMWIN; j++) test[j]->dotest(w, h);
+			}
+			fprintf(stderr, "\n");
 		}
 
 		for(i=0; i<NUMWIN; i++)
@@ -263,7 +363,10 @@ int main(void)
 			delete test[i];
 		}
 	}
-	catch (char *e) {hpprintf("Main- %s\n", e);  exit(1);}	
-	catch (const char *e) {hpprintf("Main- %s\n", e);  exit(1);}	
+	catch (rrerror &e)
+	{
+		fprintf(stderr, "%s\n%s\n", e.getMethod(), e.getMessage());
+		exit(1);
+	}	
 	return 0;
 }
