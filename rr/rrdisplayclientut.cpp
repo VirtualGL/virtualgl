7fb8dd11d65ba24ab9c1b3fd43193c4e31d63c13
DRC <dcommander@users.sourceforge.net>
2004-09-08 05:34:08 +0000

    Initial import
    git-svn-id: svn+ssh://svn.code.sf.net/p/virtualgl/code/trunk@52 799e4f7b-5fd2-41f6-823c-2ecc41bc7f0b

---
+++ rr/rrdisplayclientut.cpp
@@ -0,0 +1,134 @@
+#include "rrdisplayclient.h"
+#include "rrtimer.h"
+#include "bmp.h"
+
+int main(int argc, char **argv)
+{
+	rrtimer t;  double elapsed;
+	unsigned char *buf=NULL, *buf2=NULL, *buf3=NULL;
+	Display *dpy=NULL;  Window win=0;  int dpynum=0;
+
+	try {
+
+	int qual, subsamp;
+	if(argc<4 || (qual=atoi(argv[3]))<0 || qual>100 || (subsamp=atoi(argv[2]))<0
+		|| subsamp>RR_SUBSAMP)
+	{
+		printf("USAGE: %s <bitmap file> <subsamp> <qual> [server]\n", argv[0]);
+		printf("subsamp = 0=none, 1=4:2:2, 2=4:1:1\n");
+		printf("qual = 0-100 inclusive\n");
+		exit(1);
+	}
+
+	char *servername=NULL;  if(argc>4) servername=argv[4];
+	unsigned short port=0;
+	if(servername) port=RR_DEFAULTPORT;
+
+	rrdisplayclient rrdpy(servername, port, false);
+	int w, h, d;
+	const char *err;
+	if((err=loadbmp(argv[1], &buf, &w, &h, &d, 0))!=NULL) _throw(err);
+	if((err=loadbmp(argv[1], &buf2, &w, &h, &d, 0))!=NULL) _throw(err);
+	if((err=loadbmp(argv[1], &buf3, &w, &h, &d, 0))!=NULL) _throw(err);
+	printf("Source image: %d x %d x %d-bit\n", w, h, d*8);
+
+	if(servername)
+	{
+		if(!XInitThreads()) _throw("Could not initialize X threads");
+		if((dpy=XOpenDisplay(0))==NULL) _throw("Could not open display");
+		if((win=XCreateSimpleWindow(dpy, DefaultRootWindow(dpy), 0, 0,
+			w, h, 0, WhitePixel(dpy, DefaultScreen(dpy)),
+			BlackPixel(dpy, DefaultScreen(dpy))))==0) _throw("Could not create window");
+		printf("Creating window %d\n", win);
+		errifnot(XMapRaised(dpy, win));
+		XSync(dpy, False);
+		dpynum=0;  char *dpystring=NULL, *ptr=NULL;
+		dpystring=DisplayString(dpy);
+		if((ptr=strchr(dpystring, ':'))!=NULL && strlen(ptr)>1) dpynum=atoi(ptr+1);
+	}
+
+	int i;
+	for(i=0; i<w*h*d; i++) buf2[i]=255-buf2[i];
+	for(i=0; i<w*h*d/2; i++) buf3[i]=255-buf3[i];
+
+	rrframe *b;
+
+	printf("\nTesting full-frame send ...\n");
+
+	int frames=0, fill=0;  t.start();
+	do
+	{
+		errifnot(b=rrdpy.getbitmap(w, h, d));
+		if(fill) memcpy(b->bits, buf, w*h*d);
+		else memcpy(b->bits, buf2, w*h*d);
+		b->h.qual=qual;  b->h.subsamp=subsamp;
+		b->h.dpynum=dpynum;  b->h.winid=win;
+		fill=1-fill;
+		rrdpy.sendframe(b);
+		frames++;
+	} while((elapsed=t.elapsed())<2.);
+
+	printf("%f Megapixels/sec\n", (double)w*(double)h*(double)frames/1000000./elapsed);
+
+	printf("\nTesting full-frame send (spoiling) ...\n");
+
+	fill=0, frames=0;  int clientframes=0;  t.start();
+	do
+	{
+		if(!rrdpy.frameready())
+		{
+			if(fill) memcpy(b->bits, buf, w*h*d);
+			else memcpy(b->bits, buf2, w*h*d);
+			frames++;  continue;
+		}
+		errifnot(b=rrdpy.getbitmap(w, h, d));
+		if(fill) memcpy(b->bits, buf, w*h*d);
+		else memcpy(b->bits, buf2, w*h*d);
+		b->h.qual=qual;  b->h.subsamp=subsamp;
+		b->h.dpynum=dpynum;  b->h.winid=win;
+		fill=1-fill;
+		rrdpy.sendframe(b);
+		clientframes++;  frames++;
+	} while((elapsed=t.elapsed())<2.);
+
+	printf("%f Megapixels/sec (server)\n", (double)w*(double)h*(double)frames/1000000./elapsed);
+	printf("%f Megapixels/sec (client)\n", (double)w*(double)h*(double)clientframes/1000000./elapsed);
+
+	printf("\nTesting half-frame send ...\n");
+
+	fill=0, frames=0;  t.start();
+	do
+	{
+		errifnot(b=rrdpy.getbitmap(w, h, d));
+		if(fill) memcpy(b->bits, buf, w*h*d);
+		else memcpy(b->bits, buf3, w*h*d);
+		b->h.qual=qual;  b->h.subsamp=subsamp;
+		b->h.dpynum=dpynum;  b->h.winid=win;
+		fill=1-fill;
+		rrdpy.sendframe(b);
+		frames++;
+	} while((elapsed=t.elapsed())<2.);
+
+	printf("%f Megapixels/sec\n", (double)w*(double)h*(double)frames/1000000./elapsed);
+
+	printf("\nTesting zero-frame send ...\n");
+
+	frames=0;  t.start();
+	do
+	{
+		errifnot(b=rrdpy.getbitmap(w, h, d));
+		memcpy(b->bits, buf, w*h*d);
+		b->h.qual=qual;  b->h.subsamp=subsamp;
+		b->h.dpynum=dpynum;  b->h.winid=win;
+		rrdpy.sendframe(b);
+		frames++;
+	} while((elapsed=t.elapsed())<2.);
+
+	printf("%f Megapixels/sec\n", (double)w*(double)h*(double)frames/1000000./elapsed);
+
+	} catch(rrerror &e) {printf("%s--\n%s\n", e.getMethod(), e.getMessage());}
+
+	if(win) XDestroyWindow(dpy, win);
+	if(dpy) XCloseDisplay(dpy);
+	if(buf) free(buf);  if(buf2) free(buf2);  if(buf3) free(buf3);
+}
