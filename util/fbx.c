f533e1c8b8fd50be1a5ed721ea540663f1bc0a04
DRC <dcommander@users.sourceforge.net>
2004-09-16 22:47:35 +0000

    Set pitch after 32-bit buffer is auto-selected
    git-svn-id: svn+ssh://svn.code.sf.net/p/virtualgl/code/branches/1.0.x@103 799e4f7b-5fd2-41f6-823c-2ecc41bc7f0b

--- util/fbx.c
+++ util/fbx.c
@@ -106,9 +106,8 @@ int fbx_init(fbx_struct *s, fbx_wh wh, int width, int height, int useshm)
 	if(height>0) bminfo.bmi.bmiHeader.biHeight=height;
 	s->width=bminfo.bmi.bmiHeader.biWidth;
 	s->height=bminfo.bmi.bmiHeader.biHeight;
-	s->pitch=BMPPAD(s->width*s->ps);  // Windoze bitmaps are always padded
 
-	if(s->ps<24)
+	if(s->ps<3)
 	{
 		// Make the buffer BGRA
 		bminfo.bmi.bmiHeader.biCompression=BI_BITFIELDS;
@@ -118,6 +117,8 @@ int fbx_init(fbx_struct *s, fbx_wh wh, int width, int height, int useshm)
 		(*(DWORD *)&bminfo.bmi.bmiColors[1])=0xFF00;
 		(*(DWORD *)&bminfo.bmi.bmiColors[2])=0xFF;
 	}
+
+	s->pitch=BMPPAD(s->width*s->ps);  // Windoze bitmaps are always padded
 
 	if(bminfo.bmi.bmiHeader.biCompression==BI_BITFIELDS)
 	{
