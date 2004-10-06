338fae2aeabfa550175fb031e47c0273eee6f63d
DRC <dcommander@users.sourceforge.net>
2004-10-06 17:22:16 +0000

    Adjustable strip height
    git-svn-id: svn+ssh://svn.code.sf.net/p/virtualgl/code/trunk@159 799e4f7b-5fd2-41f6-823c-2ecc41bc7f0b

--- rr/rrdisplayclientut.cpp
+++ rr/rrdisplayclientut.cpp
@@ -24,16 +24,22 @@ int main(int argc, char **argv)
 	try {
 
 	int qual, subsamp;
-	if(argc<4 || (qual=atoi(argv[3]))<0 || qual>100 || (subsamp=atoi(argv[2]))<0
+	if(argc<5 || (qual=atoi(argv[3]))<0 || qual>100 || (subsamp=atoi(argv[2]))<0
 		|| subsamp>RR_SUBSAMP)
 	{
-		printf("USAGE: %s <bitmap file> <subsamp> <qual> [server]\n", argv[0]);
+		printf("USAGE: %s <bitmap file> <subsamp> <qual> <server> [striph]\n", argv[0]);
 		printf("subsamp = 0=none, 1=4:2:2, 2=4:1:1\n");
 		printf("qual = 0-100 inclusive\n");
+		printf("server = machine where RRXClient is running (0=local test only)\n");
+		printf("striph = height of each inter-frame difference tile\n");
 		exit(1);
 	}
 
-	char *servername=NULL;  if(argc>4) servername=argv[4];
+	int striph=RR_DEFAULTSTRIPHEIGHT, temp;
+	if(argc>5 && (temp=atoi(argv[5]))>0) striph=temp;
+	printf("Strip height = %d pixels\n", striph);
+	char *servername=argv[4];
+	if(!stricmp(servername, "0")) servername=NULL;
 	unsigned short port=0;
 	if(servername) port=RR_DEFAULTPORT;
 
@@ -76,6 +82,7 @@ int main(int argc, char **argv)
 		else memcpy(b->bits, buf2, w*h*d);
 		b->h.qual=qual;  b->h.subsamp=subsamp;
 		b->h.dpynum=dpynum;  b->h.winid=win;
+		b->strip_height=striph;
 		fill=1-fill;
 		rrdpy.sendframe(b);
 		frames++;
@@ -99,6 +106,7 @@ int main(int argc, char **argv)
 		else memcpy(b->bits, buf2, w*h*d);
 		b->h.qual=qual;  b->h.subsamp=subsamp;
 		b->h.dpynum=dpynum;  b->h.winid=win;
+		b->strip_height=striph;
 		fill=1-fill;
 		rrdpy.sendframe(b);
 		clientframes++;  frames++;
@@ -117,6 +125,7 @@ int main(int argc, char **argv)
 		else memcpy(b->bits, buf3, w*h*d);
 		b->h.qual=qual;  b->h.subsamp=subsamp;
 		b->h.dpynum=dpynum;  b->h.winid=win;
+		b->strip_height=striph;
 		fill=1-fill;
 		rrdpy.sendframe(b);
 		frames++;
@@ -133,6 +142,7 @@ int main(int argc, char **argv)
 		memcpy(b->bits, buf, w*h*d);
 		b->h.qual=qual;  b->h.subsamp=subsamp;
 		b->h.dpynum=dpynum;  b->h.winid=win;
+		b->strip_height=striph;
 		rrdpy.sendframe(b);
 		frames++;
 	} while((elapsed=t.elapsed())<2.);
