b17164116b28fd84b6945a5a143e9dce698d1141
DRC <dcommander@users.sourceforge.net>
2004-10-26 17:43:35 +0000

    Back-ported FBX unit test from head
    git-svn-id: svn+ssh://svn.code.sf.net/p/virtualgl/code/branches/1.0.x@198 799e4f7b-5fd2-41f6-823c-2ecc41bc7f0b

--- util/fbxtest.cpp
+++ util/fbxtest.cpp
@@ -20,9 +20,8 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <string.h>
-#include "rrthread.h"
-#include "rrutil.h"
-#include "rrtimer.h"
+#include "pthread.h"
+#include "hputil.h"
 #include "fbx.h"
 #ifndef WIN32
  #include <errno.h>
@@ -57,9 +56,11 @@ int xhandler(Display *dpy, XErrorEvent *xe)
 
 int width, height;
 fbx_wh wh;
-rrtimer timer;
+#define fbx(f) {if((f)==-1) throw(fbx_geterrmsg());}
+#define errifnot(f) {if(!(f)) throw("Unexpected NULL condition");}
 #ifdef WIN32
 #define fg() SetForegroundWindow(wh)
+#define tryw32(f) {if(!(f)) throw(hputil_strerror());}
 #else
 #define fg()
 #endif
@@ -132,11 +133,11 @@ void nativewrite(int useshm)
 	try {
 
 	fbx(fbx_init(&s, wh, 0, 0, useshm));
-	if(useshm && !s.shm) _throw("MIT-SHM not available");
+	if(useshm && !s.shm) throw("MIT-SHM not available");
 	fprintf(stderr, "Native Pixel Format:  %s", fbx_formatname(s.format));
 	fprintf(stderr, "\n");
 	if(s.width!=width || s.height!=height)
-		_throw("The benchmark window lost input focus or was obscured.\nSkipping native write test\n");
+		throw("The benchmark window lost input focus or was obscured.\nSkipping native write test\n");
 
 	clearfb();
 	initbuf(0, 0, width, s.pitch, height, s.format, (unsigned char *)s.bits);
@@ -148,14 +149,14 @@ void nativewrite(int useshm)
 	do
 	{
 		n+=n;
-		timer.start();
+		rbtime=hptime();
 		for (i=0; i<n; i++)
 		{
 			for(int j=0; j<height; j++)
 				fbx(fbx_awrite(&s, 0, j, 0, height-j-1, width, 1));
 			fbx_sync(&s);
 		}
-		rbtime=timer.elapsed();
+		rbtime=hptime()-rbtime;
 	} while(rbtime<2.);
 	fprintf(stderr, "%f Mpixels/sec\n", (double)n*(double)(width*height)/((double)1000000.*rbtime));
 
@@ -168,16 +169,17 @@ void nativewrite(int useshm)
 	do
 	{
 		n+=n;
-		timer.start();
+		rbtime=hptime();
 		for (i=0; i<n; i++)
 		{
 			fbx(fbx_write(&s, 0, 0, 0, 0, 0, 0));
 		}
-		rbtime=timer.elapsed();
+		rbtime=hptime()-rbtime;
 	} while(rbtime<2.);
 	fprintf(stderr, "%f Mpixels/sec\n", (double)n*(double)(width*height)/((double)1000000.*rbtime));
 
-	} catch(rrerror &e) {fprintf(stderr, "%s\n", e.getMessage());}
+	} catch(char *e) {fprintf(stderr, "%s\n", e);}
+	catch(const char *e) {fprintf(stderr, "%s\n", e);}
 
 	fbx_term(&s);
 }
@@ -192,9 +194,9 @@ void nativeread(int useshm)
 
 	fbx(fbx_init(&s, wh, 0, 0, useshm));
 	int ps=fbx_ps[s.format];
-	if(useshm && !s.shm) _throw("MIT-SHM not available");
+	if(useshm && !s.shm) throw("MIT-SHM not available");
 	if(s.width!=width || s.height!=height)
-		_throw("The benchmark window lost input focus or was obscured.\nSkipping native read test\n");
+		throw("The benchmark window lost input focus or was obscured.\nSkipping native read test\n");
 	if(useshm)
 		fprintf(stderr, "FBX read [SHM]:                   ");
 	else
@@ -204,115 +206,172 @@ void nativeread(int useshm)
 	do
 	{
 		n+=n;
-		timer.start();
+		rbtime=hptime();
 		for(i=0; i<n; i++)
 		{
 			fbx(fbx_read(&s, 0, 0));
 		}
-		rbtime=timer.elapsed();
+		rbtime=hptime()-rbtime;
 		if(!cmpbuf(0, 0, width, s.pitch, height, s.format, (unsigned char *)s.bits))
-			_throw("ERROR: Bogus data read back.");
+			throw("ERROR: Bogus data read back.");
 	} while (rbtime<2.);
 	fprintf(stderr, "%f Mpixels/sec\n", (double)n*(double)(width*height)/((double)1000000.*rbtime));
 
-	} catch(rrerror &e) {fprintf(stderr, "%s\n", e.getMessage());}
+	} catch(char *e) {fprintf(stderr, "%s\n", e);}
+	catch(const char *e) {fprintf(stderr, "%s\n", e);}
 
 	fbx_term(&s);
 }
 
 // This serves as a unit test for the FBX library
-class writethread : public Runnable
+class writethread
 {
 	public:
 		writethread(int _myrank, int _iter, int _useshm) : myrank(_myrank),
-			iter(_iter), useshm(_useshm) {}
-
-		void run(void)
+			iter(_iter), useshm(_useshm), t(0)
 		{
+			int err;
+			if((err=pthread_create(&t, NULL, run, this))!=0) throw(strerror(err));
+		}
+
+		~writethread(void) {stop(false);}
+
+		void stop(bool checkerror)
+		{
+			char *err=NULL;
+			if(t) {pthread_join(t, (void **)&err);  t=0;}
+			if(err && checkerror) throw(err);
+		}
+
+		static void *run(void *param)
+		{
+			writethread *w=(writethread *)param;
 			int i;  fbx_struct stressfb;
 			memset(&stressfb, 0, sizeof(stressfb));
 
 			try {
 
 			int mywidth, myheight, myx=0, myy=0;
-			if(myrank<2) {mywidth=width/2;  myx=0;}
+			if(w->myrank<2) {mywidth=width/2;  myx=0;}
 			else {mywidth=width-width/2;  myx=width/2;}
-			if(myrank%2==0) {myheight=height/2;  myy=0;}
+			if(w->myrank%2==0) {myheight=height/2;  myy=0;}
 			else {myheight=height-height/2;  myy=height/2;}
-			fbx(fbx_init(&stressfb, wh, mywidth, myheight, useshm));
-			if(useshm && !stressfb.shm) _throw("MIT-SHM not available");
+			fbx(fbx_init(&stressfb, wh, mywidth, myheight, w->useshm));
+			if(w->useshm && !stressfb.shm) throw("MIT-SHM not available");
 			initbuf(myx, myy, mywidth, stressfb.pitch, myheight, stressfb.format, (unsigned char *)stressfb.bits);
-			for (i=0; i<iter; i++)
+			for (i=0; i<w->iter; i++)
 				fbx(fbx_write(&stressfb, 0, 0, myx, myy, mywidth, myheight));
 
-			} catch(...) {fbx_term(&stressfb);  throw;}
+			} catch(char *e) {fbx_term(&stressfb);  return (void *)e;}
+			catch(const char *e) {fbx_term(&stressfb);  return (void *)e;}
+			fbx_term(&stressfb);
+			return 0;
 		}
 
 	private:
 		int myrank, iter, useshm;
+		pthread_t t;
 };
 
 fbx_struct stressfb0;
 
-class writethread1 : public Runnable
+class writethread1
 {
 	public:
 		writethread1(int _myrank, int _iter, int _useshm) : myrank(_myrank),
-			iter(_iter), useshm(_useshm) {}
-
-		void run(void)
+			iter(_iter), useshm(_useshm), t(0)
 		{
+			int err;
+			if((err=pthread_create(&t, NULL, run, this))!=0) throw(strerror(err));
+		}
+
+		~writethread1(void) {stop(false);}
+
+		void stop(bool checkerror)
+		{
+			char *err=NULL;
+			if(t) {pthread_join(t, (void **)&err);  t=0;}
+			if(err && checkerror) throw(err);
+		}
+
+		static void *run(void *param)
+		{
+			try {
+
+			writethread1 *w=(writethread1 *)param;
 			int i, mywidth, myheight, myx=0, myy=0;
-			if(myrank<2) {mywidth=width/2;  myx=0;}
+			if(w->myrank<2) {mywidth=width/2;  myx=0;}
 			else {mywidth=width-width/2;  myx=width/2;}
-			if(myrank%2==0) {myheight=height/2;  myy=0;}
+			if(w->myrank%2==0) {myheight=height/2;  myy=0;}
 			else {myheight=height-height/2;  myy=height/2;}
-			for (i=0; i<iter; i++)
+			for (i=0; i<w->iter; i++)
 				fbx(fbx_write(&stressfb0, myx, myy, myx, myy, mywidth, myheight));
+
+			} catch(char *e) {return (void *)e;}
+			catch(const char *e) {return (void *)e;}
+			return 0;
 		}
 
 	private:
 		int myrank, iter, useshm;
+		pthread_t t;
 };
 
-class readthread : public Runnable
+class readthread
 {
 	public:
 		readthread(int _myrank, int _iter, int _useshm) : myrank(_myrank),
-			iter(_iter), useshm(_useshm) {}
-
-		void run(void)
+			iter(_iter), useshm(_useshm), t(0)
 		{
+			int err;
+			if((err=pthread_create(&t, NULL, run, this))!=0) throw(strerror(err));
+		}
+
+		~readthread(void) {stop(false);}
+
+		void stop(bool checkerror)
+		{
+			char *err=NULL;
+			if(t) {pthread_join(t, (void **)&err);  t=0;}
+			if(err && checkerror) throw(err);
+		}
+
+		static void *run(void *param)
+		{
+			readthread *r=(readthread *)param;
 			fbx_struct stressfb;
 			memset(&stressfb, 0, sizeof(stressfb));
 
 			try {
 
 			int i, mywidth, myheight, myx=0, myy=0;
-			if(myrank<2) {mywidth=width/2;  myx=0;}
+			if(r->myrank<2) {mywidth=width/2;  myx=0;}
 			else {mywidth=width-width/2;  myx=width/2;}
-			if(myrank%2==0) {myheight=height/2;  myy=0;}
+			if(r->myrank%2==0) {myheight=height/2;  myy=0;}
 			else {myheight=height-height/2;  myy=height/2;}
-			fbx(fbx_init(&stressfb, wh, mywidth, myheight, useshm));
-			if(useshm && !stressfb.shm) _throw("MIT-SHM not available");
+			fbx(fbx_init(&stressfb, wh, mywidth, myheight, r->useshm));
+			if(r->useshm && !stressfb.shm) throw("MIT-SHM not available");
 			int ps=fbx_ps[stressfb.format];
 			memset(stressfb.bits, 0, mywidth*myheight*ps);
-			for(i=0; i<iter; i++)
+			for(i=0; i<r->iter; i++)
 				fbx(fbx_read(&stressfb, myx, myy));
 			if(!cmpbuf(myx, myy, mywidth, stressfb.pitch, myheight, stressfb.format, (unsigned char *)stressfb.bits))
-				_throw("ERROR: Bogus data read back.");
+				throw("ERROR: Bogus data read back.");
 
-			} catch(...) {fbx_term(&stressfb);  throw;}
+			} catch(char *e) {fbx_term(&stressfb);  return (void *)e;}
+			catch(const char *e) {fbx_term(&stressfb);  return (void *)e;}
+			fbx_term(&stressfb);
+			return 0;
 		}
 
 	private:
 		int myrank, iter, useshm;
+		pthread_t t;
 };
 
 void nativestress(int useshm)
 {
 	int i, n;  double rbtime;
-	Thread *t[4];
 
 	memset(&stressfb0, 0, sizeof(stressfb0));
 
@@ -327,22 +386,18 @@ void nativestress(int useshm)
 	do
 	{
 		n+=n;
-		timer.start();
+		rbtime=hptime();
 		writethread *wt[4];
 		for(i=0; i<4; i++)
-		{
 			wt[i]=new writethread(i, n, useshm);
-			t[i]=new Thread(wt[i]);
-			t[i]->start();
-		}
-		for(i=0; i<4; i++) t[i]->stop();
-		for(i=0; i<4; i++) t[i]->checkerror();
-		for(i=0; i<4; i++) {delete t[i];  delete wt[i];}
-		rbtime=timer.elapsed();
+		for(i=0; i<4; i++) wt[i]->stop(true);
+		for(i=0; i<4; i++) delete wt[i];
+		rbtime=hptime()-rbtime;
 	} while (rbtime<2.);
 	fprintf(stderr, "%f Mpixels/sec\n", (double)n*(double)(width*height)/((double)1000000.*rbtime));
 
-	} catch(rrerror &e) {fprintf(stderr, "%s\n", e.getMessage());}
+	} catch(char *e) {fprintf(stderr, "%s\n", e);}
+	catch(const char *e) {fprintf(stderr, "%s\n", e);}
 
 	try {
 
@@ -354,22 +409,18 @@ void nativestress(int useshm)
 	do
 	{
 		n+=n;
-		timer.start();
+		rbtime=hptime();
 		readthread *rt[4];
 		for(i=0; i<4; i++)
-		{
 			rt[i]=new readthread(i, n, useshm);
-			t[i]=new Thread(rt[i]);
-			t[i]->start();
-		}
-		for(i=0; i<4; i++) t[i]->stop();
-		for(i=0; i<4; i++) t[i]->checkerror();
-		for(i=0; i<4; i++) {delete t[i];  delete rt[i];}
-		rbtime=timer.elapsed();
+		for(i=0; i<4; i++) rt[i]->stop(true);
+		for(i=0; i<4; i++) delete rt[i];
+		rbtime=hptime()-rbtime;
 	} while (rbtime<2.);
 	fprintf(stderr, "%f Mpixels/sec\n", (double)n*(double)(width*height)/((double)1000000.*rbtime));
 
-	} catch(rrerror &e) {fprintf(stderr, "%s\n", e.getMessage());}
+	} catch(char *e) {fprintf(stderr, "%s\n", e);}
+	catch(const char *e) {fprintf(stderr, "%s\n", e);}
 
 	try {
 
@@ -380,29 +431,25 @@ void nativestress(int useshm)
 		fprintf(stderr, "FBX write [1 bmp, 4 blits]:       ");
 	memset(&stressfb0, 0, sizeof(stressfb0));
 	fbx(fbx_init(&stressfb0, wh, width, height, useshm));
-	if(useshm && !stressfb0.shm) _throw("MIT-SHM not supported");
+	if(useshm && !stressfb0.shm) throw("MIT-SHM not supported");
 	initbuf(0, 0, width, stressfb0.pitch, height, stressfb0.format, (unsigned char *)stressfb0.bits);
 	n=N;
 	do
 	{
 		n+=n;
-		timer.start();
+		rbtime=hptime();
 		writethread1 *wt1[4];
 		for(i=0; i<4; i++)
-		{
 			wt1[i]=new writethread1(i, n, useshm);
-			t[i]=new Thread(wt1[i]);
-			t[i]->start();
-		}
-		for(i=0; i<4; i++) t[i]->stop();
-		for(i=0; i<4; i++) t[i]->checkerror();
-		for(i=0; i<4; i++) {delete t[i];  delete wt1[i];}
-		rbtime=timer.elapsed();
+		for(i=0; i<4; i++) wt1[i]->stop(true);
+		for(i=0; i<4; i++) delete wt1[i];
+		rbtime=hptime()-rbtime;
 	} while (rbtime<2.);
 	fbx_term(&stressfb0);
 	fprintf(stderr, "%f Mpixels/sec\n", (double)n*(double)(width*height)/((double)1000000.*rbtime));
 
-	} catch(rrerror &e) {fprintf(stderr, "%s\n", e.getMessage());}
+	} catch(char *e) {fprintf(stderr, "%s\n", e);}
+	catch(const char *e) {fprintf(stderr, "%s\n", e);}
 
 	fg();  nativeread(useshm);
 
@@ -454,6 +501,8 @@ int main(int argc, char **argv)
 {
 	fprintf(stderr, "\n%s v%s (Build %s)\n", bench_name, __VERSION, __BUILD);
 
+	hptimer_init();
+
 	try {
 
 	#ifdef WIN32
@@ -504,7 +553,7 @@ int main(int argc, char **argv)
 	BOOL ret;
 	while(1)
 	{
-		if((ret=GetMessage(&msg, NULL, 0, 0))==-1) _throww32();
+		if((ret=GetMessage(&msg, NULL, 0, 0))==-1) throw(hputil_strerror());
 		else if(ret==0) break;
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
@@ -523,5 +572,6 @@ int main(int argc, char **argv)
 
 	#endif
 
-	} catch(rrerror &e) {fprintf(stderr, "%s\n", e.getMessage());}
+	} catch(char *e) {fprintf(stderr, "%s\n", e);}
+	catch(const char *e) {fprintf(stderr, "%s\n", e);}
 }
