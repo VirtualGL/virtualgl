f5eef619b994efe3657302c96fd8be0bccba8fdd
DRC <dcommander@users.sourceforge.net>
2005-03-16 23:00:22 +0000

    Client now generates its own certificates
    git-svn-id: svn+ssh://svn.code.sf.net/p/virtualgl/code/branches/1.0.x@409 799e4f7b-5fd2-41f6-823c-2ecc41bc7f0b

--- rr/newrrcert.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set CERTFILE="%USERPROFILE%\rrcert.pem"
-if /i "%1"=="root" set CERTFILE=rrcert.pem
-openssl req -new -x509 -days 365 -nodes -config rrcert.cnf -out %CERTFILE% -keyout %CERTFILE%
-openssl x509 -subject -dates -fingerprint -noout -in %CERTFILE%
-pause
