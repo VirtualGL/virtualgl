760216b7bb897ef306fd7e983be6a3a10542f4eb
DRC <dcommander@users.sourceforge.net>
2005-03-17 00:05:47 +0000

    Client certificates are now automatically generated
    git-svn-id: svn+ssh://svn.code.sf.net/p/virtualgl/code/trunk@416 799e4f7b-5fd2-41f6-823c-2ecc41bc7f0b

--- rr/newrrcert.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set CERTFILE="%USERPROFILE%\vglcert.pem"
-if /i "%1"=="root" set CERTFILE=vglcert.pem
-openssl req -new -x509 -days 365 -nodes -config vglcert.cnf -out %CERTFILE% -keyout %CERTFILE%
-openssl x509 -subject -dates -fingerprint -noout -in %CERTFILE%
-pause
