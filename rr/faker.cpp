8c61ddc8d22c204a4b42359c8db43a5be47bfdd5
DRC <dcommander@users.sourceforge.net>
2005-03-31 20:32:15 +0000

    Use tweaked out Mesa glXUseXFont() in both GLP and GLX modes
    git-svn-id: svn+ssh://svn.code.sf.net/p/virtualgl/code/trunk@459 799e4f7b-5fd2-41f6-823c-2ecc41bc7f0b

--- rr/faker.cpp
+++ rr/faker.cpp
@@ -802,43 +802,15 @@ void glXDestroyPixmap(Display *dpy, GLXPixmap pix)
 	CATCH();
 }
 
-// This will fail if the server and client don't have a reasonably
-// similar set of fonts
-
-#ifdef USEGLP
 #include "xfonts.c"
-#endif
 
+// We use a tweaked out version of the Mesa glXUseXFont()
+// implementation.
 void glXUseXFont(Font font, int first, int count, int list_base)
 {
 	TRY();
-	#ifdef USEGLP
-	if(fconfig.glp)
-	{
-		Fake_glXUseXFont(font, first, count, list_base);
-		return;
-	}
-	else
-	#endif
-	{
-		XFontStruct *fs=NULL;
-		Display *dpy=NULL;  pbwin *pb=NULL;
-		errifnot(pb=winh.findpb(glXGetCurrentDrawable()));
-		dpy=pb->getwindpy();
-		errifnot(fs=XQueryFont(dpy, font));
-		char *fontname=NULL;
-		for(int i=0; i<fs->n_properties; i++)
-		{
-			char *atom;
-			errifnot(atom=XGetAtomName(dpy, fs->properties[i].name));
-			if(!strcmp(atom, "FONT"))
-				errifnot(fontname=XGetAtomName(dpy, fs->properties[i].card32));
-		}
-		errifnot(font=XLoadFont(_localdpy, fontname));
-		_glXUseXFont(font, first, count, list_base);
-		XUnloadFont(_localdpy, font);
-		XFreeFontInfo(NULL, fs, 1);
-	}
+	Fake_glXUseXFont(font, first, count, list_base);
+	return;
 	CATCH();
 }
 
