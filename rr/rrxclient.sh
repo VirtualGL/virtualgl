53703d506a4431200510d5bc1dcf3ff116e47be6
DRC <dcommander@users.sourceforge.net>
2006-02-15 19:40:53 +0000

    Use LSB init functions rather than SuSE-specific ones
    git-svn-id: svn+ssh://svn.code.sf.net/p/virtualgl/code/trunk@1012 799e4f7b-5fd2-41f6-823c-2ecc41bc7f0b

--- rr/rrxclient.sh
+++ rr/rrxclient.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # description: Linux Client Daemon
 
-SUSE=0
-if [ -f /etc/rc.status ]; then
-	SUSE=1
-	. /etc/rc.status
+REDHAT=0
+if [ -f /etc/rc.d/init.d/functions ]; then
+	. /etc/rc.d/init.d/functions
+	REDHAT=1;
 else
-	if [ -f /etc/rc.d/init.d/functions ]; then
-		. /etc/rc.d/init.d/functions
+	if [ -f /lib/lsb/init-functions ]; then
+		. /lib/lsb/init-functions
 	else
 		echo "Unsupported platform"
 		exit 1
@@ -21,11 +21,10 @@ LOGPATH=/var/log/
 
 start() {
 	echo -n $"Starting Client Daemon: "
-	if [ $SUSE -eq 1 ]; then
-		startproc /usr/bin/vglclient --service -l $LOGPATH/vglclient.log
-		rc_status -v
-	else
+	if [ $REDHAT -eq 1 ]; then
 		daemon /usr/bin/vglclient --service -l $LOGPATH/vglclient.log && success || failure
+	else
+		start_daemon /usr/bin/vglclient --service -l $LOGPATH/vglclient.log && log_success_msg || log_failure_msg
 	fi
 	echo
 	if [ ! $? -eq 0 ]; then RETVAL=$?; fi
@@ -33,11 +32,10 @@ start() {
 
 stop() {
 	echo -n $"Shutting down Client Daemon: "
-	if [ $SUSE -eq 1 ]; then
-		killproc -TERM /usr/bin/vglclient
-		rc_status -v
-	else
+	if [ $REDHAT -eq 1 ]; then
 		killproc vglclient
+	else
+		killproc vglclient && log_success_msg || log_failure_msg
 	fi
 	echo
 }
@@ -50,10 +48,6 @@ reload() {
 ID=`id -u`
 if [ ! $ID -eq 0 ]; then
 	LOGPATH=$HOME/
-fi
-
-if [ $SUSE -eq 1 ]; then
-	rc_reset
 fi
 
 case "$1" in
