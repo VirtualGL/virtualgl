1cb18aae57d79680d380f23032b93004eb2d76ca
DRC <dcommander@users.sourceforge.net>
2007-02-07 08:42:47 +0000

    Hide all non-interposed symbols in librrfaker
    git-svn-id: svn+ssh://svn.code.sf.net/p/virtualgl/code/branches/2.0.x@1465 799e4f7b-5fd2-41f6-823c-2ecc41bc7f0b

---
+++ rr/faker-mapfile.c
@@ -0,0 +1,112 @@
+#ifdef linux
+VERS_2.0.1
+#endif
+{
+	global:
+		glXChooseVisual;
+		glXCopyContext;
+		glXCreateContext;
+		glXCreateGLXPixmap;
+		glXDestroyContext;
+		glXDestroyGLXPixmap;
+		glXGetConfig;
+		#ifdef USEGLP
+		glXGetCurrentContext;
+		#endif
+		glXGetCurrentDrawable;
+		glXIsDirect;
+		glXMakeCurrent;
+		glXQueryExtension;
+		glXQueryVersion;
+		glXSwapBuffers;
+		glXUseXFont;
+		glXWaitGL;
+
+		glXGetClientString;
+		glXQueryServerString;
+		glXQueryExtensionsString;
+
+		glXChooseFBConfig;
+		glXCreateNewContext;
+		glXCreatePbuffer;
+		glXCreatePixmap;
+		glXCreateWindow;
+		glXDestroyPbuffer;
+		glXDestroyPixmap;
+		glXDestroyWindow;
+		glXGetCurrentDisplay;
+		glXGetCurrentReadDrawable;
+		glXGetFBConfigAttrib;
+		glXGetFBConfigs;
+		glXGetSelectedEvent;
+		glXGetVisualFromFBConfig;
+		glXMakeContextCurrent;
+		glXQueryContext;
+		glXQueryDrawable;
+		glXSelectEvent;
+
+		glXFreeContextEXT;
+		glXImportContextEXT;
+		glXQueryContextInfoEXT;
+
+		glXJoinSwapGroupNV;
+		glXBindSwapBarrierNV;
+		glXQuerySwapGroupNV;
+		glXQueryMaxSwapGroupsNV;
+		glXQueryFrameCountNV;
+		glXResetFrameCountNV;
+
+		glXGetProcAddressARB;
+		glXGetProcAddress;
+
+		glFinish;
+		glFlush;
+		glViewport;
+		glDrawBuffer;
+		glPopAttrib;
+		glReadPixels;
+		glDrawPixels;
+		#ifdef SUNOGL
+		glBegin;
+		#else
+		glIndexd;
+		glIndexf;
+		glIndexi;
+		glIndexs;
+		glIndexub;
+		glIndexdv;
+		glIndexfv;
+		glIndexiv;
+		glIndexsv;
+		glIndexubv;
+		#endif
+		glClearIndex;
+		glGetDoublev;
+		glGetFloatv;
+		glGetIntegerv;
+		glPixelTransferf;
+		glPixelTransferi;
+
+		XCheckMaskEvent;
+		XCheckTypedEvent;
+		XCheckTypedWindowEvent;
+		XCheckWindowEvent;
+		XConfigureWindow;
+		XCopyArea;
+		XCreateWindow;
+		XCreateSimpleWindow;
+		XDestroyWindow;
+		XFree;
+		XGetGeometry;
+		XMaskEvent;
+		XMoveResizeWindow;
+		XNextEvent;
+		XOpenDisplay;
+		XQueryExtension;
+		XResizeWindow;
+		XServerVendor;
+		XWindowEvent;
+
+	local:
+		*;
+};
