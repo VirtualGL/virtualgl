92d9ea3867d7e3f106b21bc26d3315b2fb36921f
DRC <dcommander@users.sourceforge.net>
2007-02-23 01:08:32 +0000

    Merge from 2.0
    git-svn-id: svn+ssh://svn.code.sf.net/p/virtualgl/code/trunk@1576 799e4f7b-5fd2-41f6-823c-2ecc41bc7f0b

---
+++ rr/faker-mapfile.c
@@ -0,0 +1,131 @@
+{
+	global:
+		glXChooseVisual;
+		glXCopyContext;
+		glXCreateContext;
+		glXCreateGLXPixmap;
+		glXDestroyContext;
+		glXDestroyGLXPixmap;
+		glXGetConfig;
+		#ifdef USEGLP
+		glXGetCurrentContext;
+		#endif
+		glXGetCurrentDrawable;
+		glXIsDirect;
+		glXMakeCurrent;
+		glXQueryExtension;
+		glXQueryVersion;
+		glXSwapBuffers;
+		glXUseXFont;
+		glXWaitGL;
+
+		glXGetClientString;
+		glXQueryServerString;
+		glXQueryExtensionsString;
+
+		glXChooseFBConfig;
+		glXCreateNewContext;
+		glXCreatePbuffer;
+		glXCreatePixmap;
+		glXCreateWindow;
+		glXDestroyPbuffer;
+		glXDestroyPixmap;
+		glXDestroyWindow;
+		glXGetCurrentDisplay;
+		glXGetCurrentReadDrawable;
+		glXGetFBConfigAttrib;
+		glXGetFBConfigs;
+		glXGetSelectedEvent;
+		glXGetVisualFromFBConfig;
+		glXMakeContextCurrent;
+		glXQueryContext;
+		glXQueryDrawable;
+		glXSelectEvent;
+
+		glXChooseFBConfigSGIX;
+		glXCreateContextWithConfigSGIX;
+		glXCreateGLXPbufferSGIX;
+		glXCreateGLXPixmapWithConfigSGIX;
+		glXDestroyGLXPbufferSGIX;
+		glXGetCurrentReadDrawableSGI;
+		glXGetFBConfigAttribSGIX;
+		glXGetFBConfigFromVisualSGIX;
+		glXGetSelectedEventSGIX;
+ 		glXGetVisualFromFBConfigSGIX;
+		glXMakeCurrentReadSGI;
+		glXQueryGLXPbufferSGIX;
+		glXSelectEventSGIX;
+
+		glXFreeContextEXT;
+		glXImportContextEXT;
+		glXQueryContextInfoEXT;
+
+		glXJoinSwapGroupNV;
+		glXBindSwapBarrierNV;
+		glXQuerySwapGroupNV;
+		glXQueryMaxSwapGroupsNV;
+		glXQueryFrameCountNV;
+		glXResetFrameCountNV;
+
+		glXGetTransparentIndexSUN;
+
+		glXGetProcAddressARB;
+		glXGetProcAddress;
+
+		glFinish;
+		glFlush;
+		glViewport;
+		glDrawBuffer;
+		glPopAttrib;
+		glReadPixels;
+		glDrawPixels;
+		#ifdef SUNOGL
+		glBegin;
+		#else
+		glIndexd;
+		glIndexf;
+		glIndexi;
+		glIndexs;
+		glIndexub;
+		glIndexdv;
+		glIndexfv;
+		glIndexiv;
+		glIndexsv;
+		glIndexubv;
+		#endif
+		glClearIndex;
+		glGetDoublev;
+		glGetFloatv;
+		glGetIntegerv;
+		glPixelTransferf;
+		glPixelTransferi;
+
+		XCheckMaskEvent;
+		XCheckTypedEvent;
+		XCheckTypedWindowEvent;
+		XCheckWindowEvent;
+		XConfigureWindow;
+		XCopyArea;
+		XCreateWindow;
+		XCreateSimpleWindow;
+		XDestroyWindow;
+		XFree;
+		XGetGeometry;
+		XListExtensions;
+		XMaskEvent;
+		XMoveResizeWindow;
+		XNextEvent;
+		XOpenDisplay;
+		XQueryExtension;
+		XResizeWindow;
+		XServerVendor;
+		XWindowEvent;
+
+		__XFree;
+		__XGetGeometry;
+		__XNextEvent;
+		__XOpenDisplay;
+
+	local:
+		*;
+};
