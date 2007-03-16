8fdd5216be51d09ff9cb8e8d6bec2c628beddd41
DRC <dcommander@users.sourceforge.net>
2007-03-16 01:16:12 +0000

    CVS headers annoy me
    git-svn-id: svn+ssh://svn.code.sf.net/p/virtualgl/code/branches/2.0.x@1604 799e4f7b-5fd2-41f6-823c-2ecc41bc7f0b

--- rr/glx.h
+++ rr/glx.h
@@ -22,7 +22,6 @@
 ** Those portions of the Subject Software created by Silicon Graphics, Inc.
 ** are Copyright (c) 1991-9 Silicon Graphics, Inc. All Rights Reserved.
 **
-** $Header: /home/drc/cvs/vgl/rr/glx.h,v 1.8.2.1 2006-08-21 20:50:31 dcommander Exp $
 */
 
 #include <X11/Xlib.h>
