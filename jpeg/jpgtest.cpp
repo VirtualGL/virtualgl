835198804d17439d9a52abc1b5a96a0fbe6f62bb
DRC <dcommander@users.sourceforge.net>
2009-06-22 07:41:50 +0000

    Errant line feed
    git-svn-id: svn+ssh://svn.code.sf.net/p/virtualgl/code/trunk@2531 799e4f7b-5fd2-41f6-823c-2ecc41bc7f0b

--- jpeg/jpgtest.cpp
+++ jpeg/jpgtest.cpp
@@ -297,8 +297,8 @@ int main(int argc, char *argv[])
 		printf("       [-rgb | -bgr | -rgba | -bgra | -abgr | -argb]\n");
 		printf("       Test the specified color conversion path in the codec (default: BGR)\n\n");
 		printf("       [-fastupsample]\n");
-		printf("       Use fast, inaccurate upsampling code to perform 4:2:2 and 4:2:0\n\n");
-		printf("       YUV decoding in libjpeg decompressor\n");
+		printf("       Use fast, inaccurate upsampling code to perform 4:2:2 and 4:2:0\n");
+		printf("       YUV decoding in libjpeg decompressor\n\n");
 		printf("       [-quiet]\n");
 		printf("       Output in tabular rather than verbose format\n\n");
 		printf("       NOTE: If the quality is specified as a range, i.e. 90-100, a separate\n");
