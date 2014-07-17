VirtualGL/virtualgl

commit 869104e5126deed418bc609780181cfd806069b1

Fix compiler warnings revealed by Oracle Solaris Studio
git-svn-id: svn+ssh://svn.code.sf.net/p/virtualgl/code/trunk@3545 799e4f7b-5fd2-41f6-823c-2ecc41bc7f0b

server/VirtualPixmap.cpp
@@ -22,12 +22,12 @@ using namespace vglcommon;
 using namespace vglserver;
 
 
-VirtualPixmap::VirtualPixmap(Display *dpy, XVisualInfo *vis, Pixmap pm)
-	: VirtualDrawable(dpy, pm)
+VirtualPixmap::VirtualPixmap(Display *dpy_, XVisualInfo *vis_, Pixmap pm)
+	: VirtualDrawable(dpy_, pm)
 {
 	CS::SafeLock l(mutex);
 	profPMBlit.setName("PMap Blit ");
-	_newcheck(frame=new FBXFrame(dpy, pm, vis->visual));
+	_newcheck(frame=new FBXFrame(dpy_, pm, vis->visual));
 }
 
 

client/GLFrame.cpp
@@ -45,11 +45,11 @@ GLFrame::GLFrame(Display *dpy_, Window win_) : Frame(),
 
 	dpy=dpy_;
 	isGL=true;
-	init(dpy, win);
+	init();
 }
 
 
-void GLFrame::init(Display *dpy, Window win)
+void GLFrame::init(void)
 {
 	XVisualInfo *v=NULL;
 

client/GLFrame.h
@@ -38,7 +38,7 @@ namespace vglcommon
 
 		private:
 
-			void init(Display *dpy, Window win);
+			void init(void);
 			int glError(void);
 
 			Display *dpy;  Window win;

common/Frame.cpp
@@ -491,15 +491,15 @@ void CompressedFrame::compressRGB(Frame &f)
 	int bu=(f.flags&FRAME_BOTTOMUP)? 1:0;
 	if(f.flags&FRAME_BGR || f.flags&FRAME_ALPHAFIRST || f.pixelSize!=3)
 		throw(Error("RGB compressor", "Source frame is not RGB"));
-	int pitch=f.hdr.width*f.pixelSize;
+	int dstPitch=f.hdr.width*f.pixelSize;
 	int srcStride=bu? f.pitch:-f.pitch;
 
 	init(f.hdr, f.stereo? RR_LEFT:0);
 	srcptr=bu? f.bits:&f.bits[f.pitch*(f.hdr.height-1)];
 	for(i=0, dstptr=bits; i<f.hdr.height; i++, srcptr+=srcStride,
-		dstptr+=pitch)
-		memcpy(dstptr, srcptr, pitch);
-	hdr.size=pitch*f.hdr.height;
+		dstptr+=dstPitch)
+		memcpy(dstptr, srcptr, dstPitch);
+	hdr.size=dstPitch*f.hdr.height;
 
 	if(f.stereo && f.rbits)
 	{
@@ -508,9 +508,9 @@ void CompressedFrame::compressRGB(Frame &f)
 		{
 			srcptr=bu? f.rbits:&f.rbits[f.pitch*(f.hdr.height-1)];
 			for(i=0, dstptr=rbits; i<f.hdr.height; i++, srcptr+=srcStride,
-				dstptr+=pitch)
-				memcpy(dstptr, srcptr, pitch);
-			rhdr.size=pitch*f.hdr.height;
+				dstptr+=dstPitch)
+				memcpy(dstptr, srcptr, dstPitch);
+			rhdr.size=dstPitch*f.hdr.height;
 		}
 	}
 }
@@ -658,17 +658,17 @@ void FBXFrame::redraw(void)
 
 // Frame created using X Video
 
-XVFrame::XVFrame(Display *dpy, Window win) : Frame()
+XVFrame::XVFrame(Display *dpy_, Window win_) : Frame()
 {
-	if(!dpy || !win) throw(Error("XVFrame::XVFrame", "Invalid argument"));
-	XFlush(dpy);
-	init(DisplayString(dpy), win);
+	if(!dpy_ || !win_) throw(Error("XVFrame::XVFrame", "Invalid argument"));
+	XFlush(dpy_);
+	init(DisplayString(dpy_), win_);
 }
 
 
-XVFrame::XVFrame(char *dpystring, Window win) : Frame()
+XVFrame::XVFrame(char *dpystring, Window win_) : Frame()
 {
-	init(dpystring, win);
+	init(dpystring, win_);
 }
 
 

include/Error.h
@@ -32,24 +32,24 @@ namespace vglutil
 	{
 		public:
 
-			Error(const char *method, char *message)
+			Error(const char *method_, char *message_)
 			{
-				init(method, message, -1);
+				init(method_, message_, -1);
 			}
 
-			Error(const char *method, const char *message)
+			Error(const char *method_, const char *message_)
 			{
-				init(method, (char *)message, -1);
+				init(method_, (char *)message_, -1);
 			}
 
-			Error(const char *method, char *message, int line)
+			Error(const char *method_, char *message_, int line)
 			{
-				init(method, message, line);
+				init(method_, message_, line);
 			}
 
-			Error(const char *method, const char *message, int line)
+			Error(const char *method_, const char *message_, int line)
 			{
-				init(method, (char *)message, line);
+				init(method_, (char *)message_, line);
 			}
 
 			void init(const char *method_, char *message_, int line)

server/VirtualDrawable.cpp
@@ -82,7 +82,7 @@ VirtualDrawable::OGLDrawable::OGLDrawable(int width_, int height_,
 	glxDraw=glXCreatePbuffer(_dpy3D, config, pbattribs);
 	if(!glxDraw) _throw("Could not create Pbuffer");
 
-	setVisAttribs(config);
+	setVisAttribs();
 }
 
 
@@ -106,7 +106,7 @@ VirtualDrawable::OGLDrawable::OGLDrawable(int width_, int height_, int depth_,
 	glxDraw=glXCreatePixmap(_dpy3D, config, pm, attribs);
 	if(!glxDraw) goto bailout;
 
-	setVisAttribs(config);
+	setVisAttribs();
 	return;
 
 	bailout:
@@ -115,7 +115,7 @@ VirtualDrawable::OGLDrawable::OGLDrawable(int width_, int height_, int depth_,
 }
 
 
-void VirtualDrawable::OGLDrawable::setVisAttribs(GLXFBConfig config)
+void VirtualDrawable::OGLDrawable::setVisAttribs(void)
 {
 	if(glxvisual::visAttrib3D(config, GLX_STEREO))
 		stereo=true;

server/VirtualDrawable.h
@@ -73,7 +73,7 @@ namespace vglserver
 
 				private:
 
-					void setVisAttribs(GLXFBConfig config);
+					void setVisAttribs(void);
 
 					bool cleared, stereo;
 					GLXDrawable glxDraw;

server/VirtualWin.cpp
@@ -48,8 +48,8 @@ static inline int drawingToRight(void)
 // This class encapsulates the 3D off-screen drawable, its most recent
 // ancestor, and information specific to its corresponding X window
 
-VirtualWin::VirtualWin(Display *dpy, Window win) :
-	VirtualDrawable(dpy, win)
+VirtualWin::VirtualWin(Display *dpy_, Window win) :
+	VirtualDrawable(dpy_, win)
 {
 	eventdpy=NULL;
 	oldDraw=NULL;  newWidth=newHeight=-1;
@@ -113,11 +113,11 @@ VirtualWin::~VirtualWin(void)
 }
 
 
-int VirtualWin::init(int w, int h, GLXFBConfig config)
+int VirtualWin::init(int w, int h, GLXFBConfig config_)
 {
 	CS::SafeLock l(mutex);
 	if(doWMDelete) _throw("Window has been deleted by window manager");
-	return VirtualDrawable::init(w, h, config);
+	return VirtualDrawable::init(w, h, config_);
 }
 
 
@@ -340,7 +340,7 @@ void VirtualWin::sendPlugin(GLint drawBuf, bool spoilLast, bool sync,
 {
 	Frame f;
 	int w=oglDraw->getWidth(), h=oglDraw->getHeight();
-	RRFrame *frame=NULL;
+	RRFrame *rrframe=NULL;
 
 	if(!plugin)
 	{
@@ -363,27 +363,27 @@ void VirtualWin::sendPlugin(GLint drawBuf, bool spoilLast, bool sync,
 	if(oglDraw->getFormat()==GL_RGBA) desiredformat=RRTRANS_RGBA;
 	if(!trueColor) desiredformat=RRTRANS_INDEX;
 
-	frame=plugin->getFrame(w, h, desiredformat,
+	rrframe=plugin->getFrame(w, h, desiredformat,
 		doStereo && stereoMode==RRSTEREO_QUADBUF);
-	f.init(frame->bits, frame->w, frame->pitch, frame->h,
-		rrtrans_ps[frame->format], (rrtrans_bgr[frame->format]? FRAME_BGR:0) |
-		(rrtrans_afirst[frame->format]? FRAME_ALPHAFIRST:0) |
+	f.init(rrframe->bits, rrframe->w, rrframe->pitch, rrframe->h,
+		rrtrans_ps[rrframe->format], (rrtrans_bgr[rrframe->format]? FRAME_BGR:0) |
+		(rrtrans_afirst[rrframe->format]? FRAME_ALPHAFIRST:0) |
 		FRAME_BOTTOMUP);
-	int glformat= (rrtrans_ps[frame->format]==3? GL_RGB:GL_RGBA);
+	int glformat= (rrtrans_ps[rrframe->format]==3? GL_RGB:GL_RGBA);
 	#ifdef GL_BGR_EXT
-	if(frame->format==RRTRANS_BGR) glformat=GL_BGR_EXT;
+	if(rrframe->format==RRTRANS_BGR) glformat=GL_BGR_EXT;
 	#endif
 	#ifdef GL_BGRA_EXT
-	if(frame->format==RRTRANS_BGRA) glformat=GL_BGRA_EXT;
+	if(rrframe->format==RRTRANS_BGRA) glformat=GL_BGRA_EXT;
 	#endif
 	#ifdef GL_ABGR_EXT
 	// FIXME: Implement ARGB properly
-	if(frame->format==RRTRANS_ABGR || frame->format==RRTRANS_ARGB)
+	if(rrframe->format==RRTRANS_ABGR || rrframe->format==RRTRANS_ARGB)
 		glformat=GL_ABGR_EXT;
 	#endif
-	if(frame->format==RRTRANS_INDEX) glformat=GL_COLOR_INDEX;
+	if(rrframe->format==RRTRANS_INDEX) glformat=GL_COLOR_INDEX;
 
-	if(doStereo && stereoMode==RRSTEREO_QUADBUF && frame->rbits==NULL)
+	if(doStereo && stereoMode==RRSTEREO_QUADBUF && rrframe->rbits==NULL)
 	{
 		static bool message=false;
 		if(!message)
@@ -410,15 +410,15 @@ void VirtualWin::sendPlugin(GLint drawBuf, bool spoilLast, bool sync,
 		GLint buf=drawBuf;
 		if(doStereo || stereoMode==RRSTEREO_LEYE) buf=leye(drawBuf);
 		if(stereoMode==RRSTEREO_REYE) buf=reye(drawBuf);
-		readPixels(0, 0, frame->w, frame->pitch, frame->h, glformat,
-			rrtrans_ps[frame->format], frame->bits, buf, doStereo);
-		if(doStereo && frame->rbits)
-			readPixels(0, 0, frame->w, frame->pitch, frame->h, glformat,
-				rrtrans_ps[frame->format], frame->rbits, reye(drawBuf), doStereo);
+		readPixels(0, 0, rrframe->w, rrframe->pitch, rrframe->h, glformat,
+			rrtrans_ps[rrframe->format], rrframe->bits, buf, doStereo);
+		if(doStereo && rrframe->rbits)
+			readPixels(0, 0, rrframe->w, rrframe->pitch, rrframe->h, glformat,
+				rrtrans_ps[rrframe->format], rrframe->rbits, reye(drawBuf), doStereo);
 	}
 	if(!syncdpy) { XSync(dpy, False);  syncdpy=true; }
 	if(fconfig.logo) f.addLogo();
-	plugin->sendFrame(frame, sync);
+	plugin->sendFrame(rrframe, sync);
 }
 
 

commit 36d9e75fba2141af86c707007255e865066edab8

Fix segfault introduced by r3545 and remove unneeded class member
git-svn-id: svn+ssh://svn.code.sf.net/p/virtualgl/code/trunk@3548 799e4f7b-5fd2-41f6-823c-2ecc41bc7f0b

server/VirtualPixmap.cpp
@@ -22,7 +22,7 @@ using namespace vglcommon;
 using namespace vglserver;
 
 
-VirtualPixmap::VirtualPixmap(Display *dpy_, XVisualInfo *vis_, Pixmap pm)
+VirtualPixmap::VirtualPixmap(Display *dpy_, XVisualInfo *vis, Pixmap pm)
 	: VirtualDrawable(dpy_, pm)
 {
 	CriticalSection::SafeLock l(mutex);

server/VirtualPixmap.h
@@ -39,7 +39,6 @@ namespace vglserver
 
 			vglcommon::Profiler profPMBlit;
 			vglcommon::FBXFrame *frame;
-			XVisualInfo *vis;
 	};
 }
 