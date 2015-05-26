517a0b32f1637e554baaa5cb273b9db494a9c364
DRC <dcommander@users.sourceforge.net>
2015-05-26 23:37:25 +0000

    Add regression tests for the Viewperf/FEMFAT issue (fixed in 2.4) and the Traptester issue (fixed in 2.4.1).
    git-svn-id: svn+ssh://svn.code.sf.net/p/virtualgl/code/trunk@3636 799e4f7b-5fd2-41f6-823c-2ecc41bc7f0b

---
+++ server/fakerut-supplement.c
@@ -0,0 +1,17 @@
+/* Copyright (C)2015 D. R. Commander
+ *
+ * This library is free software and may be redistributed and/or modified under
+ * the terms of the wxWindows Library License, Version 3.1 or (at your option)
+ * any later version.  The full license is in the LICENSE.txt file included
+ * with this distribution.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * wxWindows Library License for more details.
+ */
+
+#include "glx.h"
+
+/* This will cause a crash in VirtualGL 2.4 beta1 and earlier. */
+PFNGLBINDBUFFERPROC glBindBuffer=NULL;
