d06ce499b566d7ef9c285ffc2a74a2fe5d58b8df
DRC <information@virtualgl.org>
2016-10-01 14:21:28 -0500

    Build: Fix minor regression in TurboJPEG detection
    (caused by previous commit.)  We have to always unset TJPEG_INCLUDE_DIR
    if it is specified, because otherwise the test in line 15 will never
    fail, even if TJPEG_INCLUDE_DIR points to a bogus directory.

--- cmakescripts/FindTurboJPEG.cmake
+++ cmakescripts/FindTurboJPEG.cmake
@@ -5,13 +5,15 @@ if(NOT TJPEG_INCLUDE_DIR)
 	set(TJPEG_INCLUDE_DIR_HINTS ${DEFAULT_TJPEG_INCLUDE_DIR})
 else()
 	set(TJPEG_INCLUDE_DIR_HINTS ${TJPEG_INCLUDE_DIR})
+	unset(TJPEG_INCLUDE_DIR)
+	unset(TJPEG_INCLUDE_DIR CACHE)
 endif()
 
 find_path(TJPEG_INCLUDE_DIR turbojpeg.h
 	DOC "TurboJPEG include directory (default: ${DEFAULT_TJPEG_INCLUDE_DIR})"
 	HINTS ${TJPEG_INCLUDE_DIR_HINTS})
 if(TJPEG_INCLUDE_DIR STREQUAL "TJPEG_INCLUDE_DIR-NOTFOUND")
-	message(FATAL_ERROR "Could not find turbojpeg.h in ${DEFAULT_TJPEG_INCLUDE_DIR}.  If it is installed in a different place, then set TJPEG_INCLUDE_DIR accordingly.")
+	message(FATAL_ERROR "Could not find turbojpeg.h in ${TJPEG_INCLUDE_DIR_HINTS}.  If it is installed in a different place, then set TJPEG_INCLUDE_DIR accordingly.")
 else()
 	message(STATUS "TJPEG_INCLUDE_DIR = ${TJPEG_INCLUDE_DIR}")
 endif()
