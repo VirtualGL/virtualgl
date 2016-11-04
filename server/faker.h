VirtualGL/virtualgl

commit 262e077ed9f21ebb3176e556b32821c7f8400c9c

Faker: Connect to 3D X server only when needed
This reduces the overhead of the faker when running non-OpenGL
applications.  Note also that this commit removes the crufty is3D()
macro that was previously used to guard against recursion.  Instead,
the GLX and X11 interposers now use the same technique as the XCB
interposer to guard against recursion (the faker level TLS variable.)
This should be more robust, and it has the added benefit of not
requiring the 3D X server connection to be open.

server/faker.h
@@ -29,22 +29,22 @@
 namespace vglfaker
 {
 	extern Display *dpy3D;
+	extern Display *init3D(void);
 	extern void safeExit(int);
 	extern int deadYet;
 	extern long getTraceLevel(void);
 	extern void setTraceLevel(long level);
-	#ifdef FAKEXCB
 	extern long getFakerLevel(void);
 	extern void setFakerLevel(long level);
-	#endif
 	extern bool excludeDisplay(char *name);
 	extern bool getExcludeCurrent(void);
 	extern void setExcludeCurrent(bool excludeCurrent);
 }
 
-#define _dpy3D vglfaker::dpy3D
+#define _dpy3D vglfaker::init3D()
 
-#define is3D(dpy) (vglfaker::dpy3D && dpy==vglfaker::dpy3D)
+#define isExcluded(dpy)  \
+	(vglfaker::deadYet || vglfaker::getFakerLevel()>0 || dpyhash.find(dpy))
 
 #define isFront(drawbuf) (drawbuf==GL_FRONT || drawbuf==GL_FRONT_AND_BACK \
 	|| drawbuf==GL_FRONT_LEFT || drawbuf==GL_FRONT_RIGHT || drawbuf==GL_LEFT \

server/faker-glx.cpp
@@ -1,6 +1,6 @@
 /* Copyright (C)2004 Landmark Graphics Corporation
  * Copyright (C)2005, 2006 Sun Microsystems, Inc.
- * Copyright (C)2009, 2011-2015 D. R. Commander
+ * Copyright (C)2009, 2011-2016 D. R. Commander
  *
  * This library is free software and may be redistributed and/or modified under
  * the terms of the wxWindows Library License, Version 3.1 or (at your option)
@@ -263,7 +263,7 @@ GLXFBConfig *glXChooseFBConfig(Display *dpy, int screen,
 
 	// If this is called internally from within another GLX function, then use
 	// the real function.
-	if(is3D(dpy) || dpyhash.find(dpy))
+	if(isExcluded(dpy))
 		return _glXChooseFBConfig(dpy, screen, attrib_list, nelements);
 
 	// If 'attrib_list' specifies properties for transparent overlay rendering,
@@ -377,7 +377,7 @@ XVisualInfo *glXChooseVisual(Display *dpy, int screen, int *attrib_list)
 
 	TRY();
 
-	if(is3D(dpy) || dpyhash.find(dpy))
+	if(isExcluded(dpy))
 		return _glXChooseVisual(dpy, screen, attrib_list);
 
 	// If 'attrib_list' specifies properties for transparent overlay rendering,
@@ -477,7 +477,7 @@ void glXCopyContext(Display *dpy, GLXContext src, GLXContext dst,
 {
 	TRY();
 
-	if(dpyhash.find(dpy))
+	if(isExcluded(dpy))
 		return _glXCopyContext(dpy, src, dst, mask);
 
 	bool srcOverlay=false, dstOverlay=false;
@@ -504,7 +504,7 @@ GLXContext glXCreateContext(Display *dpy, XVisualInfo *vis,
 
 	TRY();
 
-	if(is3D(dpy) || dpyhash.find(dpy))
+	if(isExcluded(dpy))
 		return _glXCreateContext(dpy, vis, share_list, direct);
 
 	if(!fconfig.allowindirect) direct=True;
@@ -568,7 +568,7 @@ GLXContext glXCreateContextAttribsARB(Display *dpy, GLXFBConfig config,
 
 	TRY();
 
-	if(is3D(dpy) || dpyhash.find(dpy))
+	if(isExcluded(dpy))
 		return _glXCreateContextAttribsARB(dpy, config, share_context, direct,
 			attribs);
 
@@ -623,7 +623,7 @@ GLXContext glXCreateNewContext(Display *dpy, GLXFBConfig config,
 
 	TRY();
 
-	if(is3D(dpy) || dpyhash.find(dpy))
+	if(isExcluded(dpy))
 		return _glXCreateNewContext(dpy, config, render_type, share_list, direct);
 
 	if(!fconfig.allowindirect) direct=True;
@@ -684,7 +684,7 @@ GLXPbuffer glXCreatePbuffer(Display *dpy, GLXFBConfig config,
 
 	TRY();
 
-	if(dpyhash.find(dpy))
+	if(isExcluded(dpy))
 		return _glXCreatePbuffer(dpy, config, attrib_list);
 
 		opentrace(glXCreatePbuffer);  prargd(dpy);  prargc(config);
@@ -728,7 +728,7 @@ GLXPixmap glXCreateGLXPixmap(Display *dpy, XVisualInfo *vis, Pixmap pm)
 
 	TRY();
 
-	if(is3D(dpy) || dpyhash.find(dpy))
+	if(isExcluded(dpy))
 		return _glXCreateGLXPixmap(dpy, vis, pm);
 
 	// Not sure whether a transparent pixmap has any meaning, but in any case,
@@ -782,7 +782,7 @@ GLXPixmap glXCreatePixmap(Display *dpy, GLXFBConfig config, Pixmap pm,
 
 	TRY();
 
-	if(is3D(dpy) || dpyhash.find(dpy) || rcfghash.isOverlay(dpy, config))
+	if(isExcluded(dpy) || rcfghash.isOverlay(dpy, config))
 		return _glXCreatePixmap(dpy, config, pm, attribs);
 
 		opentrace(glXCreatePixmap);  prargd(dpy);  prargc(config);  prargx(pm);
@@ -830,7 +830,7 @@ GLXWindow glXCreateWindow(Display *dpy, GLXFBConfig config, Window win,
 
 	TRY();
 
-	if(is3D(dpy) || dpyhash.find(dpy))
+	if(isExcluded(dpy))
 		return _glXCreateWindow(dpy, config, win, attrib_list);
 
 	// Overlay config.  Hand off to 2D X server.
@@ -849,11 +849,8 @@ GLXWindow glXCreateWindow(Display *dpy, GLXFBConfig config, Window win,
 	if(!vw && !glxdhash.getCurrentDisplay(win))
 	{
 		// Apparently win was created in another process or using XCB.
-		if(!is3D(dpy))
-		{
-			winhash.add(dpy, win);
-			vw=winhash.initVW(dpy, win, config);
-		}
+		winhash.add(dpy, win);
+		vw=winhash.initVW(dpy, win, config);
 	}
 	if(!vw)
 		_throw("Cannot create virtual window for specified X window");
@@ -872,7 +869,7 @@ void glXDestroyContext(Display* dpy, GLXContext ctx)
 {
 	TRY();
 
-	if(ctxhash.isOverlay(ctx) || dpyhash.find(dpy))
+	if(isExcluded(dpy) || ctxhash.isOverlay(ctx))
 	{
 		_glXDestroyContext(dpy, ctx);  return;
 	}
@@ -895,7 +892,7 @@ void glXDestroyPbuffer(Display *dpy, GLXPbuffer pbuf)
 {
 	TRY();
 
-	if(dpyhash.find(dpy)) { _glXDestroyPbuffer(dpy, pbuf);  return; }
+	if(isExcluded(dpy)) { _glXDestroyPbuffer(dpy, pbuf);  return; }
 
 		opentrace(glXDestroyPbuffer);  prargd(dpy);  prargx(pbuf);  starttrace();
 
@@ -921,7 +918,7 @@ void glXDestroyGLXPixmap(Display *dpy, GLXPixmap pix)
 {
 	TRY();
 
-	if(is3D(dpy) || dpyhash.find(dpy))
+	if(isExcluded(dpy))
 	{
 		_glXDestroyGLXPixmap(dpy, pix);  return;
 	}
@@ -944,7 +941,7 @@ void glXDestroyPixmap(Display *dpy, GLXPixmap pix)
 {
 	TRY();
 
-	if(is3D(dpy) || dpyhash.find(dpy))
+	if(isExcluded(dpy))
 	{
 		_glXDestroyPixmap(dpy, pix);  return;
 	}
@@ -970,7 +967,7 @@ void glXDestroyWindow(Display *dpy, GLXWindow win)
 {
 	TRY();
 
-	if(is3D(dpy) || dpyhash.find(dpy) || winhash.isOverlay(dpy, win))
+	if(isExcluded(dpy) || winhash.isOverlay(dpy, win))
 	{
 		_glXDestroyWindow(dpy, win);  return;
 	}
@@ -992,7 +989,7 @@ void glXFreeContextEXT(Display *dpy, GLXContext ctx)
 {
 	TRY();
 
-	if(dpyhash.find(dpy) || ctxhash.isOverlay(ctx))
+	if(isExcluded(dpy) || ctxhash.isOverlay(ctx))
 	{
 		_glXFreeContextEXT(dpy, ctx);  return;
 	}
@@ -1063,7 +1060,7 @@ const char *glXGetClientString(Display *dpy, int name)
 {
 	TRY();
 
-	if(is3D(dpy) || dpyhash.find(dpy))
+	if(isExcluded(dpy))
 		return _glXGetClientString(dpy, name);
 
 	if(name==GLX_EXTENSIONS) return getGLXExtensions();
@@ -1088,7 +1085,7 @@ int glXGetConfig(Display *dpy, XVisualInfo *vis, int attrib, int *value)
 
 	TRY();
 
-	if(is3D(dpy) || dpyhash.find(dpy))
+	if(isExcluded(dpy))
 		return _glXGetConfig(dpy, vis, attrib, value);
 
 	if(!dpy || !vis || !value)
@@ -1232,7 +1229,7 @@ int glXGetFBConfigAttrib(Display *dpy, GLXFBConfig config, int attribute,
 
 	TRY();
 
-	if(is3D(dpy) || dpyhash.find(dpy) || rcfghash.isOverlay(dpy, config))
+	if(isExcluded(dpy) || rcfghash.isOverlay(dpy, config))
 		return _glXGetFBConfigAttrib(dpy, config, attribute, value);
 
 	int screen=dpy? DefaultScreen(dpy):0;
@@ -1301,7 +1298,7 @@ GLXFBConfigSGIX glXGetFBConfigFromVisualSGIX(Display *dpy, XVisualInfo *vis)
 {
 	TRY();
 
-	if(dpyhash.find(dpy))
+	if(isExcluded(dpy))
 		return _glXGetFBConfigFromVisualSGIX(dpy, vis);
 	else return matchConfig(dpy, vis);
 
@@ -1318,7 +1315,7 @@ GLXFBConfig *glXGetFBConfigs(Display *dpy, int screen, int *nelements)
 
 	TRY();
 
-	if(dpyhash.find(dpy))
+	if(isExcluded(dpy))
 		return _glXGetFBConfigs(dpy, screen, nelements);
 
 		opentrace(glXGetFBConfigs);  prargd(dpy);  prargi(screen);
@@ -1346,7 +1343,7 @@ void glXBindTexImageEXT(Display *dpy, GLXDrawable drawable, int buffer,
 {
 	TRY();
 
-	if(dpyhash.find(dpy))
+	if(isExcluded(dpy))
 		return _glXBindTexImageEXT(dpy, drawable, buffer, attrib_list);
 
 		opentrace(glXBindTexImageEXT);  prargd(dpy);  prargx(drawable);
@@ -1387,7 +1384,7 @@ void glXReleaseTexImageEXT(Display *dpy, GLXDrawable drawable, int buffer)
 {
 	TRY();
 
-	if(dpyhash.find(dpy))
+	if(isExcluded(dpy))
 		return _glXReleaseTexImageEXT(dpy, drawable, buffer);
 
 		opentrace(glXReleaseTexImageEXT);  prargd(dpy);  prargx(drawable);
@@ -1561,7 +1558,7 @@ void glXGetSelectedEvent(Display *dpy, GLXDrawable draw,
 {
 	TRY();
 
-	if(dpyhash.find(dpy) || winhash.isOverlay(dpy, draw))
+	if(isExcluded(dpy) || winhash.isOverlay(dpy, draw))
 		return _glXGetSelectedEvent(dpy, draw, event_mask);
 
 	_glXGetSelectedEvent(_dpy3D, ServerDrawable(dpy, draw), event_mask);
@@ -1586,7 +1583,7 @@ XVisualInfo *glXGetVisualFromFBConfig(Display *dpy, GLXFBConfig config)
 
 	TRY();
 
-	if(is3D(dpy) || dpyhash.find(dpy) || rcfghash.isOverlay(dpy, config))
+	if(isExcluded(dpy) || rcfghash.isOverlay(dpy, config))
 		return _glXGetVisualFromFBConfig(dpy, config);
 
 		opentrace(glXGetVisualFromFBConfig);  prargd(dpy);  prargc(config);
@@ -1621,7 +1618,7 @@ GLXContext glXImportContextEXT(Display *dpy, GLXContextID contextID)
 {
 	TRY();
 
-	if(dpyhash.find(dpy))
+	if(isExcluded(dpy))
 		return _glXImportContextEXT(dpy, contextID);
 
 	return _glXImportContextEXT(_dpy3D, contextID);
@@ -1640,7 +1637,7 @@ Bool glXIsDirect(Display *dpy, GLXContext ctx)
 
 	TRY();
 
-	if(dpyhash.find(dpy) || ctxhash.isOverlay(ctx))
+	if(isExcluded(dpy) || ctxhash.isOverlay(ctx))
 		return _glXIsDirect(dpy, ctx);
 
 		opentrace(glXIsDirect);  prargd(dpy);  prargx(ctx);
@@ -1662,7 +1659,8 @@ Bool glXMakeCurrent(Display *dpy, GLXDrawable drawable, GLXContext ctx)
 	Bool retval=False;  const char *renderer="Unknown";
 	VirtualWin *vw;  GLXFBConfig config=0;
 
-	if(is3D(dpy)) return _glXMakeCurrent(dpy, drawable, ctx);
+	if(vglfaker::deadYet || vglfaker::getFakerLevel()>0)
+		return _glXMakeCurrent(dpy, drawable, ctx);
 
 	TRY();
 
@@ -1723,15 +1721,12 @@ Bool glXMakeCurrent(Display *dpy, GLXDrawable drawable, GLXContext ctx)
 		{
 			// Apparently it isn't a Pbuffer or a Pixmap, so it must be a window
 			// that was created in another process.
-			if(!is3D(dpy))
+			winhash.add(dpy, drawable);
+			vw=winhash.initVW(dpy, drawable, config);
+			if(vw)
 			{
-				winhash.add(dpy, drawable);
-				vw=winhash.initVW(dpy, drawable, config);
-				if(vw)
-				{
-					drawable=vw->updateGLXDrawable();
-					vw->setDirect(direct);
-				}
+				drawable=vw->updateGLXDrawable();
+				vw->setDirect(direct);
 			}
 		}
 	}
@@ -1763,7 +1758,8 @@ Bool glXMakeContextCurrent(Display *dpy, GLXDrawable draw, GLXDrawable read,
 	Bool retval=False;  const char *renderer="Unknown";
 	VirtualWin *vw;  GLXFBConfig config=0;
 
-	if(is3D(dpy)) return _glXMakeContextCurrent(dpy, draw, read, ctx);
+	if(vglfaker::deadYet || vglfaker::getFakerLevel()>0)
+		return _glXMakeContextCurrent(dpy, draw, read, ctx);
 
 	TRY();
 
@@ -1825,15 +1821,12 @@ Bool glXMakeContextCurrent(Display *dpy, GLXDrawable draw, GLXDrawable read,
 		{
 			// Apparently it isn't a Pbuffer or a Pixmap, so it must be a window
 			// that was created in another process.
-			if(!is3D(dpy))
+			winhash.add(dpy, draw);
+			drawVW=winhash.initVW(dpy, draw, config);
+			if(drawVW)
 			{
-				winhash.add(dpy, draw);
-				drawVW=winhash.initVW(dpy, draw, config);
-				if(drawVW)
-				{
-					draw=drawVW->updateGLXDrawable();
-					drawVW->setDirect(direct);
-				}
+				draw=drawVW->updateGLXDrawable();
+				drawVW->setDirect(direct);
 			}
 		}
 
@@ -1848,15 +1841,12 @@ Bool glXMakeContextCurrent(Display *dpy, GLXDrawable draw, GLXDrawable read,
 		{
 			// Apparently it isn't a Pbuffer or a Pixmap, so it must be a window
 			// that was created in another process.
-			if(!is3D(dpy))
+			winhash.add(dpy, read);
+			readVW=winhash.initVW(dpy, read, config);
+			if(readVW)
 			{
-				winhash.add(dpy, read);
-				readVW=winhash.initVW(dpy, read, config);
-				if(readVW)
-				{
-					read=readVW->updateGLXDrawable();
-					readVW->setDirect(direct);
-				}
+				read=readVW->updateGLXDrawable();
+				readVW->setDirect(direct);
 			}
 		}
 	}
@@ -1896,7 +1886,7 @@ int glXQueryContext(Display *dpy, GLXContext ctx, int attribute, int *value)
 
 	TRY();
 
-	if(dpyhash.find(dpy) || ctxhash.isOverlay(ctx))
+	if(isExcluded(dpy) || ctxhash.isOverlay(ctx))
 		return _glXQueryContext(dpy, ctx, attribute, value);
 
 		opentrace(glXQueryContext);  prargd(dpy);  prargx(ctx);  prargix(attribute);
@@ -1918,7 +1908,7 @@ int glXQueryContextInfoEXT(Display *dpy, GLXContext ctx, int attribute,
 
 	TRY();
 
-	if(dpyhash.find(dpy) || ctxhash.isOverlay(ctx))
+	if(isExcluded(dpy) || ctxhash.isOverlay(ctx))
 		return _glXQueryContextInfoEXT(dpy, ctx, attribute, value);
 
 		opentrace(glXQueryContextInfoEXT);  prargd(dpy);  prargx(ctx);
@@ -1942,7 +1932,7 @@ void glXQueryDrawable(Display *dpy, GLXDrawable draw, int attribute,
 {
 	TRY();
 
-	if(dpyhash.find(dpy) || winhash.isOverlay(dpy, draw))
+	if(isExcluded(dpy) || winhash.isOverlay(dpy, draw))
 	{
 		_glXQueryDrawable(dpy, draw, attribute, value);
 		return;
@@ -1990,7 +1980,7 @@ Bool glXQueryExtension(Display *dpy, int *error_base, int *event_base)
 {
 	TRY();
 
-	if(dpyhash.find(dpy))
+	if(isExcluded(dpy))
 		return _glXQueryExtension(dpy, error_base, event_base);
 
 	return _glXQueryExtension(_dpy3D, error_base, event_base);
@@ -2006,7 +1996,7 @@ const char *glXQueryExtensionsString(Display *dpy, int screen)
 {
 	TRY();
 
-	if(is3D(dpy) || dpyhash.find(dpy))
+	if(isExcluded(dpy))
 		return _glXQueryExtensionsString(dpy, screen);
 
 	return getGLXExtensions();
@@ -2022,7 +2012,7 @@ const char *glXQueryServerString(Display *dpy, int screen, int name)
 {
 	TRY();
 
-	if(is3D(dpy) || dpyhash.find(dpy))
+	if(isExcluded(dpy))
 		return _glXQueryServerString(dpy, screen, name);
 
 	if(name==GLX_EXTENSIONS) return getGLXExtensions();
@@ -2044,7 +2034,7 @@ Bool glXQueryVersion(Display *dpy, int *major, int *minor)
 {
 	TRY();
 
-	if(dpyhash.find(dpy))
+	if(isExcluded(dpy))
 		return _glXQueryVersion(dpy, major, minor);
 
 	return _glXQueryVersion(_dpy3D, major, minor);
@@ -2061,7 +2051,7 @@ void glXSelectEvent(Display *dpy, GLXDrawable draw, unsigned long event_mask)
 {
 	TRY();
 
-	if(dpyhash.find(dpy) || winhash.isOverlay(dpy, draw))
+	if(isExcluded(dpy) || winhash.isOverlay(dpy, draw))
 		return _glXSelectEvent(dpy, draw, event_mask);
 
 	_glXSelectEvent(_dpy3D, ServerDrawable(dpy, draw), event_mask);
@@ -2086,7 +2076,7 @@ void glXSwapBuffers(Display* dpy, GLXDrawable drawable)
 
 	TRY();
 
-	if(dpyhash.find(dpy) || winhash.isOverlay(dpy, drawable))
+	if(isExcluded(dpy) || winhash.isOverlay(dpy, drawable))
 	{
 		_glXSwapBuffers(dpy, drawable);
 		return;
@@ -2095,7 +2085,7 @@ void glXSwapBuffers(Display* dpy, GLXDrawable drawable)
 		opentrace(glXSwapBuffers);  prargd(dpy);  prargx(drawable);  starttrace();
 
 	fconfig.flushdelay=0.;
-	if(!is3D(dpy) && winhash.find(dpy, drawable, vw))
+	if(winhash.find(dpy, drawable, vw))
 	{
 		vw->readback(GL_BACK, false, fconfig.sync);
 		vw->swapBuffers();
@@ -2121,7 +2111,7 @@ void glXSwapBuffers(Display* dpy, GLXDrawable drawable)
 	}
 	else _glXSwapBuffers(_dpy3D, drawable);
 
-		stoptrace();  if(!is3D(dpy) && vw) { prargx(vw->getGLXDrawable()); }
+		stoptrace();  if(vw) { prargx(vw->getGLXDrawable()); }
 		closetrace();
 
 	CATCH();
@@ -2139,7 +2129,7 @@ int glXGetTransparentIndexSUN(Display *dpy, Window overlay,
 
 	TRY();
 
-	if(dpyhash.find(dpy))
+	if(isExcluded(dpy))
 		return _glXGetTransparentIndexSUN(dpy, overlay, underlay,
 			transparentIndex);
 
@@ -2173,7 +2163,7 @@ Bool glXJoinSwapGroupNV(Display *dpy, GLXDrawable drawable, GLuint group)
 {
 	TRY();
 
-	if(dpyhash.find(dpy))
+	if(isExcluded(dpy))
 		return _glXJoinSwapGroupNV(dpy, drawable, group);
 
 	return _glXJoinSwapGroupNV(_dpy3D, ServerDrawable(dpy, drawable), group);
@@ -2189,7 +2179,7 @@ Bool glXBindSwapBarrierNV(Display *dpy, GLuint group, GLuint barrier)
 {
 	TRY();
 
-	if(dpyhash.find(dpy))
+	if(isExcluded(dpy))
 		return _glXBindSwapBarrierNV(dpy, group, barrier);
 
 	return _glXBindSwapBarrierNV(_dpy3D, group, barrier);
@@ -2207,7 +2197,7 @@ Bool glXQuerySwapGroupNV(Display *dpy, GLXDrawable drawable, GLuint *group,
 {
 	TRY();
 
-	if(dpyhash.find(dpy))
+	if(isExcluded(dpy))
 		return _glXQuerySwapGroupNV(dpy, drawable, group, barrier);
 
 	return _glXQuerySwapGroupNV(_dpy3D, ServerDrawable(dpy, drawable), group,
@@ -2225,7 +2215,7 @@ Bool glXQueryMaxSwapGroupsNV(Display *dpy, int screen, GLuint *maxGroups,
 {
 	TRY();
 
-	if(dpyhash.find(dpy))
+	if(isExcluded(dpy))
 		return _glXQueryMaxSwapGroupsNV(dpy, screen, maxGroups, maxBarriers);
 
 	return _glXQueryMaxSwapGroupsNV(_dpy3D, DefaultScreen(_dpy3D),
@@ -2242,7 +2232,7 @@ Bool glXQueryFrameCountNV(Display *dpy, int screen, GLuint *count)
 {
 	TRY();
 
-	if(dpyhash.find(dpy))
+	if(isExcluded(dpy))
 		return _glXQueryFrameCountNV(dpy, screen, count);
 
 	return _glXQueryFrameCountNV(_dpy3D, DefaultScreen(_dpy3D), count);
@@ -2258,7 +2248,7 @@ Bool glXResetFrameCountNV(Display *dpy, int screen)
 {
 	TRY();
 
-	if(dpyhash.find(dpy))
+	if(isExcluded(dpy))
 		return _glXResetFrameCountNV(dpy, screen);
 
 	return _glXResetFrameCountNV(_dpy3D, DefaultScreen(_dpy3D));
@@ -2278,7 +2268,7 @@ void glXSwapIntervalEXT(Display *dpy, GLXDrawable drawable, int interval)
 	TRY();
 
 	// If drawable is an overlay, hand off to the 2D X Server
-	if(dpyhash.find(dpy) || winhash.isOverlay(dpy, drawable))
+	if(isExcluded(dpy) || winhash.isOverlay(dpy, drawable))
 	{
 		_glXSwapIntervalEXT(dpy, drawable, interval);
 		return;

server/faker-sym.h
@@ -45,10 +45,8 @@ namespace vglfaker
 {
 	extern void safeExit(int);
 	extern void init(void);
-	#ifdef FAKEXCB
 	extern long getFakerLevel(void);
 	extern void setFakerLevel(long level);
-	#endif
 
 	void *loadSymbol(const char *name, bool optional=false);
 	void unloadSymbols(void);
@@ -85,13 +83,8 @@ namespace vglfaker
 #endif
 
 
-#ifdef FAKEXCB
-#define DISABLE_FAKEXCB() vglfaker::setFakerLevel(vglfaker::getFakerLevel()+1);
-#define ENABLE_FAKEXCB()  vglfaker::setFakerLevel(vglfaker::getFakerLevel()-1);
-#else
-#define DISABLE_FAKEXCB()
-#define ENABLE_FAKEXCB()
-#endif
+#define DISABLE_FAKER() vglfaker::setFakerLevel(vglfaker::getFakerLevel()+1);
+#define ENABLE_FAKER()  vglfaker::setFakerLevel(vglfaker::getFakerLevel()-1);
 
 
 #define FUNCDEF0(RetType, f, fake_f) \
@@ -100,9 +93,9 @@ namespace vglfaker
 	static inline RetType _##f(void) { \
 		RetType retval; \
 		CHECKSYM(f, fake_f); \
-		DISABLE_FAKEXCB(); \
+		DISABLE_FAKER(); \
 		retval=__##f(); \
-		ENABLE_FAKEXCB(); \
+		ENABLE_FAKER(); \
 		return retval; \
 	}
 
@@ -111,9 +104,9 @@ namespace vglfaker
 	SYMDEF(f); \
 	static inline void _##f(void) { \
 		CHECKSYM(f, fake_f); \
-		DISABLE_FAKEXCB(); \
+		DISABLE_FAKER(); \
 		__##f(); \
-		ENABLE_FAKEXCB(); \
+		ENABLE_FAKER(); \
 	}
 
 #define FUNCDEF1(RetType, f, at1, a1, fake_f) \
@@ -122,9 +115,9 @@ namespace vglfaker
 	static inline RetType _##f(at1 a1) { \
 		RetType retval; \
 		CHECKSYM(f, fake_f); \
-		DISABLE_FAKEXCB(); \
+		DISABLE_FAKER(); \
 		retval=__##f(a1); \
-		ENABLE_FAKEXCB(); \
+		ENABLE_FAKER(); \
 		return retval; \
 	}
 
@@ -133,9 +126,9 @@ namespace vglfaker
 	SYMDEF(f); \
 	static inline void _##f(at1 a1) { \
 		CHECKSYM(f, fake_f); \
-		DISABLE_FAKEXCB(); \
+		DISABLE_FAKER(); \
 		__##f(a1); \
-		ENABLE_FAKEXCB(); \
+		ENABLE_FAKER(); \
 	}
 
 #define FUNCDEF2(RetType, f, at1, a1, at2, a2, fake_f) \
@@ -144,9 +137,9 @@ namespace vglfaker
 	static inline RetType _##f(at1 a1, at2 a2) { \
 		RetType retval; \
 		CHECKSYM(f, fake_f); \
-		DISABLE_FAKEXCB(); \
+		DISABLE_FAKER(); \
 		retval=__##f(a1, a2); \
-		ENABLE_FAKEXCB(); \
+		ENABLE_FAKER(); \
 		return retval; \
 	}
 
@@ -155,9 +148,9 @@ namespace vglfaker
 	SYMDEF(f); \
 	static inline void _##f(at1 a1, at2 a2) { \
 		CHECKSYM(f, fake_f); \
-		DISABLE_FAKEXCB(); \
+		DISABLE_FAKER(); \
 		__##f(a1, a2); \
-		ENABLE_FAKEXCB(); \
+		ENABLE_FAKER(); \
 	}
 
 #define FUNCDEF3(RetType, f, at1, a1, at2, a2, at3, a3, fake_f) \
@@ -166,9 +159,9 @@ namespace vglfaker
 	static inline RetType _##f(at1 a1, at2 a2, at3 a3) { \
 		RetType retval; \
 		CHECKSYM(f, fake_f); \
-		DISABLE_FAKEXCB(); \
+		DISABLE_FAKER(); \
 		retval=__##f(a1, a2, a3); \
-		ENABLE_FAKEXCB(); \
+		ENABLE_FAKER(); \
 		return retval; \
 	}
 
@@ -177,9 +170,9 @@ namespace vglfaker
 	SYMDEF(f); \
 	static inline void _##f(at1 a1, at2 a2, at3 a3) { \
 		CHECKSYM(f, fake_f); \
-		DISABLE_FAKEXCB(); \
+		DISABLE_FAKER(); \
 		__##f(a1, a2, a3); \
-		ENABLE_FAKEXCB(); \
+		ENABLE_FAKER(); \
 	}
 
 #define FUNCDEF4(RetType, f, at1, a1, at2, a2, at3, a3, at4, a4, fake_f) \
@@ -188,9 +181,9 @@ namespace vglfaker
 	static inline RetType _##f(at1 a1, at2 a2, at3 a3, at4 a4) { \
 		RetType retval; \
 		CHECKSYM(f, fake_f); \
-		DISABLE_FAKEXCB(); \
+		DISABLE_FAKER(); \
 		retval=__##f(a1, a2, a3, a4); \
-		ENABLE_FAKEXCB(); \
+		ENABLE_FAKER(); \
 		return retval; \
 	}
 
@@ -199,9 +192,9 @@ namespace vglfaker
 	SYMDEF(f); \
 	static inline void _##f(at1 a1, at2 a2, at3 a3, at4 a4) { \
 		CHECKSYM(f, fake_f); \
-		DISABLE_FAKEXCB(); \
+		DISABLE_FAKER(); \
 		__##f(a1, a2, a3, a4); \
-		ENABLE_FAKEXCB(); \
+		ENABLE_FAKER(); \
 	}
 
 #define FUNCDEF5(RetType, f, at1, a1, at2, a2, at3, a3, at4, a4, at5, a5, \
@@ -211,9 +204,9 @@ namespace vglfaker
 	static inline RetType _##f(at1 a1, at2 a2, at3 a3, at4 a4, at5 a5) { \
 		RetType retval; \
 		CHECKSYM(f, fake_f); \
-		DISABLE_FAKEXCB(); \
+		DISABLE_FAKER(); \
 		retval=__##f(a1, a2, a3, a4, a5); \
-		ENABLE_FAKEXCB(); \
+		ENABLE_FAKER(); \
 		return retval; \
 	}
 
@@ -222,9 +215,9 @@ namespace vglfaker
 	SYMDEF(f); \
 	static inline void _##f(at1 a1, at2 a2, at3 a3, at4 a4, at5 a5) { \
 		CHECKSYM(f, fake_f); \
-		DISABLE_FAKEXCB(); \
+		DISABLE_FAKER(); \
 		__##f(a1, a2, a3, a4, a5); \
-		ENABLE_FAKEXCB(); \
+		ENABLE_FAKER(); \
 	}
 
 #define VFUNCDEF6(f, at1, a1, at2, a2, at3, a3, at4, a4, at5, a5, at6, a6, \
@@ -233,9 +226,9 @@ namespace vglfaker
 	SYMDEF(f); \
 	static inline void _##f(at1 a1, at2 a2, at3 a3, at4 a4, at5 a5, at6 a6) { \
 		CHECKSYM(f, fake_f); \
-		DISABLE_FAKEXCB(); \
+		DISABLE_FAKER(); \
 		__##f(a1, a2, a3, a4, a5, a6); \
-		ENABLE_FAKEXCB(); \
+		ENABLE_FAKER(); \
 	}
 
 #define FUNCDEF6(RetType, f, at1, a1, at2, a2, at3, a3, at4, a4, at5, a5, \
@@ -246,9 +239,9 @@ namespace vglfaker
 		at6 a6) { \
 		RetType retval; \
 		CHECKSYM(f, fake_f); \
-		DISABLE_FAKEXCB(); \
+		DISABLE_FAKER(); \
 		retval=__##f(a1, a2, a3, a4, a5, a6); \
-		ENABLE_FAKEXCB(); \
+		ENABLE_FAKER(); \
 		return retval; \
 	}
 
@@ -259,9 +252,9 @@ namespace vglfaker
 	static inline void _##f(at1 a1, at2 a2, at3 a3, at4 a4, at5 a5, at6 a6, \
 		at7 a7) { \
 		CHECKSYM(f, fake_f); \
-		DISABLE_FAKEXCB(); \
+		DISABLE_FAKER(); \
 		__##f(a1, a2, a3, a4, a5, a6, a7); \
-		ENABLE_FAKEXCB(); \
+		ENABLE_FAKER(); \
 	}
 
 #define FUNCDEF8(RetType, f, at1, a1, at2, a2, at3, a3, at4, a4, at5, a5, \
@@ -272,9 +265,9 @@ namespace vglfaker
 		at7 a7, at8 a8) { \
 		RetType retval; \
 		CHECKSYM(f, fake_f); \
-		DISABLE_FAKEXCB(); \
+		DISABLE_FAKER(); \
 		retval=__##f(a1, a2, a3, a4, a5, a6, a7, a8); \
-		ENABLE_FAKEXCB(); \
+		ENABLE_FAKER(); \
 		return retval; \
 	}
 
@@ -286,9 +279,9 @@ namespace vglfaker
 		at7 a7, at8 a8, at9 a9) { \
 		RetType retval; \
 		CHECKSYM(f, fake_f); \
-		DISABLE_FAKEXCB(); \
+		DISABLE_FAKER(); \
 		retval=__##f(a1, a2, a3, a4, a5, a6, a7, a8, a9); \
-		ENABLE_FAKEXCB(); \
+		ENABLE_FAKER(); \
 		return retval; \
 	}
 
@@ -301,9 +294,9 @@ namespace vglfaker
 		at7 a7, at8 a8, at9 a9, at10 a10) { \
 		RetType retval; \
 		CHECKSYM(f, fake_f); \
-		DISABLE_FAKEXCB(); \
+		DISABLE_FAKER(); \
 		retval=__##f(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10); \
-		ENABLE_FAKEXCB(); \
+		ENABLE_FAKER(); \
 		return retval; \
 	}
 
@@ -317,9 +310,9 @@ namespace vglfaker
 		at7 a7, at8 a8, at9 a9, at10 a10, at11 a11, at12 a12) { \
 		RetType retval; \
 		CHECKSYM(f, fake_f); \
-		DISABLE_FAKEXCB(); \
+		DISABLE_FAKER(); \
 		retval=__##f(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12); \
-		ENABLE_FAKEXCB(); \
+		ENABLE_FAKER(); \
 		return retval; \
 	}
 

commit 847335228d11058449e8ebebeeefb036445bff5c

Faker: Correct type mismatch of deadYet variable
deadYet was made a bool in 7af4f87a066135aa4acff734a494dce3fbebafba,
and the extern reference to it should have been changed to a bool as
well in a7a910167b6c4b99252c3308aaa706f115115cb0.  This bug escaped
detection until recently, but for reasons not yet fully understood, the
32-bit optimizer in recent versions of GCC caused the bug to surface in
unpredictable ways.  Under certain circumstances, the extern int value
of deadYet would take on a value other than 0 or 1, which caused a false
positive in sections of the code that check this value.  More
specifically, I observed that the changes made in
262e077ed9f21ebb3176e556b32821c7f8400c9c (on-demand connections to the
3D X server) caused this problem to surface with a 32-bit build of
VirtualGL built with GCC 6.2.  The result was that VirtualGL's
interposed version of XQueryExtension() sometimes erroneously passed
through to the "real" XQueryExtension() function, and this caused libGL
(nVidia) to believe that GLX was not supported by the X server
(TurboVNC, which lacks a GLX extension.)  Others have reported that this
same bug caused Steam to crash, although I wasn't able to reproduce
that.

Long and the short of it-- the mismatch between deadYet types across
different execution units led to unpredictable faker behavior in some
cases.

Fixes #31, Closes #33

server/faker.h
@@ -31,7 +31,7 @@ namespace vglfaker
 	extern Display *dpy3D;
 	extern Display *init3D(void);
 	extern void safeExit(int);
-	extern int deadYet;
+	extern bool deadYet;
 	extern long getTraceLevel(void);
 	extern void setTraceLevel(long level);
 	extern long getFakerLevel(void);