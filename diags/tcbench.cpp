VirtualGL/virtualgl

commit 7bf2449084dd3b7a365eda28d32598d588959f29

TCBench: Port auto mouse movement feature to Un*x
Refer to 877132dc1f71b0239d3f3405a9de62ea31eac14c.

diags/tcbench.cpp
@@ -1,6 +1,6 @@
 /* Copyright (C)2004 Landmark Graphics Corporation
  * Copyright (C)2006-2007 Sun Microsystems, Inc.
- * Copyright (C)2011, 2013-2014, 2016 D. R. Commander
+ * Copyright (C)2011, 2013-2014, 2016-2017 D. R. Commander
  *
  * This library is free software and may be redistributed and/or modified under
  * the terms of the wxWindows Library License, Version 3.1 or (at your option)
@@ -20,7 +20,6 @@
 #include "vglutil.h"
 #include "Timer.h"
 #include "fbx.h"
-
 #ifndef _WIN32
 extern "C" {
 #define NeedFunctionPrototypes 1
@@ -29,6 +28,7 @@ extern "C" {
 #include <sys/signal.h>
 #include <sys/time.h>
 #include <sys/resource.h>
+#include <X11/extensions/XTest.h>
 #endif
 
 using namespace vglutil;
@@ -83,6 +83,32 @@ char *program_name;
 		_throww32("Could not inject mouse events");  \
 }
 
+#else
+
+Bool deadYet = False;
+
+void handler(int type)
+{
+	deadYet=true;
+}
+
+#define MOVE_MOUSE() {  \
+	XTestFakeMotionEvent(wh.dpy, DefaultScreen(wh.dpy), currentX, currentY,  \
+		CurrentTime);  \
+	if(moveX>0) {  \
+		currentX+=incX;  \
+		if(currentX<=rootx+16-moveX ||  \
+			currentX>=rootx+16+moveX)  \
+			incX=-incX;  \
+	}  \
+	if(moveY>0) {  \
+		currentY+=incY;  \
+		if(currentY<=rooty+16-moveY ||  \
+			currentY>=rooty+16+moveY)  \
+			incY=-incY;  \
+	}  \
+}
+
 #endif
 
 
@@ -92,7 +118,6 @@ void usage(void)
 	printf("USAGE: %s [options]\n\n", program_name);
 	printf("Options:\n");
 	printf("-h or -? = This help screen\n");
-	#ifdef _WIN32
 	printf("-lb = Simulate holding down the left mouse button while the benchmark is\n");
 	printf("      running\n");
 	printf("-mb = Simulate holding down the middle mouse button while the benchmark is\n");
@@ -105,7 +130,6 @@ void usage(void)
 	printf("          relative to the center of the sampling block)\n");
 	printf("-rb = Simulate holding down the right mouse button while the benchmark is\n");
 	printf("      running\n");
-	#endif
 	printf("-s <s> = Sample the window <s> times per second (default: %d)\n",
 		DEFSAMPLERATE);
 	printf("-t <t> = Run the benchmark for <t> seconds (default: %.1f)\n",
@@ -126,9 +150,12 @@ int main(int argc, char **argv)
 	#ifdef _WIN32
 	INPUT inputs[4];
 	RECT winRect;
-	int moveX=0, moveY=0, incX=-1, incY=-1, currentX, currentY;
 	DWORD downFlags=0, upFlags=0;
+	#else
+	unsigned int button=0;
+	int rootx, rooty;
 	#endif
+	int moveX=0, moveY=0, incX=-1, incY=-1, currentX=0, currentY=0;
 
 	program_name=argv[0];
 	memset(&wh, 0, sizeof(wh));
@@ -164,7 +191,6 @@ int main(int argc, char **argv)
 			if(sscanf(argv[++i], "%x", &temp)==1) wh.d=(Drawable)temp;
 			#endif
 		}
-		#ifdef _WIN32
 		if(!stricmp(argv[i], "-mx") && i<argc-1)
 		{
 			int temp=0;
@@ -177,20 +203,31 @@ int main(int argc, char **argv)
 		}
 		if(!stricmp(argv[i], "-lb"))
 		{
+			#ifdef _WIN32
 			downFlags|=MOUSEEVENTF_LEFTDOWN;
 			upFlags|=MOUSEEVENTF_LEFTUP;
+			#else
+			button=1;
+			#endif
 		}
 		if(!stricmp(argv[i], "-mb"))
 		{
+			#ifdef _WIN32
 			downFlags|=MOUSEEVENTF_MIDDLEDOWN;
 			upFlags|=MOUSEEVENTF_MIDDLEUP;
+			#else
+			button=2;
+			#endif
 		}
 		if(!stricmp(argv[i], "-rb"))
 		{
+			#ifdef _WIN32
 			downFlags|=MOUSEEVENTF_RIGHTDOWN;
 			upFlags|=MOUSEEVENTF_RIGHTUP;
+			#else
+			button=3;
+			#endif
 		}
-		#endif
 	}
 
 	printf("\nThin Client Benchmark\n");
@@ -221,6 +258,10 @@ int main(int argc, char **argv)
 
 	#else
 
+	signal(SIGINT, handler);
+	signal(SIGTERM, handler);
+	signal(SIGHUP, handler);
+
 	if(!XInitThreads())
 	{
 		fprintf(stderr, "XInitThreads() failed\n");
@@ -230,6 +271,15 @@ int main(int argc, char **argv)
 	{
 		fprintf(stderr, "Could not open display %s\n", XDisplayName(0));
 		exit(1);
+	}
+	if(moveX>0 || moveY>0)
+	{
+		int dummy;
+		if(!XTestQueryExtension(wh.dpy, &dummy, &dummy, &dummy, &dummy))
+		{
+			fprintf(stderr, "XTEST extension not available\n");
+			exit(1);
+		}
 	}
 	if(!wh.d)
 	{
@@ -261,29 +311,34 @@ int main(int argc, char **argv)
 	SetPriorityClass(GetCurrentProcess(), HIGH_PRIORITY_CLASS);
 	#endif
 
-	#ifdef _WIN32
 	if(moveX>0)
 		printf("Simulating horizontal mouse movement +/- %d pixels\n", moveX);
 	if(moveY>0)
 		printf("Simulating vertical mouse movement +/- %d pixels\n", moveY);
 	if(moveX>0 || moveY>0)
 	{
+		#ifdef _WIN32
 		memset(inputs, 0, sizeof(INPUT)*4);
 		if(!GetWindowRect(wh, &winRect))
 			_throww32("Could not get window rectangle");
 		currentX=winRect.left+x+(moveY>0 ? 40:16);
 		currentY=winRect.top+y+(moveX>0 ? 40:16);
+		#else
+		Window child;
+		XTranslateCoordinates(wh.dpy, wh.d, DefaultRootWindow(wh.dpy), x, y,
+			&rootx, &rooty, &child);
+		currentX=rootx+(moveY>0 ? 40:16);
+		currentY=rooty+(moveX>0 ? 40:16);
+		if(button>0) XTestFakeButtonEvent(wh.dpy, button, True, CurrentTime);
+		#endif
 		MOVE_MOUSE();
 	}
-	#endif
 	timer.start();
 	do
 	{
 		_fbx(fbx_read(&fb, x, y));
-		#ifdef _WIN32
 		if(moveX>0 || moveY>0)
 			MOVE_MOUSE();
-		#endif
 		samples++;
 		if(first)
 		{
@@ -301,12 +356,19 @@ int main(int argc, char **argv)
 		#else
 		int sleepTime=(int)(1000000.*((double)samples/(double)sampleRate-elapsed));
 		if(sleepTime>0) usleep(sleepTime);
+		if(deadYet) break;
 		#endif
 	} while((elapsed=timer.elapsed())<benchTime);
 	#ifdef _WIN32
 	SetPriorityClass(GetCurrentProcess(), NORMAL_PRIORITY_CLASS);
 	inputs[3].mi.dwFlags=upFlags;
 	SendInput(1, &inputs[3], sizeof(INPUT));
+	#else
+	if((moveX>0 || moveY>0) && button>0)
+	{
+			XTestFakeButtonEvent(wh.dpy, button, False, CurrentTime);
+			XSync(wh.dpy, False);
+	}
 	#endif
 
 	printf("Samples: %d  Frames: %d  Time: %f s  Frames/sec: %f\n", samples,

diags/CMakeLists.txt
@@ -15,7 +15,9 @@ endif()
 if(WIN32)
 	add_executable(tcbench tcbench.cpp)
 else()
+	include_directories(${X11_XTest_INCLUDE_PATH})
 	add_executable(tcbench tcbench.cpp dsimple.c)
+	target_link_libraries(tcbench ${X11_XTest_LIB})
 endif()
 target_link_libraries(tcbench fbx)
 install(TARGETS tcbench DESTINATION ${CMAKE_INSTALL_BINDIR})

commit 4a7cbfac5926bd91ff7884c7662d2c9ecf99b666

tcbench.cpp: Fix Solaris build
Broken by 7bf2449084dd3b7a365eda28d32598d588959f29.  Linux doesn't seem
to care that we include sys/signal.h instead of signal.h, but Solaris
does.

diags/tcbench.cpp
@@ -1,6 +1,6 @@
 /* Copyright (C)2004 Landmark Graphics Corporation
  * Copyright (C)2006-2007 Sun Microsystems, Inc.
- * Copyright (C)2011, 2013-2014, 2016-2017 D. R. Commander
+ * Copyright (C)2011, 2013-2014, 2016-2018 D. R. Commander
  *
  * This library is free software and may be redistributed and/or modified under
  * the terms of the wxWindows Library License, Version 3.1 or (at your option)
@@ -26,7 +26,7 @@
 		#define NeedFunctionPrototypes  1
 		#include "dsimple.h"
 	}
-	#include <sys/signal.h>
+	#include <signal.h>
 	#include <sys/time.h>
 	#include <sys/resource.h>
 	#include <X11/extensions/XTest.h>