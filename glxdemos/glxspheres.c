VirtualGL/virtualgl

commit 0a8fd5a10c4e92f2ce7c69b38fb286d857b0f397

Fix code issues identified by Oracle Dev. Studio
(+ a couple identified by GCC 6)

Unreachable statements, variable scope hiding, etc.

glxdemos/glxspheres.c
@@ -1,5 +1,5 @@
 /* Copyright (C)2007 Sun Microsystems, Inc.
- * Copyright (C)2011, 2013-2015, 2017 D. R. Commander
+ * Copyright (C)2011, 2013-2015, 2017-2018 D. R. Commander
  *
  * This library is free software and may be redistributed and/or modified under
  * the terms of the wxWindows Library License, Version 3.1 or (at your option)
@@ -31,9 +31,18 @@
 #define _throw(m) \
 { \
 	fprintf(stderr, "ERROR in line %d:\n%s\n", __LINE__,  m); \
+	retval = -1; \
 	goto bailout; \
 }
-#define _catch(f)  { if((f) == -1) goto bailout; }
+
+#define _catch(f) \
+{ \
+	if((f) == -1) \
+	{ \
+		retval = -1; \
+		goto bailout; \
+	} \
+}
 
 #define np2(i)  ((i) > 0 ? (1 << (int)(log((double)(i)) / log(2.))) : 0)
 
@@ -81,7 +90,7 @@ int width = DEF_WIDTH, height = DEF_HEIGHT;
 
 int setColorScheme(Colormap cmap, int nColors, int bpc, int scheme)
 {
-	XColor xc[1024];  int i, maxColors = (1 << bpc);
+	XColor xc[1024];  int i, maxColors = (1 << bpc), retval = 0;
 
 	if(!nColors || !cmap) _throw("Color map not allocated");
 	if(scheme < 0 || scheme > NSCHEMES - 1 || !cmap) _throw("Invalid argument");
@@ -103,10 +112,9 @@ int setColorScheme(Colormap cmap, int nColors, int bpc, int scheme)
 			xc[i].blue = (i * (maxColors / nColors)) << (16 - bpc);
 	}
 	XStoreColors(dpy, cmap, xc, nColors);
-	return 0;
 
 	bailout:
-	return -1;
+	return retval;
 }
 
 
@@ -236,10 +244,11 @@ void renderSpheres(int buf)
 }
 
 
-void renderOverlay(void)
+int renderOverlay(void)
 {
 	int i, j, w = width / 8, h = height / 8;  unsigned char *buf = NULL;
 	int index = (int)(loneSphereColor * (GLfloat)(nOlColors - 1));
+	int retval = 0;
 
 	glShadeModel(GL_FLAT);
 	glDisable(GL_DEPTH_TEST);
@@ -277,6 +286,7 @@ void renderOverlay(void)
 
 	bailout:
 	if(buf) free(buf);
+	return retval;
 }
 
 
@@ -288,6 +298,7 @@ int display(int advance)
 	static char temps[256];
 	XFontStruct *fontInfo = NULL;  int minChar, maxChar;
 	GLfloat xAspect, yAspect;
+	int retval = 0;
 
 	if(first)
 	{
@@ -377,7 +388,7 @@ int display(int advance)
 	if(useOverlay)
 	{
 		glXMakeCurrent(dpy, olWin, olCtx);
-		renderOverlay();
+		_catch(renderOverlay());
 	}
 	else glPushAttrib(GL_CURRENT_BIT);
 	glPushAttrib(GL_LIST_BIT);
@@ -419,11 +430,13 @@ int display(int advance)
 	if(maxFrames && totalFrames > maxFrames) goto bailout;
 
 	start = getTime();
-	return 0;
 
 	bailout:
-	if(sphereQuad) { gluDeleteQuadric(sphereQuad);  sphereQuad = NULL; }
-	return -1;
+	if(sphereQuad && retval < 0)
+	{
+		gluDeleteQuadric(sphereQuad);  sphereQuad = NULL;
+	}
+	return retval;
 }
 
 
@@ -432,6 +445,8 @@ Atom protoAtom = 0, deleteAtom = 0;
 
 int eventLoop(Display *dpy)
 {
+	int retval = 0;
+
 	while(1)
 	{
 		int advance = 0, doDisplay = 0;
@@ -487,7 +502,7 @@ int eventLoop(Display *dpy)
 	}
 
 	bailout:
-	return -1;
+	return retval;
 }
 
 
@@ -521,8 +536,9 @@ void usage(char **argv)
 
 int main(int argc, char **argv)
 {
-	int i, useAlpha = 0, nPolys = -1;
+	int i, n = 0, useAlpha = 0, nPolys = -1, retval = 0;
 	XVisualInfo *v = NULL;
+	GLXFBConfig *c = NULL;
 	int rgbAttribs[] = { GLX_RENDER_TYPE, GLX_RGBA_BIT, GLX_RED_SIZE, 8,
 		GLX_GREEN_SIZE, 8, GLX_BLUE_SIZE, 8, GLX_DEPTH_SIZE, 1,
 		GLX_DOUBLEBUFFER, 1, GLX_STEREO, 0, GLX_X_VISUAL_TYPE, GLX_TRUE_COLOR,
@@ -621,16 +637,12 @@ int main(int argc, char **argv)
 		rgbAttribs[17] = 32 - bpc * 3;
 	}
 
-	int n = 0;
-	GLXFBConfig *c = glXChooseFBConfig(dpy, screen, rgbAttribs, &n);
+	c = glXChooseFBConfig(dpy, screen, rgbAttribs, &n);
 	if(!c || n < 1)
 		_throw("Could not obtain RGB visual with requested properties");
 	if((v = glXGetVisualFromFBConfig(dpy, c[0])) == NULL)
-	{
-		XFree(c);
 		_throw("Could not obtain RGB visual with requested properties");
-	}
-	XFree(c);
+	XFree(c);  c = NULL;
 	fprintf(stderr, "Visual ID of %s: 0x%.2x\n",
 		useOverlay ? "underlay" : "window", (int)v->visualid);
 
@@ -721,19 +733,14 @@ int main(int argc, char **argv)
 
 	_catch(eventLoop(dpy));
 
-	if(dpy && olCtx) { glXDestroyContext(dpy, olCtx);  olCtx = 0; }
-	if(dpy && olWin) { XDestroyWindow(dpy, olWin);  olWin = 0; }
-	if(dpy && ctx) { glXDestroyContext(dpy, ctx);  ctx = 0; }
-	if(dpy && win) { XDestroyWindow(dpy, win);  win = 0; }
-	if(dpy) XCloseDisplay(dpy);
-	return 0;
-
 	bailout:
 	if(v) XFree(v);
-	if(dpy && olCtx) { glXDestroyContext(dpy, olCtx);  olCtx = 0; }
-	if(dpy && olWin) { XDestroyWindow(dpy, olWin);  olWin = 0; }
-	if(dpy && ctx) { glXDestroyContext(dpy, ctx);  ctx = 0; }
-	if(dpy && win) { XDestroyWindow(dpy, win);  win = 0; }
+	if(c) XFree(c);
+	if(dpy && olCtx) glXDestroyContext(dpy, olCtx);
+	if(dpy && olWin) XDestroyWindow(dpy, olWin);
+	if(dpy && ctx) glXDestroyContext(dpy, ctx);
+	if(dpy && win) XDestroyWindow(dpy, win);
 	if(dpy) XCloseDisplay(dpy);
-	return -1;
+
+	return retval;
 }

client/vglclient.cpp
@@ -456,7 +456,8 @@ int start(char *displayname)
 							success = false;  if(i == 0) throw;
 						}
 						i++;
-						if(i > 4299) i = 4200;  if(i == RR_DEFAULTPORT) i = 0;
+						if(i > 4299) i = 4200;
+						if(i == RR_DEFAULTPORT) i = 0;
 					} while(!success);
 				}
 				else sslReceiver->listen(sslPort);

include/x11err.h
@@ -20,23 +20,23 @@ static const char *x11error(int code)
 		return "Extension error";
 	switch(code)
 	{
-		__case(BadRequest);
-		__case(BadValue);
-		__case(BadWindow);
-		__case(BadPixmap);
-		__case(BadAtom);
-		__case(BadCursor);
-		__case(BadFont);
-		__case(BadMatch);
-		__case(BadDrawable);
-		__case(BadAccess);
-		__case(BadAlloc);
-		__case(BadColor);
-		__case(BadGC);
-		__case(BadIDChoice);
-		__case(BadName);
-		__case(BadLength);
-		__case(BadImplementation);
+		__case(BadRequest)
+		__case(BadValue)
+		__case(BadWindow)
+		__case(BadPixmap)
+		__case(BadAtom)
+		__case(BadCursor)
+		__case(BadFont)
+		__case(BadMatch)
+		__case(BadDrawable)
+		__case(BadAccess)
+		__case(BadAlloc)
+		__case(BadColor)
+		__case(BadGC)
+		__case(BadIDChoice)
+		__case(BadName)
+		__case(BadLength)
+		__case(BadImplementation)
 		default:  return "Unknown error code";
 	}
 }

server/VirtualDrawable.cpp
@@ -1,6 +1,6 @@
 /* Copyright (C)2004 Landmark Graphics Corporation
  * Copyright (C)2005, 2006 Sun Microsystems, Inc.
- * Copyright (C)2009-2015, 2017 D. R. Commander
+ * Copyright (C)2009-2015, 2017-2018 D. R. Commander
  *
  * This library is free software and may be redistributed and/or modified under
  * the terms of the wxWindows Library License, Version 3.1 or (at your option)
@@ -219,7 +219,7 @@ VirtualDrawable::~VirtualDrawable(void)
 
 int VirtualDrawable::init(int width, int height, GLXFBConfig config_)
 {
-	static bool alreadyPrinted = false;
+	static bool alreadyPrintedDrawableType = false;
 	if(!config_ || width < 1 || height < 1) _throw("Invalid argument");
 
 	CriticalSection::SafeLock l(mutex);
@@ -228,19 +228,19 @@ int VirtualDrawable::init(int width, int height, GLXFBConfig config_)
 		return 0;
 	if(fconfig.drawable == RRDRAWABLE_PIXMAP)
 	{
-		if(!alreadyPrinted && fconfig.verbose)
+		if(!alreadyPrintedDrawableType && fconfig.verbose)
 		{
 			vglout.println("[VGL] Using Pixmaps for rendering");
-			alreadyPrinted = true;
+			alreadyPrintedDrawableType = true;
 		}
 		_newcheck(oglDraw = new OGLDrawable(width, height, 0, config_, NULL));
 	}
 	else
 	{
-		if(!alreadyPrinted && fconfig.verbose)
+		if(!alreadyPrintedDrawableType && fconfig.verbose)
 		{
 			vglout.println("[VGL] Using Pbuffers for rendering");
-			alreadyPrinted = true;
+			alreadyPrintedDrawableType = true;
 		}
 		_newcheck(oglDraw = new OGLDrawable(width, height, config_));
 	}

server/fakerconfig.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C)2009-2017 D. R. Commander
+/* Copyright (C)2009-2018 D. R. Commander
  *
  * This library is free software and may be redistributed and/or modified under
  * the terms of the wxWindows Library License, Version 3.1 or (at your option)
@@ -45,7 +45,7 @@ static bool fconfig_envset = false;
 static int fconfig_shmid = -1;
 int fconfig_getshmid(void) { return fconfig_shmid; }
 #endif
-static FakerConfig *fc = NULL;
+static FakerConfig *fconfig_instance = NULL;
 
 
 /* This is a hack necessary to defer the initialization of the recursive mutex
@@ -82,12 +82,12 @@ static DeferredCS fconfig_mutex;
 static void fconfig_init(void);
 
 
-FakerConfig *fconfig_instance(void)
+FakerConfig *fconfig_getinstance(void)
 {
-	if(fc == NULL)
+	if(fconfig_instance == NULL)
 	{
 		CriticalSection::SafeLock l(fcmutex);
-		if(fc == NULL)
+		if(fconfig_instance == NULL)
 		{
 			#if FCONFIG_USESHM == 1
 
@@ -106,32 +106,32 @@ FakerConfig *fconfig_instance(void)
 				&& !strncmp(env, "1", 1))
 				vglout.println("[VGL] Shared memory segment ID for vglconfig: %d",
 					fconfig_shmid);
-			fc = (FakerConfig *)addr;
+			fconfig_instance = (FakerConfig *)addr;
 
 			#else
 
-			fc = new FakerConfig;
-			if(!fc) _throw("Could not allocate config structure");
+			fconfig_instance = new FakerConfig;
+			if(!fconfig_instance) _throw("Could not allocate config structure");
 
 			#endif
 
 			fconfig_init();
 		}
 	}
-	return fc;
+	return fconfig_instance;
 }
 
 
 void fconfig_deleteinstance(void)
 {
-	if(fc != NULL)
+	if(fconfig_instance != NULL)
 	{
 		CriticalSection::SafeLock l(fcmutex, false);
-		if(fc != NULL)
+		if(fconfig_instance != NULL)
 		{
 			#if FCONFIG_USESHM == 1
 
-			shmdt((char *)fc);
+			shmdt((char *)fconfig_instance);
 			if(fconfig_shmid != -1)
 			{
 				int ret = shmctl(fconfig_shmid, IPC_RMID, 0);
@@ -144,11 +144,11 @@ void fconfig_deleteinstance(void)
 
 			#else
 
-			delete fc;
+			delete fconfig_instance;
 
 			#endif
 
-			fc = NULL;
+			fconfig_instance = NULL;
 		}
 	}
 }

server/fakerconfig.h.in
@@ -1,4 +1,4 @@
-/* Copyright (C)2009, 2016 D. R. Commander
+/* Copyright (C)2009, 2016, 2018 D. R. Commander
  *
  * This library is free software and may be redistributed and/or modified under
  * the terms of the wxWindows Library License, Version 3.1 or (at your option)
@@ -23,10 +23,10 @@
 
 #define VGLCONFIG_PATH  "@CMAKE_INSTALL_FULL_BINDIR@/vglconfig"
 
-FakerConfig *fconfig_instance(void);
+FakerConfig *fconfig_getinstance(void);
 void fconfig_deleteinstance(void);
 
-#define fconfig  (*fconfig_instance())
+#define fconfig  (*fconfig_getinstance())
 
 #if FCONFIG_USESHM == 1
 int fconfig_getshmid(void);

server/fakerut.cpp
@@ -1,6 +1,6 @@
 /* Copyright (C)2004 Landmark Graphics Corporation
  * Copyright (C)2005, 2006 Sun Microsystems, Inc.
- * Copyright (C)2010-2015, 2017 D. R. Commander
+ * Copyright (C)2010-2015, 2017-2018 D. R. Commander
  *
  * This library is free software and may be redistributed and/or modified under
  * the terms of the wxWindows Library License, Version 3.1 or (at your option)
@@ -78,30 +78,30 @@ void clickToContinue(Display *dpy)
 
 #define _prerror1(m, a1) \
 { \
-	char temps[256]; \
-	snprintf(temps, 255, m, a1); \
-	throw(Error(__FUNCTION__, temps, 0)); \
+	char tempErrStr[256]; \
+	snprintf(tempErrStr, 255, m, a1); \
+	throw(Error(__FUNCTION__, tempErrStr, 0)); \
 }
 
 #define _prerror2(m, a1, a2) \
 { \
-	char temps[256]; \
-	snprintf(temps, 255, m, a1, a2); \
-	throw(Error(__FUNCTION__, temps, 0)); \
+	char tempErrStr[256]; \
+	snprintf(tempErrStr, 255, m, a1, a2); \
+	throw(Error(__FUNCTION__, tempErrStr, 0)); \
 }
 
 #define _prerror3(m, a1, a2, a3) \
 { \
-	char temps[256]; \
-	snprintf(temps, 255, m, a1, a2, a3); \
-	throw(Error(__FUNCTION__, temps, 0)); \
+	char tempErrStr[256]; \
+	snprintf(tempErrStr, 255, m, a1, a2, a3); \
+	throw(Error(__FUNCTION__, tempErrStr, 0)); \
 }
 
 #define _prerror5(m, a1, a2, a3, a4, a5) \
 { \
-	char temps[256]; \
-	snprintf(temps, 255, m, a1, a2, a3, a4, a5); \
-	throw(Error(__FUNCTION__, temps, 0)); \
+	char tempErrStr[256]; \
+	snprintf(tempErrStr, 255, m, a1, a2, a3, a4, a5); \
+	throw(Error(__FUNCTION__, tempErrStr, 0)); \
 }
 
 
@@ -138,7 +138,6 @@ unsigned int checkBufferColor(void)
 		if(buf) free(buf);
 		throw;
 	}
-	return 0;
 }
 
 
@@ -953,7 +952,6 @@ GLXFBConfig getFBConfigFromVisual(Display *dpy, XVisualInfo *vis)
 		if(configs) { XFree(configs);  configs = NULL; }
 		throw;
 	}
-	return 0;
 }
 
 
@@ -1131,7 +1129,7 @@ int visTest(void)
 			_throw("No X Visuals found");
 		printf("\n");
 
-		for(int i = 0; i < n; i++)
+		for(i = 0; i < n; i++)
 		{
 			XVisualInfo *vis2 = NULL;
 			try

server/vglconfig.cpp
@@ -463,7 +463,7 @@ int main(int argc, char **argv)
 
 		if(test)
 		{
-			if(!(_fconfig = fconfig_instance()))
+			if(!(_fconfig = fconfig_getinstance()))
 				_throw("Could not allocate FakerConfig");
 			fl_open_display();
 			fconfig_setdefaultsfromdpy(fl_display);

commit 4876d553e3eebe0ab9fb5714491cc5b77a87aaef

Fix glxspheres -f option
Broken by 0a8fd5a10c4e92f2ce7c69b38fb286d857b0f397.  This commit also
causes GLXspheres to exit with a successful status when it has finished
rendering the specified number of frames.

glxdemos/glxspheres.c
@@ -71,7 +71,7 @@ Display *dpy = NULL;  Window win = 0, olWin = 0;
 GLXContext ctx = 0, olCtx = 0;
 int useStereo = 0, useOverlay = 0, useDC = 0, useImm = 0, interactive = 0,
 	olDB = 1, loColor = 0, maxFrames = 0, totalFrames = 0, directCtx = True,
-	bpc = 8;
+	bpc = 8, deadYet = 0;
 int rshift = 0, gshift = 0, bshift = 0;
 double benchTime = DEFBENCHTIME;
 int nColors = 0, nOlColors, colorScheme = GRAY;
@@ -427,15 +427,16 @@ int display(int advance)
 			elapsed = mpixels = 0.;  frames = 0;
 		}
 	}
-	if(maxFrames && totalFrames > maxFrames) goto bailout;
+	if(maxFrames && totalFrames > maxFrames)
+	{
+		deadYet = 1;  goto bailout;
+	}
 
 	start = getTime();
+	return 0;
 
 	bailout:
-	if(sphereQuad && retval < 0)
-	{
-		gluDeleteQuadric(sphereQuad);  sphereQuad = NULL;
-	}
+	if(sphereQuad) { gluDeleteQuadric(sphereQuad);  sphereQuad = NULL; }
 	return retval;
 }
 
@@ -447,7 +448,7 @@ int eventLoop(Display *dpy)
 {
 	int retval = 0;
 
-	while(1)
+	while(!deadYet)
 	{
 		int advance = 0, doDisplay = 0;
 