VirtualGL/virtualgl

commit 7b802df6f1a7b7623598d90a2f69c7de580b8402

Always pass pixel format structs by reference
Some compilers complained about an incomplete parameter type in the
definition of the convert() method, and it's really cleaner to pass
by reference anyhow, because it prevents the pixel format specifiers
returned by pf_get() from being modified.  Since PF is now typedefed to
const struct _PF, doing something stupid like:

  PF *pf = pf_get(PF_RGB);
  pf->id = -1;

would produce an error: "assignment of read-only location/member."

util/pf.c
@@ -1,4 +1,4 @@
-/* Copyright (C)2017 D. R. Commander
+/* Copyright (C)2017-2018 D. R. Commander
  *
  * This library is free software and may be redistributed and/or modified under
  * the terms of the wxWindows Library License, Version 3.1 or (at your option)
@@ -289,9 +289,9 @@
 #endif
 
 static __inline void convert_RGB(unsigned char *srcBuf, int width,
-	int srcStride, int height, unsigned char *dstBuf, int dstStride, PF dstpf)
+	int srcStride, int height, unsigned char *dstBuf, int dstStride, PF *dstpf)
 {
-	switch(dstpf.id)
+	if(dstpf) switch(dstpf->id)
 	{
 		case PF_RGB:       CONVERT_FAST(RGB)
 		case PF_RGBX:      CONVERT_RGB(RGB, RGBX)
@@ -307,9 +307,9 @@ static __inline void convert_RGB(unsigned char *srcBuf, int width,
 }
 
 static __inline void convert_RGBX(unsigned char *srcBuf, int width,
-	int srcStride, int height, unsigned char *dstBuf, int dstStride, PF dstpf)
+	int srcStride, int height, unsigned char *dstBuf, int dstStride, PF *dstpf)
 {
-	switch(dstpf.id)
+	if(dstpf) switch(dstpf->id)
 	{
 		case PF_RGB:       CONVERT_RGB(RGBX, RGB)
 		case PF_RGBX:      CONVERT_FAST(RGBX)
@@ -325,9 +325,9 @@ static __inline void convert_RGBX(unsigned char *srcBuf, int width,
 }
 
 static __inline void convert_RGB10_X2(unsigned char *srcBuf, int width,
-	int srcStride, int height, unsigned char *dstBuf, int dstStride, PF dstpf)
+	int srcStride, int height, unsigned char *dstBuf, int dstStride, PF *dstpf)
 {
-	switch(dstpf.id)
+	if(dstpf) switch(dstpf->id)
 	{
 		case PF_RGB:       CONVERT_PF4I2C(RGB10_X2, RGB)
 		case PF_RGBX:      CONVERT_PF4I(RGB10_X2, RGBX, 2, 0)
@@ -343,9 +343,9 @@ static __inline void convert_RGB10_X2(unsigned char *srcBuf, int width,
 }
 
 static __inline void convert_BGR(unsigned char *srcBuf, int width,
-	int srcStride, int height, unsigned char *dstBuf, int dstStride, PF dstpf)
+	int srcStride, int height, unsigned char *dstBuf, int dstStride, PF *dstpf)
 {
-	switch(dstpf.id)
+	if(dstpf) switch(dstpf->id)
 	{
 		case PF_RGB:       CONVERT_BGR(BGR, RGB)
 		case PF_RGBX:      CONVERT_BGR(BGR, RGBX)
@@ -361,9 +361,9 @@ static __inline void convert_BGR(unsigned char *srcBuf, int width,
 }
 
 static __inline void convert_BGRX(unsigned char *srcBuf, int width,
-	int srcStride, int height, unsigned char *dstBuf, int dstStride, PF dstpf)
+	int srcStride, int height, unsigned char *dstBuf, int dstStride, PF *dstpf)
 {
-	switch(dstpf.id)
+	if(dstpf) switch(dstpf->id)
 	{
 		case PF_RGB:       CONVERT_BGR(BGRX, RGB)
 		case PF_RGBX:      CONVERT_PF4CBGR(BGRX, RGBX)
@@ -379,9 +379,9 @@ static __inline void convert_BGRX(unsigned char *srcBuf, int width,
 }
 
 static __inline void convert_BGR10_X2(unsigned char *srcBuf, int width,
-	int srcStride, int height, unsigned char *dstBuf, int dstStride, PF dstpf)
+	int srcStride, int height, unsigned char *dstBuf, int dstStride, PF *dstpf)
 {
-	switch(dstpf.id)
+	if(dstpf) switch(dstpf->id)
 	{
 		case PF_RGB:       CONVERT_PF4I2C(BGR10_X2, RGB)
 		case PF_RGBX:      CONVERT_PF4I(BGR10_X2, RGBX, 2, 0)
@@ -397,9 +397,9 @@ static __inline void convert_BGR10_X2(unsigned char *srcBuf, int width,
 }
 
 static __inline void convert_XBGR(unsigned char *srcBuf, int width,
-	int srcStride, int height, unsigned char *dstBuf, int dstStride, PF dstpf)
+	int srcStride, int height, unsigned char *dstBuf, int dstStride, PF *dstpf)
 {
-	switch(dstpf.id)
+	if(dstpf) switch(dstpf->id)
 	{
 		case PF_RGB:       CONVERT_BGR(XBGR, RGB)
 		case PF_RGBX:      CONVERT_PF4CBGR(XBGR, RGBX)
@@ -415,9 +415,9 @@ static __inline void convert_XBGR(unsigned char *srcBuf, int width,
 }
 
 static __inline void convert_X2_BGR10(unsigned char *srcBuf, int width,
-	int srcStride, int height, unsigned char *dstBuf, int dstStride, PF dstpf)
+	int srcStride, int height, unsigned char *dstBuf, int dstStride, PF *dstpf)
 {
-	switch(dstpf.id)
+	if(dstpf) switch(dstpf->id)
 	{
 		case PF_RGB:       CONVERT_PF4I2C(X2_BGR10, RGB)
 		case PF_RGBX:      CONVERT_PF4I(X2_BGR10, RGBX, 2, 0)
@@ -433,9 +433,9 @@ static __inline void convert_X2_BGR10(unsigned char *srcBuf, int width,
 }
 
 static __inline void convert_XRGB(unsigned char *srcBuf, int width,
-	int srcStride, int height, unsigned char *dstBuf, int dstStride, PF dstpf)
+	int srcStride, int height, unsigned char *dstBuf, int dstStride, PF *dstpf)
 {
-	switch(dstpf.id)
+	if(dstpf) switch(dstpf->id)
 	{
 		case PF_RGB:       CONVERT_RGB(XRGB, RGB)
 		case PF_RGBX:      CONVERT_PF4CRGB(XRGB, RGBX)
@@ -451,9 +451,9 @@ static __inline void convert_XRGB(unsigned char *srcBuf, int width,
 }
 
 static __inline void convert_X2_RGB10(unsigned char *srcBuf, int width,
-	int srcStride, int height, unsigned char *dstBuf, int dstStride, PF dstpf)
+	int srcStride, int height, unsigned char *dstBuf, int dstStride, PF *dstpf)
 {
-	switch(dstpf.id)
+	if(dstpf) switch(dstpf->id)
 	{
 		case PF_RGB:       CONVERT_PF4I2C(X2_RGB10, RGB)
 		case PF_RGBX:      CONVERT_PF4I(X2_RGB10, RGBX, 2, 0)
@@ -557,7 +557,7 @@ static __inline void setRGB_COMP(unsigned char *pixel, int r, int g, int b)
 }
 
 static __inline void convert_COMP(unsigned char *srcBuf, int width,
-	int srcStride, int height, unsigned char *dstBuf, int dstStride, PF dstpf)
+	int srcStride, int height, unsigned char *dstBuf, int dstStride, PF *dstpf)
 {
 	/* VirtualGL doesn't ever need to convert between component and RGB */
 }
@@ -569,28 +569,28 @@ static const PF __format_COMP =
 };
 
 
-PF pf_get(int id)
+static const PF __format_NONE =
+{
+	0, "Invalid", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, getRGB_COMP, setRGB_COMP,
+		convert_COMP
+};
+
+
+PF *pf_get(int id)
 {
 	switch(id)
 	{
-		case PF_RGB:  return __format_RGB;
-		case PF_RGBX:  return __format_RGBX;
-		case PF_RGB10_X2:  return __format_RGB10_X2;
-		case PF_BGR:  return __format_BGR;
-		case PF_BGRX:  return __format_BGRX;
-		case PF_BGR10_X2:  return __format_BGR10_X2;
-		case PF_XBGR:  return __format_XBGR;
-		case PF_X2_BGR10:  return __format_X2_BGR10;
-		case PF_XRGB:  return __format_XRGB;
-		case PF_X2_RGB10:  return __format_X2_RGB10;
-		case PF_COMP:  return __format_COMP;
-		default:
-		{
-			PF pf;
-			memset(&pf, 0, sizeof(PF));
-			pf.name = "Invalid";
-			pf.getRGB = getRGB_COMP;  pf.setRGB = setRGB_COMP;
-			return pf;
-		}
+		case PF_RGB:  return &__format_RGB;
+		case PF_RGBX:  return &__format_RGBX;
+		case PF_RGB10_X2:  return &__format_RGB10_X2;
+		case PF_BGR:  return &__format_BGR;
+		case PF_BGRX:  return &__format_BGRX;
+		case PF_BGR10_X2:  return &__format_BGR10_X2;
+		case PF_XBGR:  return &__format_XBGR;
+		case PF_X2_BGR10:  return &__format_X2_BGR10;
+		case PF_XRGB:  return &__format_XRGB;
+		case PF_X2_RGB10:  return &__format_X2_RGB10;
+		case PF_COMP:  return &__format_COMP;
+		default:  return &__format_NONE;
 	}
 }

client/GLFrame.cpp
@@ -1,5 +1,5 @@
 /* Copyright (C)2005, 2006 Sun Microsystems, Inc.
- * Copyright (C)2014, 2017 D. R. Commander
+ * Copyright (C)2014, 2017-2018 D. R. Commander
  *
  * This library is free software and may be redistributed and/or modified under
  * the terms of the wxWindows Library License, Version 3.1 or (at your option)
@@ -149,13 +149,13 @@ GLFrame &GLFrame::operator= (CompressedFrame &cf)
 			}
 			int y = max(0, hdr.frameh - cf.hdr.y - height);
 			_tj(tjDecompress2(tjhnd, cf.bits, cf.hdr.size,
-				(unsigned char *)&bits[pitch * y + cf.hdr.x * pf.size],
-				width, pitch, height, tjpf[pf.id], tjflags));
+				(unsigned char *)&bits[pitch * y + cf.hdr.x * pf->size],
+				width, pitch, height, tjpf[pf->id], tjflags));
 			if(stereo && cf.rbits && rbits)
 			{
 				_tj(tjDecompress2(tjhnd, cf.rbits, cf.rhdr.size,
-					(unsigned char *)&rbits[pitch * y + cf.hdr.x * pf.size],
-					width, pitch, height, tjpf[pf.id], tjflags));
+					(unsigned char *)&rbits[pitch * y + cf.hdr.x * pf->size],
+					width, pitch, height, tjpf[pf->id], tjflags));
 			}
 		}
 	}
@@ -175,7 +175,7 @@ void GLFrame::drawTile(int x, int y, int width, int height)
 	if(x < 0 || width < 1 || (x + width) > hdr.framew || y < 0 || height < 1
 		|| (y + height) > hdr.frameh)
 		return;
-	int glFormat = (pf.id == PF_BGR ? GL_BGR : GL_RGB);
+	int glFormat = (pf->id == PF_BGR ? GL_BGR : GL_RGB);
 
 	if(!glXMakeCurrent(dpy, win, ctx))
 		_throw("Could not bind OpenGL context to window (window may have disappeared)");
@@ -184,7 +184,7 @@ void GLFrame::drawTile(int x, int y, int width, int height)
 	e = glGetError();
 	while(e != GL_NO_ERROR) e = glGetError();  // Clear previous error
 	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-	glPixelStorei(GL_UNPACK_ROW_LENGTH, pitch / pf.size);
+	glPixelStorei(GL_UNPACK_ROW_LENGTH, pitch / pf->size);
 	int oldbuf = -1;
 	glGetIntegerv(GL_DRAW_BUFFER, &oldbuf);
 	if(stereo) glDrawBuffer(GL_BACK_LEFT);
@@ -192,14 +192,14 @@ void GLFrame::drawTile(int x, int y, int width, int height)
 	glRasterPos2f(((float)x / (float)hdr.framew) * 2.0f - 1.0f,
 		((float)y / (float)hdr.frameh) * 2.0f - 1.0f);
 	glDrawPixels(width, height, glFormat, GL_UNSIGNED_BYTE,
-		&bits[pitch * y + x * pf.size]);
+		&bits[pitch * y + x * pf->size]);
 	if(stereo)
 	{
 		glDrawBuffer(GL_BACK_RIGHT);
 		glRasterPos2f(((float)x / (float)hdr.framew) * 2.0f - 1.0f,
 			((float)y / (float)hdr.frameh) * 2.0f - 1.0f);
 		glDrawPixels(width, height, glFormat, GL_UNSIGNED_BYTE,
-			&rbits[pitch * y + x * pf.size]);
+			&rbits[pitch * y + x * pf->size]);
 		glDrawBuffer(oldbuf);
 	}
 

common/Frame.cpp
@@ -1,6 +1,6 @@
 /* Copyright (C)2004 Landmark Graphics Corporation
  * Copyright (C)2005-2007 Sun Microsystems, Inc.
- * Copyright (C)2009-2012, 2014, 2017 D. R. Commander
+ * Copyright (C)2009-2012, 2014, 2017-2018 D. R. Commander
  *
  * This library is free software and may be redistributed and/or modified under
  * the terms of the wxWindows Library License, Version 3.1 or (at your option)
@@ -73,23 +73,23 @@ void Frame::init(rrframeheader &h, int pixelFormat, int flags_, bool stereo_)
 		throw(Error("Frame::init", "Invalid argument"));
 
 	flags = flags_;
-	PF newpf = pf_get(pixelFormat);
-	if(h.size == 0) h.size = h.framew * h.frameh * newpf.size;
+	PF *newpf = pf_get(pixelFormat);
+	if(h.size == 0) h.size = h.framew * h.frameh * newpf->size;
 	checkHeader(h);
-	if(h.framew != hdr.framew || h.frameh != hdr.frameh || newpf.size != pf.size
-		|| !bits)
+	if(h.framew != hdr.framew || h.frameh != hdr.frameh
+		|| newpf->size != pf->size || !bits)
 	{
 		if(bits) delete [] bits;
-		_newcheck(bits = new unsigned char[h.framew * h.frameh * newpf.size + 1]);
+		_newcheck(bits = new unsigned char[h.framew * h.frameh * newpf->size + 1]);
 	}
 	if(stereo_)
 	{
 		if(h.framew != hdr.framew || h.frameh != hdr.frameh
-			|| newpf.size != pf.size || !rbits)
+			|| newpf->size != pf->size || !rbits)
 		{
 			if(rbits) delete [] rbits;
 			_newcheck(rbits =
-				new unsigned char[h.framew * h.frameh * newpf.size + 1]);
+				new unsigned char[h.framew * h.frameh * newpf->size + 1]);
 		}
 	}
 	else
@@ -99,7 +99,7 @@ void Frame::init(rrframeheader &h, int pixelFormat, int flags_, bool stereo_)
 			delete [] rbits;  rbits = NULL;
 		}
 	}
-	pf = newpf;  pitch = pf.size * h.framew;  stereo = stereo_;  hdr = h;
+	pf = newpf;  pitch = pf->size * h.framew;  stereo = stereo_;  hdr = h;
 }
 
 
@@ -111,7 +111,7 @@ void Frame::init(unsigned char *bits_, int width, int pitch_, int height,
 	hdr.framew = hdr.width = width;
 	hdr.frameh = hdr.height = height;
 	pf = pf_get(pixelFormat);
-	hdr.size = hdr.framew * hdr.frameh * pf.size;
+	hdr.size = hdr.framew * hdr.frameh * pf->size;
 	checkHeader(hdr);
 	pitch = pitch_;
 	flags = flags_;
@@ -123,7 +123,7 @@ Frame *Frame::getTile(int x, int y, int width, int height)
 {
 	Frame *f;
 
-	if(!bits || !pitch || !pf.size) _throw("Frame not initialized");
+	if(!bits || !pitch || !pf->size) _throw("Frame not initialized");
 	if(x < 0 || y < 0 || width < 1 || height < 1 || (x + width) > hdr.width
 		|| (y + height) > hdr.height)
 		throw Error("Frame::getTile", "Argument out of range");
@@ -140,10 +140,10 @@ Frame *Frame::getTile(int x, int y, int width, int height)
 	f->stereo = stereo;
 	f->isGL = isGL;
 	bool bu = (flags & FRAME_BOTTOMUP);
-	f->bits = &bits[pitch * (bu ? hdr.height - y - height : y) + pf.size * x];
+	f->bits = &bits[pitch * (bu ? hdr.height - y - height : y) + pf->size * x];
 	if(stereo && rbits)
 		f->rbits =
-			&rbits[pitch * (bu ? hdr.height - y - height : y) + pf.size * x];
+			&rbits[pitch * (bu ? hdr.height - y - height : y) + pf->size * x];
 	return f;
 }
 
@@ -159,34 +159,34 @@ bool Frame::tileEquals(Frame *last, int x, int y, int width, int height)
 	if(last && hdr.width == last->hdr.width && hdr.height == last->hdr.height
 		&& hdr.framew == last->hdr.framew && hdr.frameh == last->hdr.frameh
 		&& hdr.qual == last->hdr.qual && hdr.subsamp == last->hdr.subsamp
-		&& pf.id == last->pf.id && hdr.winid == last->hdr.winid
+		&& pf->id == last->pf->id && hdr.winid == last->hdr.winid
 		&& hdr.dpynum == last->hdr.dpynum)
 	{
 		if(bits && last->bits)
 		{
 			unsigned char *newBits =
-				&bits[pitch * (bu ? hdr.height - y - height : y) + pf.size * x];
+				&bits[pitch * (bu ? hdr.height - y - height : y) + pf->size * x];
 			unsigned char *oldBits =
 				&last->bits[last->pitch * (bu ? hdr.height - y - height : y) +
-					pf.size * x];
+					pf->size * x];
 			for(int i = 0; i < height; i++)
 			{
 				if(memcmp(&newBits[pitch * i], &oldBits[last->pitch * i],
-					pf.size * width))
+					pf->size * width))
 					return false;
 			}
 		}
 		if(stereo && rbits && last->rbits)
 		{
 			unsigned char *newBits =
-				&rbits[pitch * (bu ? hdr.height - y - height : y) + pf.size * x];
+				&rbits[pitch * (bu ? hdr.height - y - height : y) + pf->size * x];
 			unsigned char *oldBits =
 				&last->rbits[last->pitch * (bu ? hdr.height - y - height : y) +
-					pf.size * x];
+					pf->size * x];
 			for(int i = 0; i < height; i++)
 			{
 				if(memcmp(&newBits[pitch * i], &oldBits[last->pitch * i],
-					pf.size * width))
+					pf->size * width))
 					return false;
 			}
 		}
@@ -202,13 +202,13 @@ void Frame::makeAnaglyph(Frame &r, Frame &g, Frame &b)
 	unsigned char *srcrptr = r.bits, *srcgptr = g.bits, *srcbptr = b.bits,
 		*dstptr = bits, *dstrptr, *dstgptr, *dstbptr;
 
-	if(pf.bpc != 8) _throw("Anaglyphic stereo requires 8 bits per component");
+	if(pf->bpc != 8) _throw("Anaglyphic stereo requires 8 bits per component");
 	for(j = 0; j < hdr.frameh; j++, srcrptr += r.pitch, srcgptr += g.pitch,
 		srcbptr += b.pitch, dstptr += pitch)
 	{
-		for(i = 0, dstrptr = &dstptr[pf.rindex], dstgptr = &dstptr[pf.gindex],
-			dstbptr = &dstptr[pf.bindex]; i < hdr.framew;
-			i++, dstrptr += pf.size, dstgptr += pf.size, dstbptr += pf.size)
+		for(i = 0, dstrptr = &dstptr[pf->rindex], dstgptr = &dstptr[pf->gindex],
+			dstbptr = &dstptr[pf->bindex]; i < hdr.framew;
+			i++, dstrptr += pf->size, dstgptr += pf->size, dstbptr += pf->size)
 		{
 			*dstrptr = srcrptr[i];  *dstgptr = srcgptr[i];  *dstbptr = srcbptr[i];
 		}
@@ -227,7 +227,7 @@ void Frame::makePassive(Frame &stf, int mode)
 
 	if(mode == RRSTEREO_INTERLEAVED)
 	{
-		int rowSize = pf.size * hdr.framew;
+		int rowSize = pf->size * hdr.framew;
 		for(int j = 0; j < hdr.frameh; j++)
 		{
 			if(j % 2 == 0) memcpy(dstptr, srclptr, rowSize);
@@ -237,7 +237,7 @@ void Frame::makePassive(Frame &stf, int mode)
 	}
 	else if(mode == RRSTEREO_TOPBOTTOM)
 	{
-		int rowSize = pf.size * hdr.framew;
+		int rowSize = pf->size * hdr.framew;
 		srcrptr += pitch;
 		for(int j = 0; j < (hdr.frameh + 1) / 2; j++)
 		{
@@ -252,25 +252,25 @@ void Frame::makePassive(Frame &stf, int mode)
 	}
 	else if(mode == RRSTEREO_SIDEBYSIDE)
 	{
-		int pad = pitch - hdr.framew * pf.size, h = hdr.frameh;
+		int pad = pitch - hdr.framew * pf->size, h = hdr.frameh;
 		while(h > 0)
 		{
 			unsigned char *srclptr2 = srclptr;
-			unsigned char *srcrptr2 = srcrptr + pf.size;
+			unsigned char *srcrptr2 = srcrptr + pf->size;
 			for(int i = 0; i < (hdr.framew + 1) / 2; i++)
 			{
 				*(int *)dstptr = *(int *)srclptr2;
-				srclptr2 += pf.size * 2;  dstptr += pf.size;
+				srclptr2 += pf->size * 2;  dstptr += pf->size;
 			}
 			for(int i = (hdr.framew + 1) / 2; i < hdr.framew - 1; i++)
 			{
 				*(int *)dstptr = *(int *)srcrptr2;
-				srcrptr2 += pf.size * 2;  dstptr += pf.size;
+				srcrptr2 += pf->size * 2;  dstptr += pf->size;
 			}
 			if(hdr.framew > 1)
 			{
-				memcpy(dstptr, srcrptr2, pf.size);
-				dstptr += pf.size;
+				memcpy(dstptr, srcrptr2, pf->size);
+				dstptr += pf->size;
 			}
 			srclptr += pitch;  srcrptr += pitch;  dstptr += pad;
 			h--;
@@ -283,7 +283,7 @@ void Frame::decompressRGB(Frame &f, int width, int height, bool rightEye)
 {
 	if(!f.bits || f.hdr.size < 1 || !bits || !hdr.size)
 		_throw("Frame not initialized");
-	if(pf.bpc < 8)
+	if(pf->bpc < 8)
 		throw(Error("RGB decompressor",
 			"Destination frame has the wrong pixel format"));
 
@@ -291,20 +291,20 @@ void Frame::decompressRGB(Frame &f, int width, int height, bool rightEye)
 	int srcStride = f.pitch, dstStride = pitch;
 	int startLine = dstbu ? max(0, hdr.frameh - f.hdr.y - height) : f.hdr.y;
 	unsigned char *srcptr = rightEye ? f.rbits : f.bits,
-		*dstptr = rightEye ? &rbits[pitch * startLine + f.hdr.x * pf.size] :
-			&bits[pitch * startLine + f.hdr.x * pf.size];
+		*dstptr = rightEye ? &rbits[pitch * startLine + f.hdr.x * pf->size] :
+			&bits[pitch * startLine + f.hdr.x * pf->size];
 
 	if(!dstbu)
 	{
 		srcptr = &srcptr[(height - 1) * f.pitch];  srcStride = -srcStride;
 	}
-	pf_get(PF_RGB).convert(srcptr, width, srcStride, height, dstptr, dstStride,
+	pf_get(PF_RGB)->convert(srcptr, width, srcStride, height, dstptr, dstStride,
 		pf);
 }
 
 
 #define DRAWLOGO() \
-switch(pf.size) \
+switch(pf->size) \
 { \
 	case 3: \
 	{ \
@@ -312,12 +312,12 @@ switch(pf.size) \
 		{ \
 			unsigned char *pixel = rowptr; \
 			logoptr2 = logoptr; \
-			for(int i = 0; i < width; i++, pixel += pf.size) \
+			for(int i = 0; i < width; i++, pixel += pf->size) \
 			{ \
 				if(*(logoptr2++)) \
 				{ \
-					pixel[pf.rindex] ^= 113;  pixel[pf.gindex] ^= 162; \
-					pixel[pf.bindex] ^= 117; \
+					pixel[pf->rindex] ^= 113;  pixel[pf->gindex] ^= 162; \
+					pixel[pf->bindex] ^= 117; \
 				} \
 			} \
 			logoptr += VGLLOGO_WIDTH; \
@@ -327,7 +327,7 @@ switch(pf.size) \
 	case 4: \
 	{ \
 		unsigned int mask; \
-		pf.setRGB((unsigned char *)&mask, 113, 162, 117); \
+		pf->setRGB((unsigned char *)&mask, 113, 162, 117); \
 		for(int j = 0; j < height; j++, rowptr += stride) \
 		{ \
 			unsigned int *pixel = (unsigned int *)rowptr; \
@@ -355,19 +355,19 @@ void Frame::addLogo(void)
 	int stride = flags & FRAME_BOTTOMUP ? -pitch : pitch;
 	if(height < 1 || width < 1) return;
 	if(flags & FRAME_BOTTOMUP)
-		rowptr = &bits[pitch * height + (hdr.width - width - 1) * pf.size];
+		rowptr = &bits[pitch * height + (hdr.width - width - 1) * pf->size];
 	else
 		rowptr = &bits[pitch * (hdr.height - height - 1) +
-			(hdr.width - width - 1) * pf.size];
+			(hdr.width - width - 1) * pf->size];
 	DRAWLOGO()
 
 	if(!rbits) return;
 	logoptr = vgllogo;
 	if(flags & FRAME_BOTTOMUP)
-		rowptr = &rbits[pitch * height + (hdr.width - width - 1) * pf.size];
+		rowptr = &rbits[pitch * height + (hdr.width - width - 1) * pf->size];
 	else
 		rowptr = &rbits[pitch * (hdr.height - height - 1) +
-			(hdr.width - width - 1) * pf.size];
+			(hdr.width - width - 1) * pf->size];
 	logoptr = vgllogo;
 	DRAWLOGO()
 }
@@ -419,7 +419,7 @@ CompressedFrame::~CompressedFrame(void)
 CompressedFrame &CompressedFrame::operator= (Frame &f)
 {
 	if(!f.bits) _throw("Frame not initialized");
-	if(f.pf.size < 3 || f.pf.size > 4)
+	if(f.pf->size < 3 || f.pf->size > 4)
 		_throw("Only true color frames are supported");
 	switch(f.hdr.compress)
 	{
@@ -437,12 +437,12 @@ void CompressedFrame::compressYUV(Frame &f)
 	int tjflags = 0;
 
 	if(f.hdr.subsamp != 4) throw(Error("YUV encoder", "Invalid argument"));
-	if(f.pf.bpc != 8)
+	if(f.pf->bpc != 8)
 		throw(Error("YUV encoder", "YUV encoding requires 8 bits per component"));
 	init(f.hdr, 0);
 	if(f.flags & FRAME_BOTTOMUP) tjflags |= TJ_BOTTOMUP;
 	_tj(tjEncodeYUV2(tjhnd, f.bits, f.hdr.width, f.pitch, f.hdr.height,
-		tjpf[f.pf.id], bits, TJSUBSAMP(f.hdr.subsamp), tjflags));
+		tjpf[f.pf->id], bits, TJSUBSAMP(f.hdr.subsamp), tjflags));
 	hdr.size = (unsigned int)tjBufSizeYUV(f.hdr.width, f.hdr.height,
 		TJSUBSAMP(f.hdr.subsamp));
 }
@@ -454,7 +454,7 @@ void CompressedFrame::compressJPEG(Frame &f)
 
 	if(f.hdr.qual > 100 || f.hdr.subsamp > 16 || !isPow2(f.hdr.subsamp))
 		throw(Error("JPEG compressor", "Invalid argument"));
-	if(f.pf.bpc != 8)
+	if(f.pf->bpc != 8)
 		throw(Error("JPEG compressor",
 			"JPEG compression requires 8 bits per component"));
 
@@ -462,7 +462,7 @@ void CompressedFrame::compressJPEG(Frame &f)
 	if(f.flags & FRAME_BOTTOMUP) tjflags |= TJ_BOTTOMUP;
 	unsigned long size;
 	_tj(tjCompress2(tjhnd, f.bits, f.hdr.width, f.pitch, f.hdr.height,
-		tjpf[f.pf.id], &bits, &size, TJSUBSAMP(f.hdr.subsamp), f.hdr.qual,
+		tjpf[f.pf->id], &bits, &size, TJSUBSAMP(f.hdr.subsamp), f.hdr.qual,
 		tjflags | TJFLAG_NOREALLOC));
 	hdr.size = (unsigned int)size;
 	if(f.stereo && f.rbits)
@@ -470,7 +470,7 @@ void CompressedFrame::compressJPEG(Frame &f)
 		init(f.hdr, RR_RIGHT);
 		if(rbits)
 			_tj(tjCompress2(tjhnd, f.rbits, f.hdr.width, f.pitch, f.hdr.height,
-				tjpf[f.pf.id], &rbits, &size, TJSUBSAMP(f.hdr.subsamp), f.hdr.qual,
+				tjpf[f.pf->id], &rbits, &size, TJSUBSAMP(f.hdr.subsamp), f.hdr.qual,
 				tjflags | TJFLAG_NOREALLOC));
 		rhdr.size = (unsigned int)size;
 	}
@@ -481,7 +481,7 @@ void CompressedFrame::compressRGB(Frame &f)
 {
 	unsigned char *srcptr;
 	bool bu = (f.flags & FRAME_BOTTOMUP);
-	if(f.pf.bpc != 8)
+	if(f.pf->bpc != 8)
 		throw(Error("RGB compressor",
 			"RGB encoding requires 8 bits per component"));
 	int dstPitch = f.hdr.width * 3;
@@ -489,7 +489,7 @@ void CompressedFrame::compressRGB(Frame &f)
 
 	init(f.hdr, f.stereo ? RR_LEFT : 0);
 	srcptr = bu ? f.bits : &f.bits[f.pitch * (f.hdr.height - 1)];
-	f.pf.convert(srcptr, f.hdr.width, srcStride, f.hdr.height, bits, dstPitch,
+	f.pf->convert(srcptr, f.hdr.width, srcStride, f.hdr.height, bits, dstPitch,
 		pf_get(PF_RGB));
 	hdr.size = dstPitch * f.hdr.height;
 
@@ -499,7 +499,7 @@ void CompressedFrame::compressRGB(Frame &f)
 		if(rbits)
 		{
 			srcptr = bu ? f.rbits : &f.rbits[f.pitch * (f.hdr.height - 1)];
-			f.pf.convert(srcptr, f.hdr.width, srcStride, f.hdr.height, rbits,
+			f.pf->convert(srcptr, f.hdr.width, srcStride, f.hdr.height, rbits,
 				dstPitch, pf_get(PF_RGB));
 			rhdr.size = dstPitch * f.hdr.height;
 		}
@@ -546,7 +546,7 @@ void CompressedFrame::init(rrframeheader &h, int buffer)
 		delete [] rbits;  rbits = NULL;
 		memset(&rhdr, 0, sizeof(rrframeheader));
 	}
-	pitch = hdr.width * pf.size;
+	pitch = hdr.width * pf->size;
 }
 
 
@@ -635,7 +635,7 @@ FBXFrame &FBXFrame::operator= (CompressedFrame &cf)
 		if(cf.hdr.compress == RRCOMP_RGB) decompressRGB(cf, width, height, false);
 		else
 		{
-			if(pf.bpc != 8)
+			if(pf->bpc != 8)
 				throw(Error("JPEG decompressor",
 					"JPEG decompression requires 8 bits per component"));
 			if(!tjhnd)
@@ -644,8 +644,8 @@ FBXFrame &FBXFrame::operator= (CompressedFrame &cf)
 					throw(Error("FBXFrame::decompressor", tjGetErrorStr()));
 			}
 			_tj(tjDecompress2(tjhnd, cf.bits, cf.hdr.size,
-				(unsigned char *)&fb.bits[fb.pitch * cf.hdr.y + cf.hdr.x * pf.size],
-				width, fb.pitch, height, tjpf[pf.id], tjflags));
+				(unsigned char *)&fb.bits[fb.pitch * cf.hdr.y + cf.hdr.x * pf->size],
+				width, fb.pitch, height, tjpf[pf->id], tjflags));
 		}
 	}
 	return *this;
@@ -700,7 +700,7 @@ XVFrame::~XVFrame(void)
 XVFrame &XVFrame::operator= (Frame &f)
 {
 	if(!f.bits) _throw("Frame not initialized");
-	if(f.pf.bpc != 8)
+	if(f.pf->bpc != 8)
 		throw(Error("YUV encoder", "YUV encoding requires 8 bits per component"));
 	int tjflags = 0;
 	init(f.hdr);
@@ -711,7 +711,7 @@ XVFrame &XVFrame::operator= (Frame &f)
 			throw(Error("XVFrame::compressor", tjGetErrorStr()));
 	}
 	_tj(tjEncodeYUV2(tjhnd, f.bits, f.hdr.width, f.pitch, f.hdr.height,
-		tjpf[f.pf.id], bits, TJ_420, tjflags));
+		tjpf[f.pf->id], bits, TJ_420, tjflags));
 	hdr.size = (unsigned int)tjBufSizeYUV(f.hdr.width, f.hdr.height, TJ_420);
 	if(hdr.size != (unsigned long)fb.xvi->data_size)
 		_throw("Image size mismatch in YUV encoder");

common/Frame.h
@@ -1,6 +1,6 @@
 /* Copyright (C)2004 Landmark Graphics Corporation
  * Copyright (C)2005-2007 Sun Microsystems, Inc.
- * Copyright (C)2009-2012, 2014, 2017 D. R. Commander
+ * Copyright (C)2009-2012, 2014, 2017-2018 D. R. Commander
  *
  * This library is free software and may be redistributed and/or modified under
  * the terms of the wxWindows Library License, Version 3.1 or (at your option)
@@ -61,7 +61,7 @@ namespace vglcommon
 			unsigned char *bits;
 			unsigned char *rbits;
 			int pitch, flags;
-			PF pf;
+			PF *pf;
 			bool isGL, isXV, stereo;
 
 		protected:

commit edb196f1a37b0fa7964ee64e0a917ebd5d806f8c

Fix MSVC compiler warning
Caused by 7b802df6f1a7b7623598d90a2f69c7de580b8402

util/pf.c
@@ -478,7 +478,7 @@ static INLINE void setRGB_##id(unsigned char *pixel, int r, int g, int b) \
 		(b << PF_##id##_BSHIFT); \
 } \
 \
-static const PF __format_##id = \
+static PF __format_##id = \
 { \
 	PF_##id, #id, PF_##id##_SIZE, 8, PF_##id##_RMASK, PF_##id##_GMASK, \
 		PF_##id##_BMASK, PF_##id##_RSHIFT, PF_##id##_GSHIFT, PF_##id##_BSHIFT, \
@@ -508,7 +508,7 @@ static INLINE void setRGB_##id(unsigned char *pixel, int r, int g, int b) \
 		(b << PF_##id##_BSHIFT); \
 } \
 \
-static const PF __format_##id = \
+static PF __format_##id = \
 { \
 	PF_##id, #id, PF_##id##_SIZE, 10, PF_##id##_RMASK, PF_##id##_GMASK, \
 		PF_##id##_BMASK, PF_##id##_RSHIFT, PF_##id##_GSHIFT, PF_##id##_BSHIFT, \
@@ -529,7 +529,7 @@ static INLINE void setRGB_##id(unsigned char *pixel, int r, int g, int b) \
 	pixel[PF_##id##_BINDEX] = b; \
 } \
 \
-static const PF __format_##id = \
+static PF __format_##id = \
 { \
 	PF_##id, #id, 3, 8, 0, 0, 0, 0, 0, 0, PF_##id##_RINDEX, PF_##id##_GINDEX, \
 		PF_##id##_BINDEX, getRGB_##id##X, setRGB_##id, convert_##id \
@@ -555,14 +555,14 @@ static INLINE void convert_COMP(unsigned char *srcBuf, int width,
 	/* VirtualGL doesn't ever need to convert between component and RGB */
 }
 
-static const PF __format_COMP =
+static PF __format_COMP =
 {
 	PF_COMP, "COMP", 1, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, getRGB_COMP, setRGB_COMP,
 		convert_COMP
 };
 
 
-static const PF __format_NONE =
+static PF __format_NONE =
 {
 	0, "Invalid", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, getRGB_COMP, setRGB_COMP,
 		convert_COMP