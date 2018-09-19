VirtualGL/virtualgl

commit d771339c6e4fa6302ea2a2c8a7af000ce033a38a

Work around PV segfault w/ non-GLVND nVidia driver
The proximate cause of this is the fact that ParaView calls
glXDestroyContext() to destroy a rendering context in which the front
buffer is the active draw buffer, then it calls
glXMakeCurrent(..., 0, 0) to swap out the context.  When the application
calls glXMakeCurrent(..., 0, 0), VirtualGL detects that front buffer
rendering has occurred but the front buffer has not been read back yet,
so VGL tries to read back and transport the contents of the buffer.  To
do this, VirtualGL swaps in a temporary "readback context", but when VGL
tries to swap back in the previous context (which had been destroyed but
was still current prior to the readback), a segfault occurs within the
bowels of nVidia's libGL.  I would argue that this segfault is a driver
bug and that nVidia's GLX implementation should instead throw a
GLXBadContext error.  The segfault goes away when using the
GLVND-enabled version of the driver, which lends further credence to the
argument that it is a driver bug.

Furthermore, this segfault is reproducible with a simple test program,
irrespective of VirtualGL (refer to #44.)  However, I also admit that
VirtualGL shouldn't be trying to call glXMakeContextCurrent() on a
context that has already been destroyed, so this commit prevents VGL
from doing that, thus working around the segfault.

Fixes #44

server/TempContext.h
@@ -1,6 +1,6 @@
 /* Copyright (C)2004 Landmark Graphics Corporation
  * Copyright (C)2005, 2006 Sun Microsystems, Inc.
- * Copyright (C)2011, 2014-2015 D. R. Commander
+ * Copyright (C)2011, 2014-2015, 2018 D. R. Commander
  *
  * This library is free software and may be redistributed and/or modified under
  * the terms of the wxWindows Library License, Version 3.1 or (at your option)
@@ -21,6 +21,7 @@
 #define __TEMPCONTEXT_H__
 
 #include "faker-sym.h"
+#include "ContextHash.h"
 
 
 #define EXISTING_DRAWABLE  (GLXDrawable)-1
@@ -52,7 +53,12 @@ namespace vglserver
 				{
 					if(!_glXMakeContextCurrent(dpy, draw, read, ctx))
 						_throw("Could not bind OpenGL context to window (window may have disappeared)");
-					ctxChanged = true;
+					// If oldctx has already been destroyed, then we don't want to
+					// restore it.  This can happen if the application is rendering to
+					// the front buffer and glXDestroyContext() is called to destroy the
+					// active context before glXMake*Current*() is called to switch it.
+					if(ctxhash.findConfig(oldctx))
+						ctxChanged = true;
 				}
 			}
 

commit ea525d10296f44c40824afcbb44df72df40e6a74

Fix "Invalid arg" error if readback w/ no context
Regression introduced by d771339c6e4fa6302ea2a2c8a7af000ce033a38a

Fixes #83

server/TempContext.h
@@ -57,7 +57,7 @@ namespace vglserver
 					// restore it.  This can happen if the application is rendering to
 					// the front buffer and glXDestroyContext() is called to destroy the
 					// active context before glXMake*Current*() is called to switch it.
-					if(ctxhash.findConfig(oldctx))
+					if(oldctx && ctxhash.findConfig(oldctx))
 						ctxChanged = true;
 				}
 			}