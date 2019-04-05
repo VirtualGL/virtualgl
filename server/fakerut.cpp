VirtualGL/virtualgl

commit ba112c12a9e11e1be16ba19702c0c6348d01e2e8

Faker: Don't return vis/FBC if OL/trans specified
Ensure that glXChooseFBConfig() and glXChooseVisual() return NULL if
GLX_LEVEL != 0, GLX_TRANSPARENT_TYPE != GLX_NONE, or
GLX_RENDER_TYPE == GLX_COLOR_INDEX_BIT.  Otherwise, the resulting visual
or framebuffer config almost certainly will not work as the application
expects.

server/fakerut.cpp
@@ -976,6 +976,14 @@ GLXFBConfig getFBConfigFromVisual(Display *dpy, XVisualInfo *vis)
 }
 
 
+#define TEST_ATTRIB(vattribs, cattribs, desc)  \
+	vis0 = glXChooseVisual(dpy, DefaultScreen(dpy), vattribs);  \
+	if(vis0) THROW(desc " visual found");  \
+	XFree(vis0);  vis0 = NULL;  \
+	configs = glXChooseFBConfig(dpy, DefaultScreen(dpy), cattribs, &n);  \
+	if(configs) THROW(desc " FB config found");  \
+	XFree(configs);  configs = NULL;  \
+
 // This tests the faker's client/server visual matching heuristics
 int visTest(void)
 {
@@ -994,6 +1002,17 @@ int visTest(void)
 			|| n == 0)
 			THROW("No FB configs found");
 		XFree(configs);  configs = NULL;
+
+		// These should fail, since we no longer support transparent overlays.
+		int attribs0[] = { GLX_LEVEL, 1, None };
+		TEST_ATTRIB(attribs0, attribs0, "Overlay")
+		int vattribs1[] = { None };
+		int cattribs1[] = { GLX_RENDER_TYPE, GLX_COLOR_INDEX_BIT, None };
+		TEST_ATTRIB(vattribs1, cattribs1, "CI")
+		int vattribs2[] = { GLX_RGBA, GLX_TRANSPARENT_TYPE, GLX_TRANSPARENT_RGB,
+			None };
+		int cattribs2[] = { GLX_TRANSPARENT_TYPE, GLX_TRANSPARENT_RGB, None };
+		TEST_ATTRIB(vattribs2, cattribs2, "Transparent")
 
 		try
 		{

server/glxvisual.cpp
@@ -492,7 +492,8 @@ VGLFBConfig *configsFromVisAttribs(Display *dpy, int screen,
 		{
 			renderType = attribs[i + 1];  i++;
 		}
-		else if(attribs[i] == GLX_LEVEL) i++;
+		else if(attribs[i] == GLX_LEVEL && attribs[++i] != 0)
+			return NULL;
 		else if(attribs[i] == GLX_STEREO)
 		{
 			if(glx13) { stereo = attribs[i + 1];  i++; }
@@ -514,7 +515,8 @@ VGLFBConfig *configsFromVisAttribs(Display *dpy, int screen,
 		{
 			alphaSize = attribs[i + 1];  i++;
 		}
-		else if(attribs[i] == GLX_TRANSPARENT_TYPE) i++;
+		else if(attribs[i] == GLX_TRANSPARENT_TYPE && attribs[++i] != GLX_NONE)
+			return NULL;
 		else if(attribs[i] == GLX_SAMPLES)
 		{
 			samples = attribs[i + 1];  i++;
@@ -536,6 +538,7 @@ VGLFBConfig *configsFromVisAttribs(Display *dpy, int screen,
 			i++;
 		}
 	}
+	if(renderType == GLX_COLOR_INDEX_BIT) return NULL;
 	if(doubleBuffer >= 0)
 	{
 		glxattribs[j++] = GLX_DOUBLEBUFFER;  glxattribs[j++] = doubleBuffer;

commit 0c52cf404cf7a53512ab73df881fd370de5e1e2a

fakerut.cpp: Fix code formatting error
(introduced by ba112c12a9e11e1be16ba19702c0c6348d01e2e8)

server/fakerut.cpp
@@ -975,13 +975,13 @@ GLXFBConfig getFBConfigFromVisual(Display *dpy, XVisualInfo *vis)
 }
 
 
-#define TEST_ATTRIB(vattribs, cattribs, desc)  \
-	vis0 = glXChooseVisual(dpy, DefaultScreen(dpy), vattribs);  \
-	if(vis0) THROW(desc " visual found");  \
-	XFree(vis0);  vis0 = NULL;  \
-	configs = glXChooseFBConfig(dpy, DefaultScreen(dpy), cattribs, &n);  \
-	if(configs) THROW(desc " FB config found");  \
-	XFree(configs);  configs = NULL;  \
+#define TEST_ATTRIB(vattribs, cattribs, desc) \
+	vis0 = glXChooseVisual(dpy, DefaultScreen(dpy), vattribs); \
+	if(vis0) THROW(desc " visual found"); \
+	XFree(vis0);  vis0 = NULL; \
+	configs = glXChooseFBConfig(dpy, DefaultScreen(dpy), cattribs, &n); \
+	if(configs) THROW(desc " FB config found"); \
+	XFree(configs);  configs = NULL; \
 
 // This tests the faker's client/server visual matching heuristics
 int visTest(void)