VirtualGL/virtualgl

commit f3c9a34173b84825f7599173b802fded47044add

Faker: Overhaul OpenCL interposer (again)
CLEW uses dlopen()/dlsym() to load OpenCL functions, so it is necessary
to support that use case (thus partly reverting 4ac3bab2.)  However,
supporting that use case requires linking the VGL Faker against
libOpenCL, and since libOpenCL isn't readily available on every platform
we support (notably, it requires the EPEL repository on RHEL 6-7), it is
necessary to disable the OpenCL interposer by default and enable it only
with a vglrun option or environment variable.  This commit:

- modifies the build/packaging system to generate and package a new
  faker library (libvglfaker-opencl.so), which solely contains the
  OpenCL interposer and which is solely linked against libOpenCL.  The
  other faker libraries continue to behave as they did in VirtualGL
  2.6.2.
- modifies the dlopen() interposer so that calls to dlopen("libOpenCL*")
  are redirected to libvglfaker-opencl.so.
- removes the in-tree OpenCL headers introduced by 4ac3bab2 (those were
  only useful when we didn't need to link against libOpenCL.)
- documents the new OpenCL build requirements on RHEL and Ubuntu.
- moves the ocllib field to the end of the FakerConfig struct and
  removes the #ifdef around that field (for forward ABI compatibility.)
- adds a new option (+ocl/-ocl) to vglrun to enable the new OpenCL
  interposer.
- documents the new vglrun option.
- modifies dlfakerut to test both direct-link and dlopen()/dlsym()
  OpenCL interposition for correctness.
- adds TRY()/CATCH() macros to the interposed clCreateContext() function
  (since that function calls some C++ code that may throw an exception.)

release/rpm.spec.in
@@ -2,6 +2,7 @@
 # libnvidia-tls.so.1 if it was built on a system that has the NVidia
 # drivers installed.  The custom find-requires script is created during
 # install and removed during clean
+%define _use_internal_dependency_generator 0
 %define __find_requires %{_tmppath}/%{name}-%{version}-%{release}-find-requires
 
 # Path under which VirtualGL should be installed
@@ -43,6 +44,8 @@
 
 # Path under which docs should be installed
 %define docdir %{_defaultdocdir}/%{name}-%{version}
+
+%define fakeopencl @VGL_FAKEOPENCL@
 
 Summary: A toolkit for displaying OpenGL applications to thin clients
 Name: @PKGNAME@
@@ -94,7 +97,8 @@ Ertl 2000.)
 #-->  -DCMAKE_INSTALL_DOCDIR=%{docdir} -DCMAKE_INSTALL_INCLUDEDIR=%{incdir} \
 #-->  -DCMAKE_INSTALL_LIBDIR=%{libdir} -DCMAKE_INSTALL_PREFIX=%{prefix} \
 #-->  -DVGL_BUILDSERVER=@VGL_BUILDSERVER@ -DVGL_SYSTEMFLTK=@VGL_SYSTEMFLTK@ \
-#-->  -DVGL_USESSL=@VGL_USESSL@ -DVGL_USEXV=@VGL_USEXV@
+#-->  -DVGL_USESSL=@VGL_USESSL@ -DVGL_USEXV=@VGL_USEXV@ \
+#-->  -DVGL_FAKEOPENCL=@VGL_FAKEOPENCL@
 #-->export NUMCPUS=`grep -c '^processor' /proc/cpuinfo`
 #-->make -j$NUMCPUS --load-average=$NUMCPUS
 
@@ -169,7 +173,7 @@ fi
 	ln -fs %{docdir} $RPM_BUILD_ROOT/%{prefix}/doc
 %endif
 
-echo '/usr/lib/rpm/find-requires|grep -v libGLcore|grep -v libnvidia-tls' >%{_tmppath}/%{name}-%{version}-%{release}-find-requires
+echo '/usr/lib/rpm/find-requires|grep -v libGLcore|grep -v libnvidia-tls|grep -v libOpenCL' >%{_tmppath}/%{name}-%{version}-%{release}-find-requires
 chmod 755 %{_tmppath}/%{name}-%{version}-%{release}-find-requires
 
 %clean
@@ -234,6 +238,9 @@ rm %{_tmppath}/%{name}-%{version}-%{release}-find-requires
 %endif
 %{libdir}/lib@VGL_FAKER_NAME@.so
 %{libdir}/lib@VGL_FAKER_NAME@-nodl.so
+%if %{fakeopencl}
+	%{libdir}/lib@VGL_FAKER_NAME@-opencl.so
+%endif
 %{libdir}/lib@VGL_DLFAKER_NAME@.so
 %{libdir}/lib@VGL_GEFAKER_NAME@.so
 

common/rr.h
@@ -186,9 +186,6 @@ typedef struct _FakerConfig
   char log[MAXSTR];
   char logo;
   int np;
-  #ifdef FAKEOPENCL
-  char ocllib[MAXSTR];
-  #endif
   int port;
   char probeglx;
   int qual;
@@ -217,6 +214,7 @@ typedef struct _FakerConfig
   char xcbkeysymslib[MAXSTR];
   char xcbx11lib[MAXSTR];
   char excludeddpys[MAXSTR];
+  char ocllib[MAXSTR];
 } FakerConfig;
 
 #if !defined(__SUNPRO_CC) && !defined(__SUNPRO_C)

doc/index.html
@@ -3,7 +3,7 @@
 <head>
 <meta http-equiv="content-type" content="text/html; charset=ISO-8859-1">
 <meta name="language" content="en">
-<meta name="date" content="2019-09-26T13:07:54">
+<meta name="date" content="2019-10-02T20:18:05">
 <meta name="generator" content="deplate.rb 0.8.5">
 <title>User&rsquo;s Guide for VirtualGL 2.6.3</title>
 <link rel="start" href="index.html" title="Frontpage">
@@ -3215,16 +3215,54 @@ Image transport plugins are free to handle or ignore any configuration option as
         thread, which displays the final rendered result from all of the 
         workers. <br /><br /> The <code>VGL_EXCLUDE</code> environment variable 
         specifies a list of X display names (for instance, <code>:0.1</code>) 
-        for which VirtualGL should not interpose any X11, GLX, OpenGL, or XCB 
-        calls.  In other words, VirtualGL treats these displays as 3D X servers 
-        instead of 2D X servers and does not attempt to redirect 3D rendering 
-        away from them.  When an X display connection is opened using 
+        for which VirtualGL should not interpose any X11, GLX, OpenGL, XCB, or 
+        OpenCL calls.  In other words, VirtualGL treats these displays as 3D X 
+        servers instead of 2D X servers and does not attempt to redirect 3D 
+        rendering away from them.  When an X display connection is opened using 
         <code>XOpenDisplay()</code>, VirtualGL checks if the display name 
         appears in the exclude list, and if so, all subsequent X11, GLX, OpenGL, 
         and XCB calls intended for that display are allowed to pass through 
         unimpeded.  This variable is re-checked every time 
         <code>XOpenDisplay()</code> is called, so it can be set dynamically from 
         within a 3D application.
+    </dd>
+</dl>
+
+<div class="table">
+<table class="standard">
+  <tr class="standard">
+    <td class="high standard">Environment Variable</td>
+    <td class="standard"><code>VGL_FAKEOPENCL = <em>0 | 1</em></code></td>
+  </tr>
+  <tr class="standard">
+    <td class="high standard"><code>vglrun</code> argument</td>
+    <td class="standard"><code>-ocl</code> / <code>+ocl</code></td>
+  </tr>
+  <tr class="standard">
+    <td class="high standard">Summary</td>
+    <td class="standard">Disable/enable OpenCL interposer</td>
+  </tr>
+  <tr class="standard">
+    <td class="high standard">Image Transports</td>
+    <td class="standard">All</td>
+  </tr>
+  <tr class="standard">
+    <td class="high standard">Default Value</td>
+    <td class="standard">Disabled</td>
+  </tr>
+</table>
+</div>
+
+
+<dl class="Description">
+    <dt class="Description-1 Description">Description</dt>
+    <dd class="Description-1 Description">
+        In order to support applications that use OpenCL/OpenGL interoperability 
+        functions, VirtualGL has to interpose the <code>clCreateContext()</code> 
+        function and modify its arguments before passing them to the 
+        &ldquo;real&rdquo; <code>clCreateContext()</code> function in libOpenCL.  
+        Since libOpenCL is not available on all platforms that VirtualGL 
+        supports, the OpenCL interposer is disabled by default.
     </dd>
 </dl>
 

release/makedpkg.in
@@ -88,6 +88,9 @@ makedeb()
 			mkdir -p $TMPDIR/usr/lib/i386-linux-gnu
 			ln -fs /usr/lib32/lib@VGL_FAKER_NAME@.so $TMPDIR/usr/lib/i386-linux-gnu/lib@VGL_FAKER_NAME@.so
 			ln -fs /usr/lib32/lib@VGL_FAKER_NAME@-nodl.so $TMPDIR/usr/lib/i386-linux-gnu/lib@VGL_FAKER_NAME@-nodl.so
+			if [ "$FAKEOPENCL" = "1" ]; then
+				ln -fs /usr/lib32/lib@VGL_FAKER_NAME@-opencl.so $TMPDIR/usr/lib/i386-linux-gnu/lib@VGL_FAKER_NAME@-opencl.so
+			fi
 			ln -fs /usr/lib32/lib@VGL_DLFAKER_NAME@.so $TMPDIR/usr/lib/i386-linux-gnu/lib@VGL_DLFAKER_NAME@.so
 			ln -fs /usr/lib32/lib@VGL_GEFAKER_NAME@.so $TMPDIR/usr/lib/i386-linux-gnu/lib@VGL_GEFAKER_NAME@.so
 		fi
@@ -143,6 +146,7 @@ DOCDIR=@CMAKE_INSTALL_FULL_DOCDIR@
 INCDIR=@CMAKE_INSTALL_FULL_INCLUDEDIR@
 INCSYMLINKS=@VGL_INCSYMLINKS@
 LIBDIR=@CMAKE_INSTALL_FULL_LIBDIR@
+FAKEOPENCL=@VGL_FAKEOPENCL@
 
 if [ ! `uid` -eq 0 ]; then
 	SUDO=sudo

server/CMakeLists.txt
@@ -19,30 +19,8 @@ endif()
 configure_file(fakerconfig.h.in fakerconfig.h)
 configure_file(vendor.h.in vendor.h)
 
-set(DEFAULT_VGL_FAKEOPENCL 0)
-if(CMAKE_SYSTEM_NAME STREQUAL "Linux" OR CMAKE_SYSTEM_NAME MATCHES "FreeBSD")
-	set(DEFAULT_VGL_FAKEOPENCL 1)
-endif()
-option(VGL_FAKEOPENCL "Interpose enough of the OpenCL API to make OpenCL/OpenGL interoperability work"
-	${DEFAULT_VGL_FAKEOPENCL})
-if(VGL_FAKEOPENCL)
-	add_definitions(-DFAKEOPENCL)
-	set(VGL_DEFAULTSYSTEMOCL 0)
-	option(VGL_SYSTEMOCL
-		"Compile against system-supplied versions of the OpenCL headers instead of the in-tree versions."
-			${VGL_DEFAULTSYSTEMOCL})
-	if(VGL_SYSTEMOCL)
-		cmake_minimum_required(VERSION 3.1)
-		include(FindOpenCL)
-	else()
-		set(OpenCL_INCLUDE_DIRS ocl_headers)
-	endif()
-else()
-	set(OpenCL_INCLUDE_DIRS "")
-endif()
-
 include_directories(${CMAKE_CURRENT_BINARY_DIR} ${CMAKE_CURRENT_SOURCE_DIR}
-	../common ${OpenCL_INCLUDE_DIRS})
+	../common)
 
 set(FBXFAKERLIB fbx-faker)
 if(VGL_USEXV)
@@ -57,10 +35,28 @@ if(VGL_FAKEXCB)
 	set(FAKER_XCB_SOURCES faker-xcb.cpp XCBConnHash.cpp)
 endif()
 
+set(DEFAULT_VGL_FAKEOPENCL 0)
+if(CMAKE_SYSTEM_NAME STREQUAL "Linux" OR CMAKE_SYSTEM_NAME MATCHES "FreeBSD")
+	set(DEFAULT_VGL_FAKEOPENCL 1)
+endif()
+option(VGL_FAKEOPENCL "Interpose enough of the OpenCL API to make OpenCL/OpenGL interoperability work"
+	${DEFAULT_VGL_FAKEOPENCL})
+boolean_number(VGL_FAKEOPENCL)
+boolean_number(VGL_FAKEOPENCL PARENT_SCOPE)
+report_option(VGL_FAKEOPENCL "OpenCL interposer")
+if(VGL_FAKEOPENCL)
+	cmake_minimum_required(VERSION 3.1)
+	include(FindOpenCL)
+	include_directories(${OpenCL_INCLUDE_DIRS})
+endif()
+
 get_directory_property(DEFS_PROP COMPILE_DEFINITIONS)
 foreach(def ${DEFS_PROP})
 	set(DEFINES ${DEFINES};-D${def})
 endforeach()
+if(VGL_FAKEOPENCL)
+	set(DEFINES ${DEFINES};-DFAKEOPENCL)
+endif()
 add_custom_command(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/faker-mapfile
 	COMMAND ${CMAKE_C_COMPILER} ${DEFINES} -E
 		${CMAKE_CURRENT_SOURCE_DIR}/faker-mapfile.c
@@ -95,12 +91,18 @@ set(FAKER_SOURCES
 if(VGL_USEXV)
 	set(FAKER_SOURCES ${FAKER_SOURCES};XVTrans.cpp)
 endif()
-if(VGL_FAKEOPENCL)
-	set(FAKER_SOURCES ${FAKER_SOURCES};faker-ocl.cpp)
-endif()
 
-foreach(fakerlib ${VGL_FAKER_NAME} ${VGL_FAKER_NAME}-nodl)
-	add_library(${fakerlib} SHARED ${FAKER_SOURCES})
+set(FAKERLIBS ${VGL_FAKER_NAME};${VGL_FAKER_NAME}-nodl)
+if(VGL_FAKEOPENCL)
+	set(FAKERLIBS ${FAKERLIBS};${VGL_FAKER_NAME}-opencl)
+endif()
+foreach(fakerlib ${FAKERLIBS})
+	if(${fakerlib} STREQUAL ${VGL_FAKER_NAME}-opencl)
+		add_library(${fakerlib} SHARED ${FAKER_SOURCES} faker-ocl.cpp)
+		target_compile_definitions(${fakerlib} PUBLIC -DFAKEOPENCL)
+	else()
+		add_library(${fakerlib} SHARED ${FAKER_SOURCES})
+	endif()
 	if(${fakerlib} STREQUAL ${VGL_FAKER_NAME} AND
 		CMAKE_SYSTEM_NAME MATCHES "Linux")
 		# GCC 4.6 and later on Linux apparently passes --as-needed to the linker,
@@ -125,6 +127,9 @@ foreach(fakerlib ${VGL_FAKER_NAME} ${VGL_FAKER_NAME}-nodl)
 	if(${fakerlib} STREQUAL ${VGL_FAKER_NAME})
 		target_link_libraries(${fakerlib} ${OPENGL_gl_LIBRARY})
 	endif()
+	if(${fakerlib} STREQUAL ${VGL_FAKER_NAME}-opencl)
+		target_link_libraries(${fakerlib} ${OPENGL_gl_LIBRARY} ${OpenCL_LIBRARIES})
+	endif()
 	if(CMAKE_SYSTEM_NAME STREQUAL "SunOS" AND GNUCXX)
 		# For some reason, on SunOS/GCC CMake tries to use the C compiler to link
 		# C++ shared libs.  Grrr...  (NOTE: This was fixed in CMake v3.4, so we
@@ -135,6 +140,9 @@ foreach(fakerlib ${VGL_FAKER_NAME} ${VGL_FAKER_NAME}-nodl)
 endforeach()
 
 add_library(${VGL_DLFAKER_NAME} SHARED dlfaker.c)
+if(VGL_FAKEOPENCL)
+	target_compile_definitions(${VGL_DLFAKER_NAME} PUBLIC -DFAKEOPENCL)
+endif()
 target_link_libraries(${VGL_DLFAKER_NAME} ${LIBDL})
 install(TARGETS ${VGL_DLFAKER_NAME} DESTINATION ${CMAKE_INSTALL_LIBDIR})
 
@@ -185,12 +193,19 @@ target_link_libraries(vgltransut vglcommon ${FBXLIB} vglsocket
 	${TJPEG_LIBRARY})
 
 add_executable(dlfakerut dlfakerut.c)
+if(VGL_FAKEOPENCL)
+	target_compile_definitions(dlfakerut PUBLIC -DFAKEOPENCL)
+endif()
 target_link_libraries(dlfakerut ${X11_X11_LIB} ${LIBDL})
 
 add_library(GLdlfakerut SHARED libGLdlfakerut.c)
 
 if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
 	add_library(deepbindtest SHARED libdeepbindtest.c)
+	if(VGL_FAKEOPENCL)
+		target_compile_definitions(deepbindtest PUBLIC -DFAKEOPENCL)
+		target_link_libraries(deepbindtest ${OpenCL_LIBRARIES})
+	endif()
 	target_link_libraries(deepbindtest ${X11_X11_LIB} ${OPENGL_gl_LIBRARY}
 		${LIBDL})
 	set_target_properties(deepbindtest PROPERTIES LINK_FLAGS "${MINUSZ}defs")

server/dlfaker.c
@@ -60,10 +60,10 @@ static void setDLFakerLevel(int value)
 }
 
 
-/* If an application uses dlopen()/dlsym() to load functions from libGL or
-   libX11, this bypasses the LD_PRELOAD mechanism.  Thus, VirtualGL has to
-   intercept dlopen() and return a handle to itself rather than a handle to
-   libGL or libX11.
+/* If an application uses dlopen()/dlsym() to load functions from libGL,
+   libOpenCL, or libX11, this bypasses the LD_PRELOAD mechanism.  Thus,
+   VirtualGL has to intercept dlopen() and return a handle to itself rather
+   than a handle to libGL, libOpenCL, or libX11.
 
    NOTE: If the application tries to use dlopen() to obtain a handle to libdl,
    we similarly replace the handle with a handle to libdlfaker.  This works
@@ -74,6 +74,9 @@ void *dlopen(const char *filename, int flag)
 {
 	char *env = NULL, *env2 = NULL;  const char *envname = "FAKERLIB32";
 	int verbose = 0, trace = 0;
+	#ifdef FAKEOPENCL
+	int fakeOpenCL = 0;
+	#endif
 	void *retval = NULL;
 	FILE *file = stderr;
 
@@ -91,6 +94,10 @@ void *dlopen(const char *filename, int flag)
 		&& !strncmp(env2, "1", 1)) verbose = 1;
 	if((env2 = getenv("VGL_TRACE")) != NULL && strlen(env2) > 0
 		&& !strncmp(env2, "1", 1)) trace = 1;
+	#ifdef FAKEOPENCL
+	if((env2 = getenv("VGL_FAKEOPENCL")) != NULL && strlen(env2) > 0
+		&& !strncmp(env2, "1", 1)) fakeOpenCL = 1;
+	#endif
 	if((env2 = getenv("VGL_LOG")) != NULL && strlen(env2) > 0
 		&& !strcasecmp(env2, "stdout")) file = stdout;
 
@@ -105,9 +112,19 @@ void *dlopen(const char *filename, int flag)
 	#endif
 
 	if((env = getenv(envname)) == NULL || strlen(env) < 1)
+	{
+		#ifdef FAKEOPENCL
+		if(fakeOpenCL) env = "lib"VGL_FAKER_NAME"-opencl.so";
+		else
+		#endif
 		env = "lib"VGL_FAKER_NAME".so";
+	}
 	if(filename
 		&& (!strncmp(filename, "libGL.", 6) || strstr(filename, "/libGL.")
+			#ifdef FAKEOPENCL
+			|| !strncmp(filename, "libOpenCL.", 10)
+				|| strstr(filename, "/libOpenCL.")
+			#endif
 			|| !strncmp(filename, "libX11.", 7) || strstr(filename, "/libX11.")
 			|| (flag & RTLD_LAZY
 					&& (!strncmp(filename, "libopengl.", 10)

server/dlfakerut-test.c
@@ -15,7 +15,7 @@
 #include <dlfcn.h>
 
 
-int test(const char *testName);
+int test(const char *testName, int testOpenCL);
 
 
 static int checkWindowColor(Display *dpy, Window win, unsigned int color)
@@ -69,7 +69,7 @@ static int checkFrame(Display *dpy, Window win, int desiredReadbacks,
 }
 
 
-int test(const char *testName)
+int test(const char *testName, int testOpenCL)
 {
 	Display *dpy = NULL;  Window win = 0, root;
 	int dpyw, dpyh, lastFrame = 0, retval = 0;
@@ -77,6 +77,13 @@ int test(const char *testName)
 		GLX_GREEN_SIZE, 8, GLX_BLUE_SIZE, 8, None, None };
 	XVisualInfo *v = NULL;  GLXContext ctx = 0;
 	XSetWindowAttributes swa;
+	#ifdef FAKEOPENCL
+	cl_uint nPlatforms = 0, pi, nDevices = 0, di;
+	cl_context oclctx = 0;
+	cl_platform_id *platforms = NULL;
+	cl_device_id *devices = NULL;
+	cl_int oclerr;
+	#endif
 
 	fprintf(stderr, "%s:\n", testName);
 
@@ -108,15 +115,96 @@ int test(const char *testName)
 	_glXSwapBuffers(dpy, win);
 	TRY(checkFrame(dpy, win, 1, &lastFrame));
 	TRY(checkWindowColor(dpy, win, 0x0000ff));
+
+	#ifdef FAKEOPENCL
+
+	if(testOpenCL)
+	{
+		if((oclerr = _clGetPlatformIDs(0, NULL, &nPlatforms)) != CL_SUCCESS
+			&& oclerr != CL_PLATFORM_NOT_FOUND_KHR)
+			THROW("Could not get OpenCL platforms");
+		if(nPlatforms)
+		{
+			if((platforms =
+				(cl_platform_id *)malloc(sizeof(cl_platform_id) * nPlatforms)) == NULL)
+				THROW("Memory allocation error");
+			if(_clGetPlatformIDs(nPlatforms, platforms, NULL) != CL_SUCCESS)
+				THROW("Could not get OpenCL platforms");
+
+			for(pi = 0; pi < nPlatforms; pi++)
+			{
+				char name[256];
+
+				if(_clGetPlatformInfo(platforms[pi], CL_PLATFORM_NAME, 255, name,
+					NULL) != CL_SUCCESS)
+					THROW("Could not get OpenCL platform info");
+				printf("OpenCL platform:  %s\n", name);
+
+				if((oclerr = _clGetDeviceIDs(platforms[pi], CL_DEVICE_TYPE_GPU, 0,
+					NULL, &nDevices)) != CL_SUCCESS && oclerr != CL_DEVICE_NOT_FOUND)
+					THROW("Could not get OpenCL devices");
+				if(nDevices)
+				{
+					if((devices =
+						(cl_device_id *)malloc(sizeof(cl_device_id) * nDevices)) == NULL)
+							THROW("Memory allocation error");
+					if(_clGetDeviceIDs(platforms[pi], CL_DEVICE_TYPE_GPU, nDevices,
+						devices, NULL) != CL_SUCCESS)
+						THROW("Could not get OpenCL devices");
+
+					for(di = 0; di < nDevices; di++)
+					{
+						cl_context_properties properties[] = { CL_GL_CONTEXT_KHR, 0,
+							CL_GLX_DISPLAY_KHR, 0, CL_CONTEXT_PLATFORM, 0, 0 };
+
+						if(_clGetDeviceInfo(devices[di], CL_DEVICE_NAME, 255, name,
+							NULL) != CL_SUCCESS)
+							THROW("Could not get OpenCL device info");
+						printf("  Device:  %s\n", name);
+
+						properties[1] = (cl_context_properties)ctx;
+						properties[3] = (cl_context_properties)dpy;
+						properties[5] = (cl_context_properties)platforms[pi];
+						if((oclctx = _clCreateContext(properties, 1, &devices[di], NULL,
+							NULL, NULL)) == NULL)
+							THROW("Could not create OpenCL context");
+						properties[3] = 0;
+						if(_clGetContextInfo(oclctx, CL_CONTEXT_PROPERTIES,
+							sizeof(cl_context_properties) * 7, properties,
+							NULL) != CL_SUCCESS)
+							THROW("Could not get OpenCL context properties");
+						if(!properties[3] || properties[3] == (cl_context_properties)dpy)
+							THROW("clCreateContext() not correctly interposed");
+
+						_clReleaseContext(oclctx);  oclctx = 0;
+					}
+
+					free(devices);  devices = NULL;
+				}
+				else printf("  NO GPU DEVICES\n");
+			}
+
+			free(platforms);  platforms = NULL;
+		}
+		else printf("No OpenCL platforms\n");
+	}
+
+	#endif
+
 	printf("SUCCESS\n");
 
 	bailout:
+	#ifdef FAKEOPENCL
+	if(oclctx) _clReleaseContext(oclctx);
+	if(devices) free(devices);
+	if(platforms) free(platforms);
+	#endif
 	if(ctx && dpy)
 	{
-		_glXMakeCurrent(dpy, 0, 0);  _glXDestroyContext(dpy, ctx);  ctx = 0;
+		_glXMakeCurrent(dpy, 0, 0);  _glXDestroyContext(dpy, ctx);
 	}
-	if(win && dpy) { XDestroyWindow(dpy, win);  win = 0; }
-	if(v) { XFree(v);  v = NULL; }
-	if(dpy) { XCloseDisplay(dpy);  dpy = NULL; }
+	if(win && dpy) XDestroyWindow(dpy, win);
+	if(v) XFree(v);
+	if(dpy) XCloseDisplay(dpy);
 	return retval;
 }

server/dlfakerut.c
@@ -13,6 +13,9 @@
  */
 
 #include <GL/glx.h>
+#ifdef FAKEOPENCL
+#include <CL/opencl.h>
+#endif
 #include <X11/Xlib.h>
 #include <dlfcn.h>
 #include <string.h>
@@ -60,11 +63,46 @@ _glClearType _glClear = NULL;
 typedef void (*_glClearColorType)(GLclampf, GLclampf, GLclampf, GLclampf);
 _glClearColorType _glClearColor = NULL;
 
-void *gldllhnd = NULL;
+#ifdef FAKEOPENCL
 
-#define LSYM(s) \
+typedef cl_context (*_clCreateContextType)(cl_context_properties *, cl_uint,
+	const cl_device_id *, void (*)(const char *, const void *, size_t, void *),
+	void *, cl_int *);
+_clCreateContextType _clCreateContext = NULL;
+
+typedef cl_int (*_clGetContextInfoType)(cl_context, cl_context_info, size_t,
+	void *, size_t *);
+_clGetContextInfoType _clGetContextInfo = NULL;
+
+typedef cl_int (*_clGetDeviceIDsType)(cl_platform_id, cl_device_type, cl_uint,
+	cl_device_id *, cl_uint *);
+_clGetDeviceIDsType _clGetDeviceIDs = NULL;
+
+typedef cl_int (*_clGetDeviceInfoType)(cl_device_id, cl_device_info, size_t,
+	void *, size_t *);
+_clGetDeviceInfoType _clGetDeviceInfo = NULL;
+
+typedef cl_int (*_clGetPlatformIDsType)(cl_uint, cl_platform_id *, cl_uint *);
+_clGetPlatformIDsType _clGetPlatformIDs = NULL;
+
+typedef cl_int (*_clGetPlatformInfoType)(cl_platform_id, cl_platform_info,
+	size_t, void *, size_t *);
+_clGetPlatformInfoType _clGetPlatformInfo = NULL;
+
+typedef cl_int (*_clReleaseContextType)(cl_context);
+_clReleaseContextType _clReleaseContext = NULL;
+
+#endif
+
+void *gldllhnd = NULL;
+#ifdef FAKEOPENCL
+void *ocldllhnd = NULL;
+#endif
+int fakeOpenCL = 0;
+
+#define LSYM(dllhnd, s) \
 	dlerror(); \
-	_##s = (_##s##Type)dlsym(gldllhnd, #s); \
+	_##s = (_##s##Type)dlsym(dllhnd, #s); \
 	err = dlerror(); \
 	if(err) THROW(err) \
 	else if(!_##s) THROW("Could not load symbol " #s)
@@ -85,13 +123,39 @@ static int loadSymbols1(char *prefix)
 	if(err) THROW(err)
 	else if(!gldllhnd) THROW("Could not open libGL")
 
-	LSYM(glXChooseVisual);
-	LSYM(glXCreateContext);
-	LSYM(glXDestroyContext);
-	LSYM(glXMakeCurrent);
-	LSYM(glXSwapBuffers);
-	LSYM(glClear);
-	LSYM(glClearColor);
+	LSYM(gldllhnd, glXChooseVisual);
+	LSYM(gldllhnd, glXCreateContext);
+	LSYM(gldllhnd, glXDestroyContext);
+	LSYM(gldllhnd, glXMakeCurrent);
+	LSYM(gldllhnd, glXSwapBuffers);
+	LSYM(gldllhnd, glClear);
+	LSYM(gldllhnd, glClearColor);
+
+	#ifdef FAKEOPENCL
+
+	if(fakeOpenCL)
+	{
+		if(prefix)
+		{
+			char temps[256];
+			snprintf(temps, 255, "%s/libOpenCL.so", prefix);
+			ocldllhnd = dlopen(temps, RTLD_NOW);
+		}
+		else ocldllhnd = dlopen("libOpenCL.so", RTLD_NOW);
+		err = dlerror();
+		if(err) THROW(err)
+		else if(!ocldllhnd) THROW("Could not open libOpenCL")
+
+		LSYM(ocldllhnd, clCreateContext);
+		LSYM(ocldllhnd, clGetContextInfo);
+		LSYM(ocldllhnd, clGetDeviceIDs);
+		LSYM(ocldllhnd, clGetDeviceInfo);
+		LSYM(ocldllhnd, clGetPlatformIDs);
+		LSYM(ocldllhnd, clGetPlatformInfo);
+		LSYM(ocldllhnd, clReleaseContext);
+	}
+
+	#endif
 
 	bailout:
 	return retval;
@@ -100,6 +164,9 @@ static int loadSymbols1(char *prefix)
 static void unloadSymbols1(void)
 {
 	if(gldllhnd) dlclose(gldllhnd);
+	#ifdef FAKEOPENCL
+	if(ocldllhnd) dlclose(ocldllhnd);
+	#endif
 }
 
 
@@ -112,7 +179,7 @@ static int loadSymbols2(void)
 	const char *err = NULL;
 	int retval = 0;
 
-	LSYM(glXGetProcAddressARB);
+	LSYM(gldllhnd, glXGetProcAddressARB);
 	LSYM2(glXChooseVisual);
 	LSYM2(glXCreateContext);
 	LSYM2(glXDestroyContext);
@@ -143,7 +210,7 @@ static int nameMatchTest(void)
 	if(err) THROW(err)
 	else if(!gldllhnd) THROW("Could not open libGLdlfakerut")
 
-	LSYM(myTestFunction);
+	LSYM(gldllhnd, myTestFunction);
 	_myTestFunction();
 	dlclose(gldllhnd);
 	gldllhnd = NULL;
@@ -159,7 +226,7 @@ static int nameMatchTest(void)
 #ifdef RTLD_DEEPBIND
 /* Test whether libdlfaker.so properly circumvents RTLD_DEEPBIND */
 
-typedef void (*_testType)(const char *);
+typedef void (*_testType)(const char *, int);
 _testType _test = NULL;
 
 static int deepBindTest(void)
@@ -172,8 +239,8 @@ static int deepBindTest(void)
 	if(err) THROW(err)
 	else if(!gldllhnd) THROW("Could not open libdlfakerut")
 
-	LSYM(test);
-	_test("RTLD_DEEPBIND test");
+	LSYM(gldllhnd, test);
+	_test("RTLD_DEEPBIND test", fakeOpenCL);
 	dlclose(gldllhnd);
 	gldllhnd = NULL;
 
@@ -206,15 +273,19 @@ int main(int argc, char **argv)
 	env = getenv("LD_PRELOAD_64");
 	fprintf(stderr, "LD_PRELOAD_64 = %s\n", env ? env : "(NULL)");
 	#endif
+	#ifdef FAKEOPENCL
+	if((env = getenv("VGL_FAKEOPENCL")) != NULL && strlen(env) > 0
+		&& !strncmp(env, "1", 1)) fakeOpenCL = 1;
+	#endif
 
 	fprintf(stderr, "\n");
 	TRY(nameMatchTest());
 
 	TRY(loadSymbols1(prefix));
-	TRY(test("dlopen() test"));
+	TRY(test("dlopen() test", fakeOpenCL));
 
 	TRY(loadSymbols2());
-	TRY(test("glXGetProcAddressARB() test"));
+	TRY(test("glXGetProcAddressARB() test", fakeOpenCL));
 
 	unloadSymbols1();
 

commit 18a36c3960a1a8ceae036fe071cce541fc6d2911

rpm.spec.in: Fix x86-64/i386 RPM co-install issue
... caused by f3c9a34173b84825f7599173b802fded47044add.  For some
reason, disabling the internal dependency generator eliminates the
ability to co-install the x86-64 and i386 VirtualGL RPMs if those RPMs
are built on RHEL 6.  However, a cleaner way to prevent the RPMs from
requiring libOpenCL is to temporarily remove the execute permissions
from libvglfaker-opencl.so, since RPM only generates automatic
dependencies for files that have execute permissions.

release/rpm.spec.in
@@ -2,7 +2,6 @@
 # libnvidia-tls.so.1 if it was built on a system that has the NVidia
 # drivers installed.  The custom find-requires script is created during
 # install and removed during clean
-%define _use_internal_dependency_generator 0
 %define __find_requires %{_tmppath}/%{name}-%{version}-%{release}-find-requires
 
 # Path under which VirtualGL should be installed
@@ -173,8 +172,14 @@ fi
 	ln -fs %{docdir} $RPM_BUILD_ROOT/%{prefix}/doc
 %endif
 
-echo '/usr/lib/rpm/find-requires|grep -v libGLcore|grep -v libnvidia-tls|grep -v libOpenCL' >%{_tmppath}/%{name}-%{version}-%{release}-find-requires
+echo '/usr/lib/rpm/find-requires|grep -v libGLcore|grep -v libnvidia-tls' >%{_tmppath}/%{name}-%{version}-%{release}-find-requires
 chmod 755 %{_tmppath}/%{name}-%{version}-%{release}-find-requires
+
+# This prevents the RPM from requiring libOpenCL.
+%if %{fakeopencl}
+	find $RPM_BUILD_ROOT
+	chmod 644 $RPM_BUILD_ROOT/%{libdir}/lib@VGL_FAKER_NAME@-opencl.so
+%endif
 
 %clean
 rm -rf $RPM_BUILD_ROOT
@@ -239,7 +244,7 @@ rm %{_tmppath}/%{name}-%{version}-%{release}-find-requires
 %{libdir}/lib@VGL_FAKER_NAME@.so
 %{libdir}/lib@VGL_FAKER_NAME@-nodl.so
 %if %{fakeopencl}
-	%{libdir}/lib@VGL_FAKER_NAME@-opencl.so
+	%attr(755, -, -) %{libdir}/lib@VGL_FAKER_NAME@-opencl.so
 %endif
 %{libdir}/lib@VGL_DLFAKER_NAME@.so
 %{libdir}/lib@VGL_GEFAKER_NAME@.so