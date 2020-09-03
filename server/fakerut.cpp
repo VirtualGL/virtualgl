VirtualGL/virtualgl

commit ba112c12a9e11e1be16ba19702c0c6348d01e2e8

Faker: Don't return vis/FBC if OL/trans specified
Ensure that glXChooseFBConfig() and glXChooseVisual() return NULL if
GLX_LEVEL != 0, GLX_TRANSPARENT_TYPE != GLX_NONE, or
GLX_RENDER_TYPE == GLX_COLOR_INDEX_BIT.  Otherwise, the resulting visual
or framebuffer config almost certainly will not work as the application
expects.

server/fakerut.cpp
@@ -976,6 +976,14 @@ GLXFBConfig getFBConfigFromVisual(Display *dpy, XVisualInfo *vis)
 }
 
 
+#define TEST_ATTRIB(vattribs, cattribs, desc)  \
+	vis0 = glXChooseVisual(dpy, DefaultScreen(dpy), vattribs);  \
+	if(vis0) THROW(desc " visual found");  \
+	XFree(vis0);  vis0 = NULL;  \
+	configs = glXChooseFBConfig(dpy, DefaultScreen(dpy), cattribs, &n);  \
+	if(configs) THROW(desc " FB config found");  \
+	XFree(configs);  configs = NULL;  \
+
 // This tests the faker's client/server visual matching heuristics
 int visTest(void)
 {
@@ -994,6 +1002,17 @@ int visTest(void)
 			|| n == 0)
 			THROW("No FB configs found");
 		XFree(configs);  configs = NULL;
+
+		// These should fail, since we no longer support transparent overlays.
+		int attribs0[] = { GLX_LEVEL, 1, None };
+		TEST_ATTRIB(attribs0, attribs0, "Overlay")
+		int vattribs1[] = { None };
+		int cattribs1[] = { GLX_RENDER_TYPE, GLX_COLOR_INDEX_BIT, None };
+		TEST_ATTRIB(vattribs1, cattribs1, "CI")
+		int vattribs2[] = { GLX_RGBA, GLX_TRANSPARENT_TYPE, GLX_TRANSPARENT_RGB,
+			None };
+		int cattribs2[] = { GLX_TRANSPARENT_TYPE, GLX_TRANSPARENT_RGB, None };
+		TEST_ATTRIB(vattribs2, cattribs2, "Transparent")
 
 		try
 		{

server/glxvisual.cpp
@@ -492,7 +492,8 @@ VGLFBConfig *configsFromVisAttribs(Display *dpy, int screen,
 		{
 			renderType = attribs[i + 1];  i++;
 		}
-		else if(attribs[i] == GLX_LEVEL) i++;
+		else if(attribs[i] == GLX_LEVEL && attribs[++i] != 0)
+			return NULL;
 		else if(attribs[i] == GLX_STEREO)
 		{
 			if(glx13) { stereo = attribs[i + 1];  i++; }
@@ -514,7 +515,8 @@ VGLFBConfig *configsFromVisAttribs(Display *dpy, int screen,
 		{
 			alphaSize = attribs[i + 1];  i++;
 		}
-		else if(attribs[i] == GLX_TRANSPARENT_TYPE) i++;
+		else if(attribs[i] == GLX_TRANSPARENT_TYPE && attribs[++i] != GLX_NONE)
+			return NULL;
 		else if(attribs[i] == GLX_SAMPLES)
 		{
 			samples = attribs[i + 1];  i++;
@@ -536,6 +538,7 @@ VGLFBConfig *configsFromVisAttribs(Display *dpy, int screen,
 			i++;
 		}
 	}
+	if(renderType == GLX_COLOR_INDEX_BIT) return NULL;
 	if(doubleBuffer >= 0)
 	{
 		glxattribs[j++] = GLX_DOUBLEBUFFER;  glxattribs[j++] = doubleBuffer;

commit cb41ba8ec8f5e716778f7c286ba2e3b30574bd6d

Fix GLX_LEVEL/GLX_TRANS* FB config choosing error
Due to a regression introduced by
ba112c12a9e11e1be16ba19702c0c6348d01e2e8, an empty attribute list was
passed to glxvisual::chooseFBConfig() if the 3D application specified
GLX_LEVEL, 0 or GLX_TRANSPARENT_TYPE, GLX_NONE first in the attribute
list passed to glXChooseFBConfig() or glXChooseVisual().  This was
discovered while trying to run the Qt5 framebufferobject2 example.

server/fakerut.cpp
@@ -1072,7 +1072,7 @@ int visTest(void)
 				GLX_ACCUM_BLUE_SIZE, 0, GLX_ACCUM_ALPHA_SIZE, 0, GLX_SAMPLE_BUFFERS, 0,
 				GLX_SAMPLES, 1, GLX_RGBA, GLX_X_VISUAL_TYPE, GLX_NONE, None, None,
 				None };
-			int rgbattrib13[] = { GLX_DOUBLEBUFFER, 1, GLX_STEREO, 1,
+			int rgbattrib13[] = { GLX_LEVEL, 0, GLX_DOUBLEBUFFER, 1, GLX_STEREO, 1,
 				GLX_RED_SIZE, 8, GLX_GREEN_SIZE, 8, GLX_BLUE_SIZE, 8,
 				GLX_ALPHA_SIZE, 0, GLX_DEPTH_SIZE, 0, GLX_AUX_BUFFERS, 0,
 				GLX_STENCIL_SIZE, 0, GLX_ACCUM_RED_SIZE, 0, GLX_ACCUM_GREEN_SIZE, 0,
@@ -1083,53 +1083,56 @@ int visTest(void)
 			if(DefaultDepth(dpy, DefaultScreen(dpy)) == 30)
 			{
 				rgbattrib[1] = rgbattrib[3] = rgbattrib[5] = 10;
-				rgbattrib13[5] = rgbattrib13[7] = rgbattrib13[9] = 10;
+				rgbattrib13[7] = rgbattrib13[9] = rgbattrib13[11] = 10;
 			}
 
 			for(int db = 0; db <= 1; db++)
 			{
-				rgbattrib13[1] = db;
+				rgbattrib13[0] = db ? GLX_TRANSPARENT_TYPE : GLX_LEVEL;
+				rgbattrib13[1] = db ? GLX_NONE : 0;
+
+				rgbattrib13[3] = db;
 				rgbattrib[29] = db ? GLX_DOUBLEBUFFER : 0;
 
 				for(int stereo = 0; stereo <= 1; stereo++)
 				{
-					rgbattrib13[3] = stereo;
+					rgbattrib13[5] = stereo;
 					rgbattrib[db ? 30 : 29] = stereo ? GLX_STEREO : 0;
 
 					for(int alpha = 0; alpha <= 1; alpha++)
 					{
-						rgbattrib13[11] = rgbattrib[7] = alpha;
+						rgbattrib13[13] = rgbattrib[7] = alpha;
 
 						for(int depth = 0; depth <= 1; depth++)
 						{
-							rgbattrib13[13] = rgbattrib[9] = depth;
+							rgbattrib13[15] = rgbattrib[9] = depth;
 
 							for(int aux = 0; aux <= 1; aux++)
 							{
-								rgbattrib13[15] = rgbattrib[11] = aux;
+								rgbattrib13[17] = rgbattrib[11] = aux;
 
 								for(int stencil = 0; stencil <= 1; stencil++)
 								{
-									rgbattrib13[17] = rgbattrib[13] = stencil;
+									rgbattrib13[19] = rgbattrib[13] = stencil;
 
 									for(int accum = 0; accum <= 1; accum++)
 									{
-										rgbattrib13[19] = rgbattrib13[21] =
-											rgbattrib13[23] = accum;
+										rgbattrib13[21] = rgbattrib13[23] =
+											rgbattrib13[25] = accum;
 										rgbattrib[15] = rgbattrib[17] = rgbattrib[19] = accum;
-										if(alpha) { rgbattrib13[25] = rgbattrib[21] = accum; }
-										else { rgbattrib13[25] = rgbattrib[21] = 0; }
+										if(alpha) { rgbattrib13[27] = rgbattrib[21] = accum; }
+										else { rgbattrib13[27] = rgbattrib[21] = 0; }
 
 										for(int samples = 0; samples <= 16;
 											samples == 0 ? samples = 1 : samples *= 2)
 										{
-											rgbattrib13[29] = rgbattrib[25] = samples;
-											rgbattrib13[27] = rgbattrib[23] = samples ? 1 : 0;
+											rgbattrib13[31] = rgbattrib[25] = samples;
+											rgbattrib13[29] = rgbattrib[23] = samples ? 1 : 0;
 
 											for(int visualTypeIndex = 0; visualTypeIndex < 3;
 												visualTypeIndex++)
 											{
-												rgbattrib13[31] = rgbattrib[28] =
+												rgbattrib13[33] = rgbattrib[28] =
 													visualTypes[visualTypeIndex];
 												if((!(configs = glXChooseFBConfig(dpy,
 													DefaultScreen(dpy), rgbattrib13, &n)) || n == 0)

server/glxvisual.cpp
@@ -612,8 +612,11 @@ VGLFBConfig *configsFromVisAttribs(Display *dpy, int screen,
 		{
 			renderType = attribs[i + 1];  i++;
 		}
-		else if(attribs[i] == GLX_LEVEL && attribs[++i] != 0)
-			return NULL;
+		else if(attribs[i] == GLX_LEVEL)
+		{
+			if(attribs[i + 1] != 0) return NULL;
+			i++;
+		}
 		else if(attribs[i] == GLX_STEREO)
 		{
 			if(glx13) { stereo = attribs[i + 1];  i++; }
@@ -635,8 +638,11 @@ VGLFBConfig *configsFromVisAttribs(Display *dpy, int screen,
 		{
 			alphaSize = attribs[i + 1];  i++;
 		}
-		else if(attribs[i] == GLX_TRANSPARENT_TYPE && attribs[++i] != GLX_NONE)
-			return NULL;
+		else if(attribs[i] == GLX_TRANSPARENT_TYPE)
+		{
+			if(attribs[i + 1] != GLX_NONE) return NULL;
+			i++;
+		}
 		else if(attribs[i] == GLX_SAMPLES)
 		{
 			samples = attribs[i + 1];  i++;