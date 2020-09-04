VirtualGL/virtualgl

commit 2c9c9fb5a9b985c2a6f2d174626229bbe57b5a18

EGL back end: Fix XCB; require 2D X server GLX ext
If the 2D X server has no GLX extension, then there is no sane way to
emulate XCB-GLX with the EGL back end.  Our hackish attempt to handle
GLX errors for Xlib programs, in the absense of a GLX extension, was
problematic, because we were attempting to create a fictional X
extension that wasn't registered with the 2D X server.  Thus, the GLX
error strings weren't correct (causing fakerut to fail.)  That problem
could have been fixed by interposing XGetErrorText(), but there was
still no guarantee that we wouldn't stomp on a legitimate 2D X server
extension.  And it doesn't make much sense to support the EGL back end
with Xlib and not XCB.  As much as it would be nice to support the EGL
back end with 2D X servers that lack a GLX extension, the reality is
that most, if not all, such 2D X servers are obsolete.  TigerVNC has
always had a GLX extension, and it shouldn't be a big deal to require
TurboVNC 2.2+ for a next-gen VirtualGL feature.

server/fakerut.cpp
@@ -20,6 +20,11 @@
 #include <GL/glu.h>
 #include <X11/Xlib.h>
 #include <X11/keysym.h>
+#ifdef FAKEXCB
+	#include <xcb/xcb.h>
+	#include <xcb/glx.h>
+	#include <X11/Xlib-xcb.h>
+#endif
 #include <dlfcn.h>
 #include <unistd.h>
 #include "Error.h"
@@ -2592,6 +2597,22 @@ int extensionQueryTest(void)
 		if(!XQueryExtension(dpy, "GLX", &majorOpcode, &eventBase, &errorBase)
 			|| majorOpcode <= 0 || eventBase <= 0 || errorBase <= 0)
 			THROW("GLX Extension not reported as present");
+		#ifdef FAKEXCB
+		xcb_connection_t *conn = XGetXCBConnection(dpy);
+		if(!conn) THROW("Could not get XCB connection for display");
+		const xcb_query_extension_reply_t *qeReply =
+			xcb_get_extension_data(conn, &xcb_glx_id);
+		if(!qeReply || qeReply->major_opcode <= 0 || qeReply->first_event <= 0
+			|| qeReply->first_error <= 0)
+			THROW("GLX extension not reported as present in XCB");
+		xcb_glx_query_version_cookie_t cookie = xcb_glx_query_version(conn, 1, 4);
+		xcb_generic_error_t *error;
+		xcb_glx_query_version_reply_t *qvReply =
+			xcb_glx_query_version_reply(conn, cookie, &error);
+		if(!qvReply) THROW("Could not get GLX version in XCB");
+		if(qvReply->major_version != 1 || qvReply->minor_version != 4)
+			THROW("Incorrect GLX version returned in XCB");
+		#endif
 		// For some reason, the GLX error string table isn't initialized until a
 		// substantial GLX command completes.
 		if((configs = glXGetFBConfigs(dpy, DefaultScreen(dpy), &dummy1)) != NULL)

server/CMakeLists.txt
@@ -229,8 +229,14 @@ if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
 endif()
 
 add_executable(fakerut fakerut.cpp fakerut-supplement.c)
+if(VGL_FAKEXCB)
+	string(REGEX REPLACE "libX11" "libxcb" XCB_XCB_LIB ${X11_X11_LIB})
+	string(REGEX REPLACE "libX11" "libxcb-glx" XCB_GLX_LIB ${X11_X11_LIB})
+	string(REGEX REPLACE "libX11" "libX11-xcb" X11_XCB_LIB ${X11_X11_LIB})
+endif()
 target_link_libraries(fakerut "${MINUSZ}now ${OPENGL_gl_LIBRARY}"
-	${OPENGL_glu_LIBRARY} "${MINUSZ}now ${X11_X11_LIB}" ${LIBDL} vglutil)
+	${OPENGL_glu_LIBRARY} "${MINUSZ}now ${X11_X11_LIB}" ${LIBDL} vglutil
+	${XCB_XCB_LIB} ${XCB_GLX_LIB} ${X11_XCB_LIB})
 
 add_library(vgltrans_test SHARED testplugin.cpp VGLTrans.cpp)
 unset(VGLTRANS_TEST_LINK_FLAGS)

server/faker-x11.cpp
@@ -545,6 +545,8 @@ Bool XQueryExtension(Display *dpy, _Xconst char *name, int *major_opcode,
 {
 	Bool retval = True;
 
+	TRY();
+
 	if(IS_EXCLUDED(dpy))
 		return _XQueryExtension(dpy, name, major_opcode, first_event, first_error);
 
@@ -559,6 +561,8 @@ Bool XQueryExtension(Display *dpy, _Xconst char *name, int *major_opcode,
 		STOPTRACE();  if(major_opcode) PRARGI(*major_opcode);
 		if(first_event) PRARGI(*first_event);
 		if(first_error) PRARGI(*first_error);  CLOSETRACE();
+
+	CATCH();
 
 	return retval;
 }

server/faker-xcb.cpp
@@ -43,7 +43,8 @@ const xcb_query_extension_reply_t *
 			OPENTRACE(xcb_get_extension_data);  PRARGX(conn);  PRARGS(ext->name);
 			PRARGI(ext->global_id);  STARTTRACE();
 
-		xcb_connection_t *conn3D = _XGetXCBConnection(DPY3D);
+		xcb_connection_t *conn3D =
+			(fconfig.egl ? conn : _XGetXCBConnection(DPY3D));
 		if(conn3D != NULL)
 			reply = _xcb_get_extension_data(conn3D, _xcb_glx_id());
 
@@ -83,7 +84,7 @@ xcb_glx_query_version_cookie_t
 		OPENTRACE(xcb_glx_query_version);  PRARGX(conn);  PRARGI(major_version);
 		PRARGI(minor_version);  STARTTRACE();
 
-	xcb_connection_t *conn3D = _XGetXCBConnection(DPY3D);
+	xcb_connection_t *conn3D = (fconfig.egl ? conn : _XGetXCBConnection(DPY3D));
 	if(conn3D != NULL)
 		cookie = _xcb_glx_query_version(conn3D, major_version, minor_version);
 
@@ -110,7 +111,7 @@ xcb_glx_query_version_reply_t *
 		OPENTRACE(xcb_glx_query_version_reply);  PRARGX(conn);
 		STARTTRACE();
 
-	xcb_connection_t *conn3D = _XGetXCBConnection(DPY3D);
+	xcb_connection_t *conn3D = (fconfig.egl ? conn : _XGetXCBConnection(DPY3D));
 	if(conn3D != NULL)
 		reply = _xcb_glx_query_version_reply(conn3D, cookie, error);
 

server/vglwrap.cpp
@@ -802,14 +802,11 @@ Bool VGLQueryExtension(Display *dpy, int *majorOpcode, int *eventBase,
 		// and error base.
 		Bool retval = _XQueryExtension(dpy, "GLX", majorOpcode, eventBase,
 			errorBase);
-		if(retval) return retval;
-		// Otherwise, we have no choice but to create a work of fiction.  We
-		// assign the maximum possible values for the major opcode and error base,
-		// in order to minimize the odds of stomping on another X extension.
-		*majorOpcode = 255;
-		*eventBase = 0;
-		*errorBase = LastExtensionError - __GLX_NUMBER_ERRORS + 1;
-		return True;
+		// Otherwise, there is no sane way for the EGL back end to operate, mostly
+		// because of XCB.
+		if(!retval)
+			THROW("The EGL back end requires a 2D X server with a GLX extension.");
+		return retval;
 	}
 	// When using the GLX back end, all GLX errors will come from the 3D X
 	// server.

commit fa80f603eb3cfdbf9f9bc92c96728d4822b59bc3

fakerut: Fix minor memory leak
(introduced by 2c9c9fb5a9b985c2a6f2d174626229bbe57b5a18)

server/fakerut.cpp
@@ -2658,7 +2658,11 @@ int extensionQueryTest(void)
 			xcb_glx_query_version_reply(conn, cookie, &error);
 		if(!qvReply) THROW("Could not get GLX version in XCB");
 		if(qvReply->major_version != 1 || qvReply->minor_version != 4)
+		{
+			free(qvReply);
 			THROW("Incorrect GLX version returned in XCB");
+		}
+		free(qvReply);
 		#endif
 		// For some reason, the GLX error string table isn't initialized until a
 		// substantial GLX command completes.