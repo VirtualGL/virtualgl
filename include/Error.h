VirtualGL/virtualgl

commit aadc0f16c3c2a3425a718be953c19d62c6f71771

Overhaul exception handling
- Eliminate crufty NEWCHECK() macro.  As near as I can recall, this was
  a relic of whatever version of Visual Studio (probably 6.0) that I was
  using in 2004.  These days, the new operator in all of the compilers
  we support should be standards-compliant, meaning that it will throw
  std::bad_alloc rather than returning NULL.
- Derive our Error class from std::exception and always catch
  std::exception, so we can handle both VGL errors and standard C++
  errors (including the aforementioned std::bad_alloc) without
  additional code.

include/Error.h
@@ -23,12 +23,18 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
-#include <new>
+#include <exception>
+#include <typeinfo>
+
+
+#define GET_METHOD(e) \
+	(dynamic_cast <const vglutil::Error *>(&e) ? \
+		((vglutil::Error &)e).getMethod() : "C++")
 
 
 namespace vglutil
 {
-	class Error
+	class Error : public std::exception
 	{
 		public:
 
@@ -65,9 +71,15 @@ namespace vglutil
 			Error(void) : method(NULL) { message[0] = 0; }
 
 			operator bool() { return method != NULL && message[0] != 0; }
+			Error &operator= (const std::exception &e)
+			{
+				method = GET_METHOD(e);
+				strncpy(message, e.what(), MLEN + 1);
+				return *this;
+			}
 
 			const char *getMethod(void) { return method; }
-			char *getMessage(void) { return message; }
+			virtual const char *what(void) const throw() { return message; }
 
 		protected:
 
@@ -82,15 +94,6 @@ namespace vglutil
 #endif
 #define THROW(m)  throw(vglutil::Error(__FUNCTION__, m, __LINE__))
 #define ERRIFNOT(f)  { if(!(f)) THROW("Unexpected NULL condition"); }
-#define NEWCHECK(f) \
-	try \
-	{ \
-		if(!(f)) THROW("Memory allocation error"); \
-	} \
-	catch(std::bad_alloc &e) \
-	{ \
-		THROW(e.what()); \
-	}
 
 
 #ifdef _WIN32

client/ClientWin.cpp
@@ -43,7 +43,7 @@ ClientWin::ClientWin(int dpynum_, Window window_, int drawMethod_,
 	initGL();
 	initX11();
 
-	NEWCHECK(thread = new Thread(this));
+	thread = new Thread(this);
 	thread->start();
 }
 
@@ -79,13 +79,12 @@ void ClientWin::initGL(void)
 	{
 		try
 		{
-			NEWCHECK(newfb = new GLFrame(dpystr, window));
+			newfb = new GLFrame(dpystr, window);
 			if(!newfb) throw("Could not allocate class instance");
 		}
-		catch(Error &e)
+		catch(std::exception &e)
 		{
-			vglout.println("OpenGL error-- %s\nUsing X11 drawing instead",
-				e.getMessage());
+			vglout.println("OpenGL error-- %s\nUsing X11 drawing instead", e.what());
 			if(newfb) { delete newfb;  newfb = NULL; }
 			drawMethod = RR_DRAWX11;
 			vglout.PRINTLN("Stereo requires OpenGL drawing.  Disabling stereo.");
@@ -116,7 +115,7 @@ void ClientWin::initX11(void)
 	{
 		try
 		{
-			NEWCHECK(newfb = new FBXFrame(dpystr, window));
+			newfb = new FBXFrame(dpystr, window);
 			if(!newfb) throw("Could not allocate class instance");
 		}
 		catch(...)
@@ -156,7 +155,7 @@ Frame *ClientWin::getFrame(bool useXV)
 		{
 			char dpystr[80];
 			sprintf(dpystr, ":%d.0", dpynum);
-			NEWCHECK(xvframes[cfindex] = new XVFrame(dpystr, window));
+			xvframes[cfindex] = new XVFrame(dpystr, window);
 			if(!xvframes[cfindex]) THROW("Could not allocate class instance");
 		}
 		f = (Frame *)xvframes[cfindex];
@@ -256,7 +255,7 @@ void ClientWin::run(void)
 		}
 
 	}
-	catch(Error &e)
+	catch(std::exception &e)
 	{
 		if(thread) thread->setError(e);
 		if(f) f->signalComplete();

client/VGLTransReceiver.cpp
@@ -93,7 +93,7 @@ VGLTransReceiver::VGLTransReceiver(bool doSSL_, bool ipv6_, int drawMethod_) :
 
 	if((env = getenv("VGL_VERBOSE")) != NULL && strlen(env) > 0
 		&& !strncmp(env, "1", 1)) fbx_printwarnings(vglout.getFile());
-	NEWCHECK(thread = new Thread(this));
+	thread = new Thread(this);
 }
 
 
@@ -111,7 +111,7 @@ void VGLTransReceiver::listen(unsigned short port_)
 {
 	try
 	{
-		NEWCHECK(listenSocket = new Socket(doSSL, ipv6));
+		listenSocket = new Socket(doSSL, ipv6);
 		port = listenSocket->listen(port_);
 	}
 	catch(...)
@@ -135,14 +135,14 @@ void VGLTransReceiver::run(void)
 			socket = listenSocket->accept();  if(deadYet) break;
 			vglout.println("++ %sConnection from %s.", doSSL ? "SSL " : "",
 				socket->remoteName());
-			NEWCHECK(listener = new Listener(socket, drawMethod));
+			listener = new Listener(socket, drawMethod);
 			continue;
 		}
-		catch(Error &e)
+		catch(std::exception &e)
 		{
 			if(!deadYet)
 			{
-				vglout.println("%s-- %s", e.getMethod(), e.getMessage());
+				vglout.println("%s-- %s", GET_METHOD(e), e.what());
 				if(listener) delete listener;
 				if(socket) delete socket;
 				continue;
@@ -252,9 +252,9 @@ void VGLTransReceiver::Listener::run(void)
 			}
 		}
 	}
-	catch(Error &e)
+	catch(std::exception &e)
 	{
-		vglout.println("%s-- %s", e.getMethod(), e.getMessage());
+		vglout.println("%s-- %s", GET_METHOD(e), e.what());
 	}
 	if(thread) { thread->detach();  delete thread; }
 	delete this;
@@ -297,7 +297,7 @@ ClientWin *VGLTransReceiver::Listener::addWindow(int dpynum, Window win,
 	}
 	if(nwin >= MAXWIN) THROW("No free window IDs");
 	if(dpynum < 0 || dpynum > 65535 || win == None) THROW("Invalid argument");
-	NEWCHECK(windows[winid] = new ClientWin(dpynum, win, drawMethod, stereo));
+	windows[winid] = new ClientWin(dpynum, win, drawMethod, stereo);
 
 	if(!windows[winid]) THROW("Could not create window instance");
 	nwin++;

client/VGLTransReceiver.h
@@ -19,7 +19,6 @@
 #include "Socket.h"
 #include "ClientWin.h"
 #include "Log.h"
-#include "Error.h"
 
 
 #define MAXWIN  1024
@@ -59,7 +58,7 @@ namespace vglclient
 				{
 					memset(windows, 0, sizeof(ClientWin *) * MAXWIN);
 					if(socket) remoteName = socket->remoteName();
-					NEWCHECK(thread = new vglutil::Thread(this));
+					thread = new vglutil::Thread(this);
 					thread->start();
 				}
 

client/vglclient.cpp
@@ -106,7 +106,7 @@ void killproc(bool userOnly)
 
 		UNIX(sysctl(userOnly ? mib_user : mib_all, 4, NULL, &len, NULL, 0));
 		if(len < sizeof(kinfo_proc)) THROW("Process table is empty");
-		NEWCHECK(buf = new unsigned char[len]);
+		buf = new unsigned char[len];
 		UNIX(sysctl(userOnly ? mib_user : mib_all, 4, buf, &len, NULL, 0));
 		int nprocs = len / sizeof(kinfo_proc);
 		kinfo_proc *kp = (kinfo_proc *)buf;
@@ -404,9 +404,9 @@ int main(int argc, char *argv[])
 
 		if(start(displayname) < 0) return -1;
 	}
-	catch(Error &e)
+	catch(std::exception &e)
 	{
-		vglout.println("%s-- %s", e.getMethod(), e.getMessage());  exit(1);
+		vglout.println("%s-- %s", GET_METHOD(e), e.what());  exit(1);
 	}
 
 	return 0;
@@ -446,7 +446,7 @@ int start(char *displayname)
 			if(!force) actualSSLPort = instanceCheckSSL(maindpy);
 			if(actualSSLPort == 0)
 			{
-				NEWCHECK(sslReceiver = new VGLTransReceiver(true, ipv6, drawMethod));
+				sslReceiver = new VGLTransReceiver(true, ipv6, drawMethod);
 				if(sslPort == 0)
 				{
 					bool success = false;  unsigned short i = RR_DEFAULTSSLPORT;
@@ -485,7 +485,7 @@ int start(char *displayname)
 			if(!force) actualPort = instanceCheck(maindpy);
 			if(actualPort == 0)
 			{
-				NEWCHECK(receiver = new VGLTransReceiver(false, ipv6, drawMethod));
+				receiver = new VGLTransReceiver(false, ipv6, drawMethod);
 				if(port == 0)
 				{
 					bool success = false;  unsigned short i = RR_DEFAULTPORT;
@@ -543,9 +543,9 @@ int start(char *displayname)
 		}
 
 	}
-	catch(Error &e)
+	catch(std::exception &e)
 	{
-		vglout.println("%s-- %s", e.getMethod(), e.getMessage());
+		vglout.println("%s-- %s", GET_METHOD(e), e.what());
 		retval = -1;
 	}
 

common/Frame.cpp
@@ -79,7 +79,7 @@ void Frame::init(rrframeheader &h, int pixelFormat, int flags_, bool stereo_)
 		|| newpf->size != pf->size || !bits)
 	{
 		if(bits) delete [] bits;
-		NEWCHECK(bits = new unsigned char[h.framew * h.frameh * newpf->size + 1]);
+		bits = new unsigned char[h.framew * h.frameh * newpf->size + 1];
 	}
 	if(stereo_)
 	{
@@ -87,8 +87,7 @@ void Frame::init(rrframeheader &h, int pixelFormat, int flags_, bool stereo_)
 			|| newpf->size != pf->size || !rbits)
 		{
 			if(rbits) delete [] rbits;
-			NEWCHECK(rbits =
-				new unsigned char[h.framew * h.frameh * newpf->size + 1]);
+			rbits = new unsigned char[h.framew * h.frameh * newpf->size + 1];
 		}
 	}
 	else
@@ -131,7 +130,7 @@ Frame *Frame::getTile(int x, int y, int width, int height)
 		|| (y + height) > hdr.height)
 		throw Error("Frame::getTile", "Argument out of range");
 
-	NEWCHECK(f = new Frame(false));
+	f = new Frame(false);
 	f->hdr = hdr;
 	f->hdr.x = x;
 	f->hdr.y = y;
@@ -525,8 +524,7 @@ void CompressedFrame::init(rrframeheader &h, int buffer)
 			if(h.width != hdr.width || h.height != hdr.height || !bits)
 			{
 				if(bits) delete [] bits;
-				NEWCHECK(bits = new unsigned char[tjBufSize(h.width, h.height,
-					h.subsamp)]);
+				bits = new unsigned char[tjBufSize(h.width, h.height, h.subsamp)];
 			}
 			hdr = h;  hdr.flags = RR_LEFT;  stereo = true;
 			break;
@@ -534,8 +532,7 @@ void CompressedFrame::init(rrframeheader &h, int buffer)
 			if(h.width != rhdr.width || h.height != rhdr.height || !rbits)
 			{
 				if(rbits) delete [] rbits;
-				NEWCHECK(rbits = new unsigned char[tjBufSize(h.width, h.height,
-					h.subsamp)]);
+				rbits = new unsigned char[tjBufSize(h.width, h.height, h.subsamp)];
 			}
 			rhdr = h;  rhdr.flags = RR_RIGHT;  stereo = true;
 			break;
@@ -543,8 +540,7 @@ void CompressedFrame::init(rrframeheader &h, int buffer)
 			if(h.width != hdr.width || h.height != hdr.height || !bits)
 			{
 				if(bits) delete [] bits;
-				NEWCHECK(bits = new unsigned char[tjBufSize(h.width, h.height,
-					h.subsamp)]);
+				bits = new unsigned char[tjBufSize(h.width, h.height, h.subsamp)];
 			}
 			hdr = h;  hdr.flags = 0;  stereo = false;
 			break;

common/frameut.cpp
@@ -62,13 +62,13 @@ class Blitter : public Runnable
 		{
 			for(int i = 0; i < NFRAMES; i++)
 			{
-				if(useGL) { NEWCHECK(frames[i] = new GLFrame(dpy, win)); }
+				if(useGL) { frames[i] = new GLFrame(dpy, win); }
 				#ifdef USEXV
-				else if(useXV) { NEWCHECK(frames[i] = new XVFrame(dpy, win)); }
+				else if(useXV) { frames[i] = new XVFrame(dpy, win); }
 				#endif
-				else { NEWCHECK(frames[i] = new FBXFrame(dpy, win)); }
+				else { frames[i] = new FBXFrame(dpy, win); }
 			}
-			NEWCHECK(thread = new Thread(this));
+			thread = new Thread(this);
 			thread->start();
 		}
 
@@ -150,7 +150,7 @@ class Blitter : public Runnable
 				fprintf(stderr, "Average Blitter performance = %f Mpixels/sec%s\n",
 					mpixels / totalTime, check ? " [PASSED]" : "");
 			}
-			catch(Error &e)
+			catch(std::exception &e)
 			{
 				if(thread) thread->setError(e);
 				for(int i = 0; i < NFRAMES; i++)
@@ -216,7 +216,7 @@ class Decompressor : public Runnable
 			blitter(blitter_), findex(0), deadYet(false), dpy(dpy_), win(win_),
 			myID(myID_), thread(NULL)
 		{
-			NEWCHECK(thread = new Thread(this));
+			thread = new Thread(this);
 			thread->start();
 		}
 
@@ -276,7 +276,7 @@ class Decompressor : public Runnable
 					cframe.signalComplete();
 				}
 			}
-			catch(Error &e)
+			catch(std::exception &e)
 			{
 				if(thread) thread->setError(e);
 				for(int i = 0; i < NFRAMES; i++) cframes[i].signalComplete();
@@ -301,7 +301,7 @@ class Compressor : public Runnable
 			deadYet(false), thread(NULL), decompressor(decompressor_),
 			blitter(blitter_)
 		{
-			NEWCHECK(thread = new Thread(this));
+			thread = new Thread(this);
 			thread->start();
 		}
 
@@ -379,7 +379,7 @@ class Compressor : public Runnable
 					frame.signalComplete();
 				}
 			}
-			catch(Error &e)
+			catch(std::exception &e)
 			{
 				if(thread) thread->setError(e);
 				for(int i = 0; i < NFRAMES; i++) frames[i].signalComplete();
@@ -410,10 +410,10 @@ class FrameTest
 				BlackPixel(dpy, DefaultScreen(dpy))));
 			ERRIFNOT(XMapRaised(dpy, win));
 
-			NEWCHECK(blitter = new Blitter(dpy, win, myID));
+			blitter = new Blitter(dpy, win, myID);
 			if(!useXV)
-				NEWCHECK(decompressor = new Decompressor(blitter, dpy, win, myID));
-			NEWCHECK(compressor = new Compressor(decompressor, blitter));
+				decompressor = new Decompressor(blitter, dpy, win, myID);
+			compressor = new Compressor(decompressor, blitter);
 		}
 
 		~FrameTest(void) { shutdown();  XDestroyWindow(dpy, win); }
@@ -662,7 +662,7 @@ int main(int argc, char **argv)
 
 			for(i = 0; i < NUMWIN; i++)
 			{
-				NEWCHECK(test[i] = new FrameTest(dpy, i));
+				test[i] = new FrameTest(dpy, i);
 			}
 
 			for(w = MINW; w <= MAXW; w += 33)
@@ -708,9 +708,9 @@ int main(int argc, char **argv)
 			fprintf(stderr, "\n");
 		}
 	}
-	catch(Error &e)
+	catch(std::exception &e)
 	{
-		fprintf(stderr, "%s\n%s\n", e.getMethod(), e.getMessage());
+		fprintf(stderr, "%s\n%s\n", GET_METHOD(e), e.what());
 		exit(1);
 	}
 	return 0;

include/Thread.h
@@ -1,6 +1,6 @@
 /* Copyright (C)2004 Landmark Graphics Corporation
  * Copyright (C)2005 Sun Microsystems, Inc.
- * Copyright (C)2014 D. R. Commander
+ * Copyright (C)2014, 2019 D. R. Commander
  *
  * This library is free software and may be redistributed and/or modified under
  * the terms of the wxWindows Library License, Version 3.1 or (at your option)
@@ -53,7 +53,7 @@ namespace vglutil
 			void stop(void);
 			// This allows a Unix thread to kill itself.  It has no effect on Windows.
 			void detach(void);
-			void setError(Error &e);
+			void setError(std::exception &e);
 			void checkError(void);
 
 			static unsigned long threadID(void)

commit c03c6ebddd897650fbbef7c04ecb09c76b8e8720

Error.h: Fix compiler warning w/ GCC 10
(regression introduced by aadc0f16c3c2a3425a718be953c19d62c6f71771)

include/Error.h
@@ -1,6 +1,6 @@
 // Copyright (C)2004 Landmark Graphics Corporation
 // Copyright (C)2005 Sun Microsystems, Inc.
-// Copyright (C)2014, 2019 D. R. Commander
+// Copyright (C)2014, 2019-2020 D. R. Commander
 //
 // This library is free software and may be redistributed and/or modified under
 // the terms of the wxWindows Library License, Version 3.1 or (at your option)
@@ -73,7 +73,7 @@ namespace vglutil
 			Error &operator= (const std::exception &e)
 			{
 				method = GET_METHOD(e);
-				strncpy(message, e.what(), MLEN + 1);
+				strncpy(message, e.what(), MLEN);
 				return *this;
 			}
 
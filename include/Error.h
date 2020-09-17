c03c6ebddd897650fbbef7c04ecb09c76b8e8720
DRC <information@virtualgl.org>
2020-09-17 15:12:05 -0500

    Error.h: Fix compiler warning w/ GCC 10
    (regression introduced by aadc0f16c3c2a3425a718be953c19d62c6f71771)

--- include/Error.h
+++ include/Error.h
@@ -1,6 +1,6 @@
 // Copyright (C)2004 Landmark Graphics Corporation
 // Copyright (C)2005 Sun Microsystems, Inc.
-// Copyright (C)2014, 2019 D. R. Commander
+// Copyright (C)2014, 2019-2020 D. R. Commander
 //
 // This library is free software and may be redistributed and/or modified under
 // the terms of the wxWindows Library License, Version 3.1 or (at your option)
@@ -73,7 +73,7 @@ namespace vglutil
 			Error &operator= (const std::exception &e)
 			{
 				method = GET_METHOD(e);
-				strncpy(message, e.what(), MLEN + 1);
+				strncpy(message, e.what(), MLEN);
 				return *this;
 			}
 
