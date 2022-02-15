VirtualGL/virtualgl

commit 2bcdb66e930ac8785f363e2ae1fed054047e88da

EGLBE: Fix glXCreateContextAttribsARB BadValue err
... when GLX_RENDER_TYPE was specified as an attribute.  This caused
JOGL to fail when using the EGL back end.

server/backend.cpp
@@ -227,12 +227,12 @@ GLXContext createContext(Display *dpy, VGLFBConfig config, GLXContext share,
 								true);
 							return NULL;
 						}
-						else
+						else if(glxAttribs[glxi] != GLX_RENDER_TYPE)
 						{
 							faker::sendGLXError(dpy, X_GLXCreateContextAttribsARB, BadValue,
 								true);
+							return NULL;
 						}
-						return NULL;
 				}
 			}
 		}

server/fakerut.cpp
@@ -2401,9 +2401,9 @@ int contextMismatchTest(void)
 				PFNGLXCREATECONTEXTATTRIBSARBPROC __glXCreateContextAttribsARB =
 					(PFNGLXCREATECONTEXTATTRIBSARBPROC)glXGetProcAddress(
 						(const GLubyte *)"glXCreateContextAttribsARB");
-					if(__glXCreateContextAttribsARB)
-						ctx2 = __glXCreateContextAttribsARB(dpy, config2, NULL, True,
-							attribs);
+				if(__glXCreateContextAttribsARB)
+					ctx2 = __glXCreateContextAttribsARB(dpy, config2, NULL, True,
+						attribs);
 			}
 			if(!ctx2)
 				ctx2 = glXCreateNewContext(dpy, config2, GLX_RGBA_TYPE, NULL, True);
@@ -2444,6 +2444,21 @@ int contextMismatchTest(void)
 				if(notificationStrategy != attribs[9])
 					PRERROR2("Reset notification strategy 0x%.8x != 0x%.8x",
 						notificationStrategy, attribs[9]);
+			}
+			// Make sure that glXCreateContextAttribsARB() accepts GLX_RENDER_TYPE
+			// as an attribute.  This will fail when using the EGL back end with
+			// VirtualGL 3.0.
+			if(GLX_EXTENSION_EXISTS(GLX_ARB_create_context))
+			{
+				int attribs2[] = { GLX_RENDER_TYPE, GLX_RGBA_TYPE, None };
+				glXDestroyContext(dpy, ctx2);  ctx2 = 0;
+				PFNGLXCREATECONTEXTATTRIBSARBPROC __glXCreateContextAttribsARB =
+					(PFNGLXCREATECONTEXTATTRIBSARBPROC)glXGetProcAddress(
+						(const GLubyte *)"glXCreateContextAttribsARB");
+				if(__glXCreateContextAttribsARB)
+					ctx2 = __glXCreateContextAttribsARB(dpy, config2, NULL, True,
+						attribs2);
+				if(!ctx2) THROW("Could not create context");
 			}
 
 			printf("SUCCESS\n");

commit 518d29c7dfb08d2ab0cb55e46185ca5b79de4bb7

EGLBE: Fix piglit invalid render type test
(broken by 2bcdb66e930ac8785f363e2ae1fed054047e88da)

server/backend.cpp
@@ -227,7 +227,8 @@ GLXContext createContext(Display *dpy, VGLFBConfig config, GLXContext share,
 								true);
 							return NULL;
 						}
-						else if(glxAttribs[glxi] != GLX_RENDER_TYPE)
+						else if(glxAttribs[glxi] != GLX_RENDER_TYPE
+							|| glxAttribs[glxi + 1] != GLX_RGBA_TYPE)
 						{
 							faker::sendGLXError(dpy, X_GLXCreateContextAttribsARB, BadValue,
 								true);