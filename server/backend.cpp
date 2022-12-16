VirtualGL/virtualgl

commit 2e94a75f82a1f99b615b68a0d4e3f616cebb9527

EGLBE: Destroy RBO ctx when it is no longer needed
This prevents OpenGL objects from outliving both the context and the
drawable in which they were created.

Fixes #218

server/backend.cpp
@@ -275,7 +275,7 @@ GLXContext createContext(Display *dpy, VGLFBConfig config, GLXContext share,
 			}
 			if(!share)
 			{
-				getRBOContext(dpy).createContext();
+				getRBOContext(dpy).createContext(RBOContext::REFCOUNT_CONTEXT);
 				share = (GLXContext)getRBOContext(dpy).getContext();
 			}
 			if(!_eglBindAPI(EGL_OPENGL_API))
@@ -341,6 +341,7 @@ void destroyContext(Display *dpy, GLXContext ctx)
 			if(!ctx) return;
 			VGLFBConfig config = ctxhashegl.findConfig(ctx);
 			ctxhashegl.remove(ctx);
+			getRBOContext(dpy).destroyContext(RBOContext::REFCOUNT_CONTEXT);
 			if(!_eglBindAPI(EGL_OPENGL_API))
 				THROW("Could not enable OpenGL API");
 			if(!_eglDestroyContext(EDPY, (EGLContext)ctx))

CMakeLists.txt
@@ -14,7 +14,7 @@ else()
 	project(VirtualGL)
 endif()
 string(TOLOWER ${CMAKE_PROJECT_NAME} CMAKE_PROJECT_NAME_LC)
-set(VERSION 3.0.90)
+set(VERSION 3.0.91)
 set(DOCVERSION 3.1)
 
 macro(boolean_number var)

server/FakePbuffer.cpp
@@ -53,7 +53,7 @@ FakePbuffer::FakePbuffer(Display *dpy_, VGLFBConfig config_,
 
 	try
 	{
-		getRBOContext(dpy).createContext();
+		getRBOContext(dpy).createContext(RBOContext::REFCOUNT_DRAWABLE);
 		createBuffer(true);
 		CriticalSection::SafeLock l(idMutex);
 		id = nextID++;
@@ -176,16 +176,21 @@ void FakePbuffer::destroy(bool errorCheck)
 	try
 	{
 		CriticalSection::SafeLock l(getRBOContext(dpy).getMutex());
-		TempContextEGL tc(getRBOContext(dpy).getContext());
 
-		_glBindFramebuffer(GL_FRAMEBUFFER, 0);
-		_glBindRenderbuffer(GL_RENDERBUFFER, 0);
-		for(int i = 0; i < 4; i++)
 		{
-			if(rboc[i]) { _glDeleteRenderbuffers(1, &rboc[i]);  rboc[i] = 0; }
+			TempContextEGL tc(getRBOContext(dpy).getContext());
+
+			_glBindFramebuffer(GL_FRAMEBUFFER, 0);
+			_glBindRenderbuffer(GL_RENDERBUFFER, 0);
+			for(int i = 0; i < 4; i++)
+			{
+				if(rboc[i]) { _glDeleteRenderbuffers(1, &rboc[i]);  rboc[i] = 0; }
+			}
+			if(rbod) { _glDeleteRenderbuffers(1, &rbod);  rbod = 0; }
+			if(fbo) { _glDeleteFramebuffers(1, &fbo);  fbo = 0; }
 		}
-		if(rbod) { _glDeleteRenderbuffers(1, &rbod);  rbod = 0; }
-		if(fbo) { _glDeleteFramebuffers(1, &fbo);  fbo = 0; }
+
+		getRBOContext(dpy).destroyContext(RBOContext::REFCOUNT_DRAWABLE);
 	}
 	catch(std::exception &e)
 	{

server/RBOContext.h
@@ -1,4 +1,4 @@
-// Copyright (C)2020-2021 D. R. Commander
+// Copyright (C)2020-2022 D. R. Commander
 //
 // This library is free software and may be redistributed and/or modified under
 // the terms of the wxWindows Library License, Version 3.1 or (at your option)
@@ -25,11 +25,14 @@ class RBOContext
 {
 	public:
 
-		RBOContext() : ctx(0)
+		static const int REFCOUNT_CONTEXT = 1 << 0;
+		static const int REFCOUNT_DRAWABLE = 1 << 1;
+
+		RBOContext() : ctx(0), contextRefCount(0), drawableRefCount(0)
 		{
 		}
 
-		void createContext(void)
+		void createContext(int refCounts)
 		{
 			util::CriticalSection::SafeLock l(mutex);
 
@@ -39,6 +42,33 @@ class RBOContext
 					THROW("Could not enable OpenGL API");
 				ctx = _eglCreateContext(EDPY, (EGLConfig)0, NULL, NULL);
 				if(!ctx) THROW_EGL("eglCreateContext()");
+			}
+
+			if(refCounts & REFCOUNT_CONTEXT) contextRefCount++;
+			if(refCounts & REFCOUNT_DRAWABLE) drawableRefCount++;
+		}
+
+		void destroyContext(int refCounts, bool force = false)
+		{
+			util::CriticalSection::SafeLock l(mutex);
+
+			if(refCounts & REFCOUNT_CONTEXT)
+			{
+				contextRefCount--;
+				if(contextRefCount < 0) contextRefCount = 0;
+			}
+			if(refCounts & REFCOUNT_DRAWABLE)
+			{
+				drawableRefCount--;
+				if(drawableRefCount < 0) drawableRefCount = 0;
+			}
+
+			if(ctx && (force || (contextRefCount == 0 && drawableRefCount == 0)))
+			{
+				if(_eglBindAPI(EGL_OPENGL_API))
+					_eglDestroyContext(EDPY, ctx);
+				ctx = 0;
+				contextRefCount = drawableRefCount = 0;
 			}
 		}
 
@@ -50,12 +80,7 @@ class RBOContext
 		{
 			util::CriticalSection::SafeLock l(mutex);
 
-			if(ctx)
-			{
-				if(_eglBindAPI(EGL_OPENGL_API))
-					_eglDestroyContext(EDPY, ctx);
-				ctx = 0;
-			}
+			destroyContext(REFCOUNT_CONTEXT | REFCOUNT_DRAWABLE, true);
 		}
 
 	private:
@@ -73,10 +98,23 @@ class RBOContext
 		// allows those contexts to be used for rendering into the RBOs, even
 		// though those contexts were not current when the RBOs were created.
 		EGLContext ctx;
+		// The context reference count is incremented in the body of
+		// glXCreate*Context*() and decremented in the body of glXDestroyContext().
+		// The drawable reference count is incremented when the EGL back end
+		// creates a Pbuffer (either an application-requested Pbuffer or a Pbuffer
+		// that the faker uses to emulate an OpenGL window) and decremented when
+		// the EGL back end destroys a Pbuffer.  When both reference counts reach
+		// 0, the RBO context is destroyed.  That prevents per-context objects,
+		// such as textures, from surviving longer than the GLX drawable and
+		// context in which they were created.  (Note, however, that this behavior
+		// is technically incorrect, as OpenGL objects are supposed to survive only
+		// as long as the context in which they were created.  It's the best we can
+		// do given the limitations of emulating Pbuffers using RBOs.)
+		int contextRefCount, drawableRefCount;
 		// Mutex for the RBO context.  This guards any operations that alter the
-		// context handle above, or any operations that occur while the RBO context
-		// is current (an OpenGL context can only be current in one thread at a
-		// time.)
+		// context handle or reference counts above, or any operations that occur
+		// while the RBO context is current (an OpenGL context can only be current
+		// in one thread at a time.)
 		util::CriticalSection mutex;
 };
 

commit 03840b56415e04e7da9db93614ea63d5c2071ac5

EGLBE: Incr. RBO ctx ref. when creating shared ctx
If a context is shared with another context, then it is indirectly
shared with the RBO context, so in the body of glXCreate*Context*()
(i.e. in the body of backend::createContext()), we should always call
RBOContext::createContext() regardless of whether 'share' is NULL.  If
'share' is not NULL, then the RBO context will have already been created
in a previous call to glXCreate*Context*(), so calling
RBOContext::createContext() again will increment the context reference
count for the RBO context.  That is necessary because (a) the RBO
context won't truly be unused until all fake Pbuffers and all contexts
that are shared with it (either directly or indirectly) are destroyed,
and (b) the reference count is always decremented in the body of
glXDestroyContext(), regardless of whether the context is shared.

This fixes GL_INVALID_OPERATION or GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT
errors (regression caused by 2e94a75f82a1f99b615b68a0d4e3f616cebb9527)
that occurred in FakePbuffer::createBuffer() on certain systems when a
Qt OpenGL window was destroyed and another was created.

server/backend.cpp
@@ -273,11 +273,9 @@ GLXContext createContext(Display *dpy, VGLFBConfig config, GLXContext share,
 				faker::sendGLXError(dpy, minorCode, GLXBadFBConfig, false);
 				return NULL;
 			}
+			getRBOContext(dpy).createContext(RBOContext::REFCOUNT_CONTEXT);
 			if(!share)
-			{
-				getRBOContext(dpy).createContext(RBOContext::REFCOUNT_CONTEXT);
 				share = (GLXContext)getRBOContext(dpy).getContext();
-			}
 			if(!_eglBindAPI(EGL_OPENGL_API))
 				THROW("Could not enable OpenGL API");
 			GLXContext ctx = (GLXContext)_eglCreateContext(EDPY, (EGLConfig)0,