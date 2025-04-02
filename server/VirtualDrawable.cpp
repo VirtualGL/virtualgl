VirtualGL/virtualgl

commit ac29587d377fc8eaf21f8012ed23b798365c39ce

Faker: Don't clear new Pbuffer if drawing to FBO
Since the beginning, VirtualGL has cleared new "virtual window" Pbuffers
(Pbuffers that it creates to emulate OpenGL windows) when the Pbuffers
are made current for the first time.  Unlike new X windows, the contents
of new Pbuffers are undefined, and "back in the day", some OpenGL
implementations did not clear new Pbuffers.  Applications were
particularly economical with their 3D rendering back then, and some of
them relied on the fact that new X windows start out blank.  Thus,
VirtualGL had to clear new virtual window Pbuffers, particularly ones
that it created in midstream (in response to a window resize that
occurred between glXSwapBuffers() or glXMakeCurrent() calls.)  Since the
Pbuffer clearing code was introduced before VirtualGL was open sourced,
information about the specific OpenGL implementations and 3D
applications that necessitated it has been lost to history.  Clearing
new virtual window Pbuffers is likely never necessary in 2024, and it
definitely isn't necessary unless the default framebuffer is bound to
the GL_DRAW_FRAMEBUFFER target.  (FBOs weren't a thing "back in the
day.")

VirtualGL handles midstream window resizes in the body of glViewport(),
but because FBOs and their bindings are attached to a specific context
rather than a specific drawable, the first glViewport() call that a 3D
application makes after its window is resized might be made while an FBO
is bound to the GL_DRAW_FRAMEBUFFER target.  That is the case with
Firefox, so VirtualGL's attempt to clear the new virtual window Pbuffer
that it created in the body of glViewport() cleared part of an FBO
instead, resulting in the disappearance of random characters or other
elements of the Firefox window.

Modern OpenGL implementations seem to clear new Pbuffers, at least in my
limited testing, so we could probably get away with jettisoning the
Pbuffer clearing code altogether.  However, the minimally invasive fix

server/VirtualDrawable.cpp
@@ -1,6 +1,6 @@
 // Copyright (C)2004 Landmark Graphics Corporation
 // Copyright (C)2005, 2006 Sun Microsystems, Inc.
-// Copyright (C)2009-2015, 2017-2020 D. R. Commander
+// Copyright (C)2009-2015, 2017-2020, 2024 D. R. Commander
 //
 // This library is free software and may be redistributed and/or modified under
 // the terms of the wxWindows Library License, Version 3.1 or (at your option)
@@ -169,6 +169,9 @@ void VirtualDrawable::OGLDrawable::clear(void)
 {
 	if(cleared) return;
 	cleared = true;
+	int drawFBO = -1;
+	glGetIntegerv(GL_DRAW_FRAMEBUFFER_BINDING, &drawFBO);
+	if(drawFBO != 0) return;
 	GLfloat params[4];
 	_glGetFloatv(GL_COLOR_CLEAR_VALUE, params);
 	_glClearColor(0, 0, 0, 0);

commit 9f6add469a1ddd466eb24b308682039e74b0b613

Fix build error introduced by ac29587d3
ac29587d377fc8eaf21f8012ed23b798365c39ce was cherry picked from
182c9edbf636a909350073e49c6f1fbc3a50453b, which intentionally calls
the fake version of glGetIntegerv() in order to automatically handle the
EGL back end, but VirtualGL 2.6.x doesn't have a fake version of
glGetIntegerv().

server/VirtualDrawable.cpp
@@ -1,6 +1,6 @@
 // Copyright (C)2004 Landmark Graphics Corporation
 // Copyright (C)2005, 2006 Sun Microsystems, Inc.
-// Copyright (C)2009-2015, 2017-2020, 2024 D. R. Commander
+// Copyright (C)2009-2015, 2017-2020, 2024-2025 D. R. Commander
 //
 // This library is free software and may be redistributed and/or modified under
 // the terms of the wxWindows Library License, Version 3.1 or (at your option)
@@ -170,7 +170,7 @@ void VirtualDrawable::OGLDrawable::clear(void)
 	if(cleared) return;
 	cleared = true;
 	int drawFBO = -1;
-	glGetIntegerv(GL_DRAW_FRAMEBUFFER_BINDING, &drawFBO);
+	_glGetIntegerv(GL_DRAW_FRAMEBUFFER_BINDING, &drawFBO);
 	if(drawFBO != 0) return;
 	GLfloat params[4];
 	_glGetFloatv(GL_COLOR_CLEAR_VALUE, params);