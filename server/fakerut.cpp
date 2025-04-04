VirtualGL/virtualgl

commit 736d95f76217ac791db0a585a13ecd648adcb39c

Faker: Interpose XCB window creation/destr. funcs
We need to track X windows created using the XCB API.  Otherwise, if one
of those windows is used with GLX, the faker erroneously assumes that
the window was created in another process, which causes the faker to
create a VirtualWin instance that is not destroyed until the display
connection is closed or the application exits.  Since VirtualGL
maintains as many as four X display connections for each VirtualWin
instance, failing to destroy VirtualWin instances in a timely manner can
lead to X resource exhaustion if an application attempts to create and
destroy hundreds of X windows using the same X display connection.

Fixes #195

server/fakerut.cpp
@@ -1,6 +1,6 @@
 // Copyright (C)2004 Landmark Graphics Corporation
 // Copyright (C)2005, 2006 Sun Microsystems, Inc.
-// Copyright (C)2010-2015, 2017-2021 D. R. Commander
+// Copyright (C)2010-2015, 2017-2022 D. R. Commander
 //
 // This library is free software and may be redistributed and/or modified under
 // the terms of the wxWindows Library License, Version 3.1 or (at your option)
@@ -21,6 +21,10 @@
 #include <X11/Xlib.h>
 #include <X11/XKBlib.h>
 #include <X11/keysym.h>
+#ifdef FAKEXCB
+	#include <xcb/xcb.h>
+	#include <X11/Xlib-xcb.h>
+#endif
 #include <dlfcn.h>
 #include <unistd.h>
 #include "Error.h"
@@ -2453,6 +2457,7 @@ int copyContextTest(void)
 int subWinTest(void)
 {
 	Display *dpy = NULL;  Window win = 0, win1 = 0, win2 = 0;
+	xcb_connection_t *conn = NULL;
 	TestColor clr(0);
 	int dpyw, dpyh, retval = 1, lastFrame = 0;
 	int glxattribs[] = { GLX_DOUBLEBUFFER, GLX_RGBA, GLX_RED_SIZE, 8,
@@ -2515,6 +2520,96 @@ int subWinTest(void)
 				else if(i % 3 == 1) { XDestroyWindow(dpy, win);  win = 0; }
 				else XDestroySubwindows(dpy, win);
 			}
+
+			if(win && dpy) { XDestroyWindow(dpy, win);  win = 0; }
+			if(dpy) { XCloseDisplay(dpy);  dpy = NULL; }
+
+			for(int i = 0; i < 20; i++)
+			{
+				xcb_void_cookie_t cookie = { 0 };
+
+				if(!dpy && !(dpy = XOpenDisplay(0)))
+					THROW("Could not open display");
+				if(!(conn = XGetXCBConnection(dpy)))
+					THROW("Could not get XCB connection");
+				XSetEventQueueOwner(dpy, XCBOwnsEventQueue);
+				dpyw = DisplayWidth(dpy, DefaultScreen(dpy));
+				dpyh = DisplayHeight(dpy, DefaultScreen(dpy));
+
+				if((vis = glXChooseVisual(dpy, DefaultScreen(dpy),
+					glxattribs)) == NULL)
+					THROW("Could not find a suitable visual");
+
+				Window root = RootWindow(dpy, DefaultScreen(dpy));
+
+				if(!win)
+				{
+					if((win = xcb_generate_id(conn)) == 0)
+						THROW("Could not create window");
+					cookie = xcb_create_window(conn, vis->depth, win, root, 0, 0,
+						dpyw / 2, dpyh / 2, 0, XCB_WINDOW_CLASS_INPUT_OUTPUT,
+						vis->visualid, 0, NULL);
+					if(!cookie.sequence)
+						THROW("Could not create window");
+				}
+
+				if((win1 = xcb_generate_id(conn)) == 0)
+					THROW("Could not create subwindow");
+				cookie.sequence = 0;
+				cookie = xcb_create_window_checked(conn, vis->depth, win1, win, 0, 0,
+					dpyw / 2, dpyh / 2, 0, XCB_WINDOW_CLASS_INPUT_OUTPUT, vis->visualid,
+					0, NULL);
+				if(!cookie.sequence)
+					THROW("Could not create subwindow");
+
+				if((win2 = xcb_generate_id(conn)) == 0)
+					THROW("Could not create subwindow");
+				cookie.sequence = 0;
+				if(i % 2 == 0)
+					cookie = xcb_create_window_aux(conn, vis->depth, win2, win1, 0, 0,
+						dpyw / 2, dpyh / 2, 0, XCB_WINDOW_CLASS_INPUT_OUTPUT,
+						vis->visualid, 0, NULL);
+				else
+					cookie = xcb_create_window_aux_checked(conn, vis->depth, win2, win1,
+						0, 0, dpyw / 2, dpyh / 2, 0, XCB_WINDOW_CLASS_INPUT_OUTPUT,
+						vis->visualid, 0, NULL);
+				if(!cookie.sequence)
+					THROW("Could not create subwindow");
+
+				xcb_map_subwindows(conn, win);
+				xcb_map_window(conn, win);
+
+				lastFrame = 0;
+				if(!(ctx = glXCreateContext(dpy, vis, NULL, True)))
+					THROW("Could not create context");
+				XFree(vis);  vis = NULL;
+				if(!(glXMakeCurrent(dpy, win2, ctx)))
+					THROWNL("Could not make context current");
+				clr.clear(GL_BACK);
+				glXSwapBuffers(dpy, win2);
+				checkFrame(dpy, win2, 1, lastFrame);
+				checkWindowColor(dpy, win2, clr.bits(-1), false);
+				glXMakeCurrent(dpy, 0, 0);
+				glXDestroyContext(dpy, ctx);  ctx = 0;
+
+				if(i % 3 == 0) { XCloseDisplay(dpy);  dpy = NULL;  win = 0; }
+				else if(i % 3 == 1)
+				{
+					if(i % 2 == 0)
+						xcb_destroy_window(conn, win);
+					else
+						xcb_destroy_window_checked(conn, win);
+					win = 0;
+				}
+				else
+				{
+					if(i % 2 == 0)
+						xcb_destroy_subwindows(conn, win);
+					else
+						xcb_destroy_subwindows_checked(conn, win);
+				}
+			}
+
 			printf("SUCCESS\n");
 		}
 		catch(Error &e)

server/CMakeLists.txt
@@ -225,8 +225,13 @@ if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
 endif()
 
 add_executable(fakerut fakerut.cpp fakerut-supplement.c)
+if(VGL_FAKEXCB)
+	string(REGEX REPLACE "libX11" "libxcb" XCB_XCB_LIB ${X11_X11_LIB})
+	string(REGEX REPLACE "libX11" "libX11-xcb" X11_XCB_LIB ${X11_X11_LIB})
+endif()
 target_link_libraries(fakerut "${MINUSZ}now ${OPENGL_gl_LIBRARY}"
-	${OPENGL_glu_LIBRARY} "${MINUSZ}now ${X11_X11_LIB}" ${LIBDL} vglutil)
+	${OPENGL_glu_LIBRARY} "${MINUSZ}now ${X11_X11_LIB}" ${LIBDL} vglutil
+	${XCB_XCB_LIB} ${X11_XCB_LIB})
 
 add_library(vgltrans_test SHARED testplugin.cpp VGLTrans.cpp)
 unset(VGLTRANS_TEST_LINK_FLAGS)

server/faker-mapfile.c
@@ -159,6 +159,14 @@
 
 		/* XCB */
 		#ifdef FAKEXCB
+		xcb_create_window;
+		xcb_create_window_aux;
+		xcb_create_window_aux_checked;
+		xcb_create_window_checked;
+		xcb_destroy_subwindows;
+		xcb_destroy_subwindows_checked;
+		xcb_destroy_window;
+		xcb_destroy_window_checked;
 		xcb_get_extension_data;
 		xcb_glx_query_version;
 		xcb_glx_query_version_reply;

server/faker-sym.h
@@ -1,6 +1,6 @@
 // Copyright (C)2004 Landmark Graphics Corporation
 // Copyright (C)2005, 2006 Sun Microsystems, Inc.
-// Copyright (C)2009, 2011, 2013-2016, 2018-2021 D. R. Commander
+// Copyright (C)2009, 2011, 2013-2016, 2018-2022 D. R. Commander
 //
 // This library is free software and may be redistributed and/or modified under
 // the terms of the wxWindows Library License, Version 3.1 or (at your option)
@@ -329,6 +329,23 @@ extern "C" {
 		CHECKSYM(f, fake_f); \
 		DISABLE_FAKER(); \
 		retval = __##f(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12); \
+		ENABLE_FAKER(); \
+		return retval; \
+	}
+
+#define FUNCDEF13(RetType, f, at1, a1, at2, a2, at3, a3, at4, a4, at5, a5, \
+	at6, a6, at7, a7, at8, a8, at9, a9, at10, a10, at11, a11, at12, a12, \
+	at13, a13, fake_f) \
+	typedef RetType (*_##f##Type)(at1, at2, at3, at4, at5, at6, at7, at8, at9, \
+		at10, at11, at12, at13); \
+	SYMDEF(f); \
+	static INLINE RetType _##f(at1 a1, at2 a2, at3 a3, at4 a4, at5 a5, at6 a6, \
+		at7 a7, at8 a8, at9 a9, at10 a10, at11 a11, at12 a12, at13 a13) \
+	{ \
+		RetType retval; \
+		CHECKSYM(f, fake_f); \
+		DISABLE_FAKER(); \
+		retval = __##f(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13); \
 		ENABLE_FAKER(); \
 		return retval; \
 	}
@@ -688,6 +705,46 @@ SYMDEF(dlopen);
 #ifdef FAKEXCB
 
 // XCB functions
+
+FUNCDEF13(xcb_void_cookie_t, xcb_create_window, xcb_connection_t *, conn,
+	uint8_t, depth, xcb_window_t, wid, xcb_window_t, parent, int16_t, x,
+	int16_t, y, uint16_t, width, uint16_t, height, uint16_t, border_width,
+	uint16_t, _class, xcb_visualid_t, visual, uint32_t, value_mask,
+	const void *, value_list, xcb_create_window)
+
+FUNCDEF13(xcb_void_cookie_t, xcb_create_window_aux, xcb_connection_t *, conn,
+	uint8_t, depth, xcb_window_t, wid, xcb_window_t, parent, int16_t, x,
+	int16_t, y, uint16_t, width, uint16_t, height, uint16_t, border_width,
+	uint16_t, _class, xcb_visualid_t, visual, uint32_t, value_mask,
+	const xcb_create_window_value_list_t *, value_list, xcb_create_window_aux)
+
+FUNCDEF13(xcb_void_cookie_t, xcb_create_window_aux_checked,
+	xcb_connection_t *, conn, uint8_t, depth, xcb_window_t, wid,
+	xcb_window_t, parent, int16_t, x, int16_t, y, uint16_t, width,
+	uint16_t, height, uint16_t, border_width, uint16_t, _class,
+	xcb_visualid_t, visual, uint32_t, value_mask,
+	const xcb_create_window_value_list_t *, value_list,
+	xcb_create_window_aux_checked)
+
+FUNCDEF13(xcb_void_cookie_t, xcb_create_window_checked,
+	xcb_connection_t *, conn, uint8_t, depth, xcb_window_t, wid,
+	xcb_window_t, parent, int16_t, x, int16_t, y, uint16_t, width,
+	uint16_t, height, uint16_t, border_width, uint16_t, _class,
+	xcb_visualid_t, visual, uint32_t, value_mask, const void *, value_list,
+	xcb_create_window_checked)
+
+FUNCDEF2(xcb_void_cookie_t, xcb_destroy_subwindows, xcb_connection_t *, conn,
+	xcb_window_t, window, xcb_destroy_subwindows)
+
+FUNCDEF2(xcb_void_cookie_t, xcb_destroy_subwindows_checked,
+	xcb_connection_t *, conn, xcb_window_t, window,
+	xcb_destroy_subwindows_checked)
+
+FUNCDEF2(xcb_void_cookie_t, xcb_destroy_window, xcb_connection_t *, conn,
+	xcb_window_t, window, xcb_destroy_window)
+
+FUNCDEF2(xcb_void_cookie_t, xcb_destroy_window_checked,
+	xcb_connection_t *, conn, xcb_window_t, window, xcb_destroy_window_checked)
 
 FUNCDEF2(const xcb_query_extension_reply_t *, xcb_get_extension_data,
 	xcb_connection_t *, conn, xcb_extension_t *, ext, xcb_get_extension_data)

server/faker-xcb.cpp
@@ -31,6 +31,281 @@ using namespace vglserver;
 extern "C" {
 
 
+xcb_void_cookie_t xcb_create_window(xcb_connection_t *conn, uint8_t depth,
+	xcb_window_t wid, xcb_window_t parent, int16_t x, int16_t y, uint16_t width,
+	uint16_t height, uint16_t border_width, uint16_t _class,
+	xcb_visualid_t visual, uint32_t value_mask, const void *value_list)
+{
+	xcb_void_cookie_t cookie = { 0 };
+
+	TRY();
+
+	if(!fconfig.fakeXCB || IS_EXCLUDED(xcbconnhash.getX11Display(conn)))
+		return _xcb_create_window(conn, depth, wid, parent, x, y, width, height,
+			border_width, _class, visual, value_mask, value_list);
+
+	/////////////////////////////////////////////////////////////////////////////
+	OPENTRACE(xcb_create_window);  PRARGX(conn);  PRARGI(depth);  PRARGX(wid);
+	PRARGX(parent);  PRARGI(x);  PRARGI(y);  PRARGI(width);  PRARGI(height);
+	PRARGI(border_width);  PRARGI(_class);  PRARGX(visual);  PRARGX(value_mask);
+	STARTTRACE();
+	/////////////////////////////////////////////////////////////////////////////
+
+	cookie = _xcb_create_window(conn, depth, wid, parent, x, y, width, height,
+		border_width, _class, visual, value_mask, value_list);
+	if(cookie.sequence) winhash.add(xcbconnhash.getX11Display(conn), wid);
+
+	/////////////////////////////////////////////////////////////////////////////
+	STOPTRACE();  CLOSETRACE();
+	/////////////////////////////////////////////////////////////////////////////
+
+	CATCH();
+
+	return cookie;
+}
+
+
+xcb_void_cookie_t xcb_create_window_aux(xcb_connection_t *conn, uint8_t depth,
+	xcb_window_t wid, xcb_window_t parent, int16_t x, int16_t y, uint16_t width,
+	uint16_t height, uint16_t border_width, uint16_t _class,
+	xcb_visualid_t visual, uint32_t value_mask,
+	const xcb_create_window_value_list_t *value_list)
+{
+	xcb_void_cookie_t cookie = { 0 };
+
+	TRY();
+
+	if(!fconfig.fakeXCB || IS_EXCLUDED(xcbconnhash.getX11Display(conn)))
+		return _xcb_create_window_aux(conn, depth, wid, parent, x, y, width,
+			height, border_width, _class, visual, value_mask, value_list);
+
+	/////////////////////////////////////////////////////////////////////////////
+	OPENTRACE(xcb_create_window_aux);  PRARGX(conn);  PRARGI(depth);
+	PRARGX(wid);  PRARGX(parent);  PRARGI(x);  PRARGI(y);  PRARGI(width);
+	PRARGI(height);  PRARGI(border_width);  PRARGI(_class);  PRARGX(visual);
+	PRARGX(value_mask);  STARTTRACE();
+	/////////////////////////////////////////////////////////////////////////////
+
+	cookie = _xcb_create_window_aux(conn, depth, wid, parent, x, y, width,
+		height, border_width, _class, visual, value_mask, value_list);
+	if(cookie.sequence) winhash.add(xcbconnhash.getX11Display(conn), wid);
+
+	/////////////////////////////////////////////////////////////////////////////
+	STOPTRACE();  CLOSETRACE();
+	/////////////////////////////////////////////////////////////////////////////
+
+	CATCH();
+
+	return cookie;
+}
+
+
+xcb_void_cookie_t xcb_create_window_aux_checked(xcb_connection_t *conn,
+	uint8_t depth, xcb_window_t wid, xcb_window_t parent, int16_t x, int16_t y,
+	uint16_t width, uint16_t height, uint16_t border_width, uint16_t _class,
+	xcb_visualid_t visual, uint32_t value_mask,
+	const xcb_create_window_value_list_t *value_list)
+{
+	xcb_void_cookie_t cookie = { 0 };
+
+	TRY();
+
+	if(!fconfig.fakeXCB || IS_EXCLUDED(xcbconnhash.getX11Display(conn)))
+		return _xcb_create_window_aux_checked(conn, depth, wid, parent, x, y,
+			width, height, border_width, _class, visual, value_mask, value_list);
+
+	/////////////////////////////////////////////////////////////////////////////
+	OPENTRACE(xcb_create_window_aux_checked);  PRARGX(conn);  PRARGI(depth);
+	PRARGX(wid);  PRARGX(parent);  PRARGI(x);  PRARGI(y);  PRARGI(width);
+	PRARGI(height);  PRARGI(border_width);  PRARGI(_class);  PRARGX(visual);
+	PRARGX(value_mask);  STARTTRACE();
+	/////////////////////////////////////////////////////////////////////////////
+
+	cookie = _xcb_create_window_aux_checked(conn, depth, wid, parent, x, y,
+		width, height, border_width, _class, visual, value_mask, value_list);
+	if(cookie.sequence) winhash.add(xcbconnhash.getX11Display(conn), wid);
+
+	/////////////////////////////////////////////////////////////////////////////
+	STOPTRACE();  CLOSETRACE();
+	/////////////////////////////////////////////////////////////////////////////
+
+	CATCH();
+
+	return cookie;
+}
+
+
+xcb_void_cookie_t xcb_create_window_checked(xcb_connection_t *conn,
+	uint8_t depth, xcb_window_t wid, xcb_window_t parent, int16_t x, int16_t y,
+	uint16_t width, uint16_t height, uint16_t border_width, uint16_t _class,
+	xcb_visualid_t visual, uint32_t value_mask, const void *value_list)
+{
+	xcb_void_cookie_t cookie = { 0 };
+
+	TRY();
+
+	if(!fconfig.fakeXCB || IS_EXCLUDED(xcbconnhash.getX11Display(conn)))
+		return _xcb_create_window_checked(conn, depth, wid, parent, x, y, width,
+			height, border_width, _class, visual, value_mask, value_list);
+
+	/////////////////////////////////////////////////////////////////////////////
+	OPENTRACE(xcb_create_window_checked);  PRARGX(conn);  PRARGI(depth);
+	PRARGX(wid);  PRARGX(parent);  PRARGI(x);  PRARGI(y);  PRARGI(width);
+	PRARGI(height);  PRARGI(border_width);  PRARGI(_class);  PRARGX(visual);
+	PRARGX(value_mask);  STARTTRACE();
+	/////////////////////////////////////////////////////////////////////////////
+
+	cookie = _xcb_create_window_checked(conn, depth, wid, parent, x, y, width,
+		height, border_width, _class, visual, value_mask, value_list);
+	if(cookie.sequence) winhash.add(xcbconnhash.getX11Display(conn), wid);
+
+	/////////////////////////////////////////////////////////////////////////////
+	STOPTRACE();  CLOSETRACE();
+	/////////////////////////////////////////////////////////////////////////////
+
+	CATCH();
+
+	return cookie;
+}
+
+
+// When a window is destroyed, we shut down the corresponding VirtualWin
+// instance, but we also have to walk the window tree to ensure that VirtualWin
+// instances attached to subwindows are also shut down.
+
+static void DeleteWindow(Display *dpy, Window win, bool subOnly = false)
+{
+	Window root, parent, *children = NULL;  unsigned int n = 0;
+
+	if(!subOnly) winhash.remove(dpy, win);
+	if(XQueryTree(dpy, win, &root, &parent, &children, &n)
+		&& children && n > 0)
+	{
+		for(unsigned int i = 0; i < n; i++) DeleteWindow(dpy, children[i]);
+		_XFree(children);
+	}
+}
+
+
+xcb_void_cookie_t xcb_destroy_subwindows(xcb_connection_t *conn,
+	xcb_window_t window)
+{
+	xcb_void_cookie_t cookie = { 0 };
+
+	TRY();
+
+	Display *dpy = xcbconnhash.getX11Display(conn);
+
+	if(!fconfig.fakeXCB || IS_EXCLUDED(dpy))
+		return _xcb_destroy_subwindows(conn, window);
+
+	/////////////////////////////////////////////////////////////////////////////
+	OPENTRACE(xcb_destroy_subwindows);  PRARGX(conn);  PRARGX(window);
+	STARTTRACE();
+	/////////////////////////////////////////////////////////////////////////////
+
+	if(dpy && window) DeleteWindow(dpy, window, true);
+	cookie = _xcb_destroy_subwindows(conn, window);
+
+	/////////////////////////////////////////////////////////////////////////////
+	STOPTRACE();  CLOSETRACE();
+	/////////////////////////////////////////////////////////////////////////////
+
+	CATCH();
+
+	return cookie;
+}
+
+
+xcb_void_cookie_t xcb_destroy_subwindows_checked(xcb_connection_t *conn,
+	xcb_window_t window)
+{
+	xcb_void_cookie_t cookie = { 0 };
+
+	TRY();
+
+	Display *dpy = xcbconnhash.getX11Display(conn);
+
+	if(!fconfig.fakeXCB || IS_EXCLUDED(dpy))
+		return _xcb_destroy_subwindows_checked(conn, window);
+
+	/////////////////////////////////////////////////////////////////////////////
+	OPENTRACE(xcb_destroy_subwindows_checked);  PRARGX(conn);  PRARGX(window);
+	STARTTRACE();
+	/////////////////////////////////////////////////////////////////////////////
+
+	if(dpy && window) DeleteWindow(dpy, window, true);
+	cookie = _xcb_destroy_subwindows_checked(conn, window);
+
+	/////////////////////////////////////////////////////////////////////////////
+	STOPTRACE();  CLOSETRACE();
+	/////////////////////////////////////////////////////////////////////////////
+
+	CATCH();
+
+	return cookie;
+}
+
+
+xcb_void_cookie_t xcb_destroy_window(xcb_connection_t *conn,
+	xcb_window_t window)
+{
+	xcb_void_cookie_t cookie = { 0 };
+
+	TRY();
+
+	Display *dpy = xcbconnhash.getX11Display(conn);
+
+	if(!fconfig.fakeXCB || IS_EXCLUDED(dpy))
+		return _xcb_destroy_window(conn, window);
+
+	/////////////////////////////////////////////////////////////////////////////
+	OPENTRACE(xcb_destroy_window);  PRARGX(conn);  PRARGX(window);  STARTTRACE();
+	/////////////////////////////////////////////////////////////////////////////
+
+	if(dpy && window) DeleteWindow(dpy, window);
+	cookie = _xcb_destroy_window(conn, window);
+
+	/////////////////////////////////////////////////////////////////////////////
+	STOPTRACE();  CLOSETRACE();
+	/////////////////////////////////////////////////////////////////////////////
+
+	CATCH();
+
+	return cookie;
+}
+
+
+xcb_void_cookie_t xcb_destroy_window_checked(xcb_connection_t *conn,
+	xcb_window_t window)
+{
+	xcb_void_cookie_t cookie = { 0 };
+
+	TRY();
+
+	Display *dpy = xcbconnhash.getX11Display(conn);
+
+	if(!fconfig.fakeXCB || IS_EXCLUDED(dpy))
+		return _xcb_destroy_window_checked(conn, window);
+
+	/////////////////////////////////////////////////////////////////////////////
+	OPENTRACE(xcb_destroy_window_checked);  PRARGX(conn);  PRARGX(window);
+	STARTTRACE();
+	/////////////////////////////////////////////////////////////////////////////
+
+	if(dpy && window) DeleteWindow(dpy, window);
+	cookie = _xcb_destroy_window_checked(conn, window);
+
+	/////////////////////////////////////////////////////////////////////////////
+	STOPTRACE();  CLOSETRACE();
+	/////////////////////////////////////////////////////////////////////////////
+
+	CATCH();
+
+	return cookie;
+}
+
+
 const xcb_query_extension_reply_t *
 	xcb_get_extension_data(xcb_connection_t *conn, xcb_extension_t *ext)
 {

commit f431e250bafedded606ea9c444449829cd09081b

fakerut.cpp: Fix build when VGL_FAKEXCB=0
(regression introduced by 736d95f76217ac791db0a585a13ecd648adcb39c)

server/fakerut.cpp
@@ -2446,7 +2446,9 @@ int copyContextTest(void)
 int subWinTest(void)
 {
 	Display *dpy = NULL;  Window win = 0, win1 = 0, win2 = 0;
+	#ifdef FAKEXCB
 	xcb_connection_t *conn = NULL;
+	#endif
 	TestColor clr(0);
 	int dpyw, dpyh, retval = 1, lastFrame = 0;
 	int glxattribs[] = { GLX_DOUBLEBUFFER, GLX_RGBA, GLX_RED_SIZE, 8,
@@ -2513,6 +2515,7 @@ int subWinTest(void)
 			if(win && dpy) { XDestroyWindow(dpy, win);  win = 0; }
 			if(dpy) { XCloseDisplay(dpy);  dpy = NULL; }
 
+			#ifdef FAKEXCB
 			for(int i = 0; i < 20; i++)
 			{
 				xcb_void_cookie_t cookie = { 0 };
@@ -2598,6 +2601,7 @@ int subWinTest(void)
 						xcb_destroy_subwindows_checked(conn, win);
 				}
 			}
+			#endif
 
 			printf("SUCCESS\n");
 		}