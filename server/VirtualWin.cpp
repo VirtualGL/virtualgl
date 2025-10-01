VirtualGL/virtualgl

commit 21cc496a678aed60e7a9886d08dfe5d23eb9ae37

Faker: EGL/X11 interposer
Things that aren't implemented (yet):
- An EGL/X11 equivalent of fakerut (not sure if it's really needed)
- Pixmap surfaces
- Front buffer/single buffer rendering with window surfaces (but this is
  only relevant with desktop OpenGL and is a legacy feature)

The relevant piglit tests all pass.

Closes #66

server/VirtualWin.cpp
@@ -80,8 +80,11 @@ VirtualWin::VirtualWin(Display *dpy_, Window win) :
 			vglout.println("[VGL] Selecting structure notify events in window 0x%.8x",
 				win);
 	}
-	stereoVisual = glxvisual::visAttrib(dpy, DefaultScreen(dpy),
-		xwa.visual->visualid, GLX_STEREO);
+	#ifdef EGLBACKEND
+	if(edpy != EGL_NO_DISPLAY)
+	#endif
+		stereoVisual = glxvisual::visAttrib(dpy, DefaultScreen(dpy),
+			xwa.visual->visualid, GLX_STEREO);
 }
 
 
@@ -169,6 +172,11 @@ void VirtualWin::cleanup(void)
 
 void VirtualWin::initFromWindow(VGLFBConfig config_)
 {
+	#ifdef EGLBACKEND
+	if(edpy != EGL_NO_DISPLAY)
+		THROW("VirtualWin::initFromWindow() method not supported with EGL/X11");
+	#endif
+
 	XSync(dpy, False);
 	XWindowAttributes xwa;
 	XGetWindowAttributes(dpy, x11Draw, &xwa);
@@ -208,6 +216,11 @@ void VirtualWin::checkResize(void)
 
 GLXDrawable VirtualWin::updateGLXDrawable(void)
 {
+	#ifdef EGLBACKEND
+	if(edpy != EGL_NO_DISPLAY)
+		THROW("VirtualWin::updateGLXDrawable() method not supported with EGL/X11");
+	#endif
+
 	GLXDrawable retval = 0;
 	CriticalSection::SafeLock l(mutex);
 	if(deletedByWM) THROW("Window has been deleted by window manager");
@@ -230,6 +243,11 @@ GLXDrawable VirtualWin::updateGLXDrawable(void)
 
 void VirtualWin::swapBuffers(void)
 {
+	#ifdef EGLBACKEND
+	if(edpy != EGL_NO_DISPLAY)
+		THROW("VirtualWin::swapBuffers() method not supported with EGL/X11");
+	#endif
+
 	CriticalSection::SafeLock l(mutex);
 	if(deletedByWM) THROW("Window has been deleted by window manager");
 	if(oglDraw)
@@ -369,7 +387,12 @@ TempContext *VirtualWin::setupPluginTempContext(GLint drawBuf)
 	else
 	{
 		initReadbackContext();
+		#ifdef EGLBACKEND
+		tc = new TempContext(edpy != EGL_NO_DISPLAY ? (Display *)edpy : dpy,
+			getGLXDrawable(), getGLXDrawable(), ctx, edpy != EGL_NO_DISPLAY);
+		#else
 		tc = new TempContext(dpy, getGLXDrawable(), getGLXDrawable(), ctx);
+		#endif
 		backend::readBuffer(drawBuf);
 	}
 

common/rr.h
@@ -161,6 +161,7 @@ typedef struct _FakerConfig
   char egl;
   #ifdef EGLBACKEND
   char egllib[MAXSTR];
+  char eglxIgnorePixmapBit;
   #endif
   double flushdelay;
   int forcealpha;

demos/CMakeLists.txt
@@ -23,6 +23,12 @@ if(VGL_EGLBACKEND)
 	add_executable(eglinfo eglinfo.c glinfo_common.c)
 	target_link_libraries(eglinfo ${OPENGL_gl_LIBRARY} ${OPENGL_egl_LIBRARY})
 	install(TARGETS eglinfo DESTINATION ${CMAKE_INSTALL_BINDIR})
+
+	add_executable(eglxinfo eglinfo.c glinfo_common.c)
+	target_compile_definitions(eglxinfo PUBLIC -DEGLX)
+	target_link_libraries(eglxinfo ${OPENGL_gl_LIBRARY} ${OPENGL_egl_LIBRARY}
+		${X11_X11_LIB})
+	install(TARGETS eglxinfo DESTINATION ${CMAKE_INSTALL_BINDIR})
 endif()
 
 set(GLXSPHERES glxspheres)
@@ -33,6 +39,17 @@ add_executable(${GLXSPHERES} glxspheres.c)
 target_link_libraries(${GLXSPHERES} ${OPENGL_gl_LIBRARY} ${X11_X11_LIB}
 	${OPENGL_glu_LIBRARY} m)
 install(TARGETS ${GLXSPHERES} DESTINATION ${CMAKE_INSTALL_BINDIR})
+
+if(VGL_EGLBACKEND)
+	set(EGLXSPHERES eglxspheres)
+	if(BITS EQUAL 64)
+		set(EGLXSPHERES eglxspheres64)
+	endif()
+	add_executable(${EGLXSPHERES} eglxspheres.c xfonts.c)
+	target_link_libraries(${EGLXSPHERES} ${OPENGL_gl_LIBRARY} ${OPENGL_egl_LIBRARY}
+		${X11_X11_LIB} ${OPENGL_glu_LIBRARY} m)
+	install(TARGETS ${EGLXSPHERES} DESTINATION ${CMAKE_INSTALL_BINDIR})
+endif()
 
 foreach(program pbdemo)
 	add_executable(${program} ${program}.c pbutil.c)

demos/eglinfo.c
@@ -28,7 +28,11 @@
 #include <assert.h>
 #include <GL/gl.h>
 #include <EGL/egl.h>
+#ifdef EGLX
+#include <GL/glx.h>
+#else
 #include <EGL/eglext.h>
+#endif
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -48,6 +52,11 @@ struct config_attribs
 {
    /* EGL config attribs */
    int id;
+#ifdef EGLX
+   int nativeRenderable;
+   int vis_id;
+   int klass;
+#endif
    int surfaceType;
    int transparentType;
    int transparentRedValue;
@@ -315,7 +324,11 @@ print_display_info(EGLDisplay edpy,
       CheckError(__LINE__);
 
       if (!coreWorked) {
+#ifdef EGLX
+         printf("display: %s\n", opts->displayName);
+#else
          printf("device: %s\n", opts->displayName);
+#endif
          if (opts->mode != Brief) {
             printf("EGL client APIs string: %s\n", eglAPIs);
             printf("EGL vendor string: %s\n", eglVendor);
@@ -367,6 +380,74 @@ print_display_info(EGLDisplay edpy,
 }
 
 
+#ifdef EGLX
+
+static const char *
+visual_class_name(int cls)
+{
+   switch (cls) {
+      case StaticColor:
+         return "StaticColor";
+      case PseudoColor:
+         return "PseudoColor";
+      case StaticGray:
+         return "StaticGray";
+      case GrayScale:
+         return "GrayScale";
+      case TrueColor:
+         return "TrueColor";
+      case DirectColor:
+         return "DirectColor";
+      default:
+         return "Unknown";
+   }
+}
+
+static const char *
+visual_class_abbrev(int cls)
+{
+   switch (cls) {
+      case StaticColor:
+         return "sc";
+      case PseudoColor:
+         return "pc";
+      case StaticGray:
+         return "sg";
+      case GrayScale:
+         return "gs";
+      case TrueColor:
+         return "tc";
+      case DirectColor:
+         return "dc";
+      default:
+         return "??";
+   }
+}
+
+static int
+glx_token_to_visual_class(int visual_type)
+{
+   switch (visual_type) {
+   case GLX_TRUE_COLOR:
+      return TrueColor;
+   case GLX_DIRECT_COLOR:
+      return DirectColor;
+   case GLX_PSEUDO_COLOR:
+      return PseudoColor;
+   case GLX_STATIC_COLOR:
+      return StaticColor;
+   case GLX_GRAY_SCALE:
+      return GrayScale;
+   case GLX_STATIC_GRAY:
+      return StaticGray;
+   case GLX_NONE:
+   default:
+      return -1;
+   }
+}
+
+#endif
+
 static const char *
 config_surface_type(int type)
 {
@@ -411,10 +492,24 @@ get_config_attribs(EGLDisplay edpy, EGLConfig config,
                    struct config_attribs *attribs)
 {
    const char *ext = eglQueryString(edpy, EGL_EXTENSIONS);
+#ifdef EGLX
+   int visual_type;
+#endif
 
    memset(attribs, 0, sizeof(struct config_attribs));
 
    eglGetConfigAttrib(edpy, config, EGL_CONFIG_ID, &attribs->id);
+#ifdef EGLX
+   eglGetConfigAttrib(edpy, config, EGL_NATIVE_RENDERABLE,
+                      &attribs->nativeRenderable);
+   eglGetConfigAttrib(edpy, config, EGL_NATIVE_VISUAL_ID, &attribs->vis_id);
+   eglGetConfigAttrib(edpy, config, EGL_NATIVE_VISUAL_TYPE, &visual_type);
+   /* Some EGL implementations use the GLX_X_VISUAL_TYPE tokens. */
+   if (visual_type & 0x8000)
+     attribs->klass = glx_token_to_visual_class(visual_type);
+   else
+     attribs->klass = visual_type;
+#endif
 
    eglGetConfigAttrib(edpy, config, EGL_SURFACE_TYPE, &attribs->surfaceType);
    eglGetConfigAttrib(edpy, config, EGL_LEVEL, &attribs->level);
@@ -470,7 +565,13 @@ get_config_attribs(EGLDisplay edpy, EGLConfig config,
 static void
 print_config_attribs_verbose(const struct config_attribs *attribs)
 {
-   printf("Config ID: %x\n", attribs->id);
+#ifdef EGLX
+   if (attribs->nativeRenderable)
+      printf("Config ID: %x  Visual ID=%x  class=%s\n", attribs->id,
+             attribs->vis_id, visual_class_name(attribs->klass));
+   else
+#endif
+      printf("Config ID: %x\n", attribs->id);
    printf("    Surface Types=%s\n",
           config_surface_type(attribs->surfaceType));
    printf("    bufferSize=%d level=%d colorBufferType=%s\n",
@@ -512,9 +613,15 @@ print_config_attribs_verbose(const struct config_attribs *attribs)
 static void
 print_config_attribs_short_header(void)
 {
+#ifdef EGLX
+ printf("Cfg    Visual  tra buf lev buf colorbuffer   dep ste client APIs   ms  cav  surf\n");
+ printf("ID    ID    cl ns  sz  el  typ r  g  b  a  F th  ncl GL ES ES2 VG ns b eat  typ\n");
+ printf("--------------------------------------------------------------------------------\n");
+#else
  printf("Cfg   tra buf lev buf colorbuffer   dep ste client APIs   ms  cav  surf\n");
  printf("ID    ns  sz  el  typ r  g  b  a  F th  ncl GL ES ES2 VG ns b eat  typ\n");
  printf("-----------------------------------------------------------------------\n");
+#endif
 }
 
 
@@ -523,8 +630,17 @@ print_config_attribs_short(const struct config_attribs *attribs)
 {
    const char *caveat = caveat_string(attribs->configCaveat);
 
+#ifdef EGLX
+   printf("0x%03x 0x%03x %2s %c   %-3d %-3d %3s %-2d %-2d %-2d %-2d %c %-3d %-3d",
+#else
    printf("0x%03x %c   %-3d %-3d %3s %-2d %-2d %-2d %-2d %c %-3d %-3d",
+#endif
           attribs->id,
+#ifdef EGLX
+          attribs->vis_id,
+          attribs->nativeRenderable ?
+             visual_class_abbrev(attribs->klass) : "??",
+#endif
           attribs->transparentType != EGL_NONE ? 'y' : '.',
           attribs->bufferSize,
           attribs->level,
@@ -609,9 +725,14 @@ print_config_info(EGLDisplay edpy, InfoMode mode)
 static void
 usage(void)
 {
+#ifdef EGLX
+   printf("Usage: eglinfo [-display <display>] [-v] [-h] [-l] [-s]\n");
+   printf("\t-display <display>: Print EGL configs for the specified X server.\n");
+#else
    printf("Usage: eglinfo <device> [-v] [-h] [-l] [-s]\n");
    printf("\t<device>: Print EGL configs for the specified DRI device.\n");
    printf("\t          (\"egl\" = print EGL configs for the first DRI device)\n");
+#endif
    printf("\t-B: brief output, print only the basics.\n");
    printf("\t-v: Print config info in verbose form.\n");
    printf("\t-h: This information.\n");
@@ -623,20 +744,27 @@ usage(void)
 int
 main(int argc, char *argv[])
 {
+   int major, minor, i;
+#ifdef EGLX
+   Display *dpy;
+#else
    const char *exts;
-   int numDevices, major, minor, i;
+   int numDevices;
    EGLDeviceEXT *devices = NULL;
-   EGLDisplay edpy;
-   struct options opts;
-   GLboolean coreWorked;
    PFNEGLQUERYDEVICESEXTPROC _eglQueryDevicesEXT;
    PFNEGLQUERYDEVICESTRINGEXTPROC _eglQueryDeviceStringEXT;
    PFNEGLGETPLATFORMDISPLAYEXTPROC _eglGetPlatformDisplayEXT;
+#endif
+   EGLDisplay edpy;
+   struct options opts;
+   GLboolean coreWorked;
 
+#ifndef EGLX
    if (argc < 2) {
       usage();
       exit(0);
    }
+#endif
 
    opts.mode = Normal;
    opts.limits = GL_FALSE;
@@ -660,10 +788,32 @@ main(int argc, char *argv[])
       else if (strcmp(argv[i], "-s") == 0) {
          opts.singleLine = GL_TRUE;
       }
+#ifdef EGLX
+      else if (strcmp(argv[i], "-display") == 0 && i + 1 < argc) {
+         opts.displayName = argv[i + 1];
+         i++;
+      }
+#else
       else {
          opts.displayName = argv[i];
       }
+#endif
    }
+
+#ifdef EGLX
+   dpy = XOpenDisplay(opts.displayName);
+   if (!dpy) {
+      fprintf(stderr, "Error: unable to open display %s\n",
+              XDisplayName(opts.displayName));
+      return -1;
+   }
+   edpy = eglGetDisplay(dpy);
+   if (!edpy) {
+      fprintf(stderr, "Error: unable to open EGL display\n");
+      return -1;
+   }
+   opts.displayName = DisplayString(dpy);
+#else
    if (!opts.displayName) {
       usage();
       exit(0);
@@ -737,6 +887,7 @@ main(int argc, char *argv[])
       return -1;
    }
    free(devices);
+#endif
    if (!eglInitialize(edpy, &major, &minor)) {
       fprintf(stderr, "Error: could not initialize EGL\n");
       return -1;

demos/xfonts.c
@@ -1,0 +1,285 @@
+/*
+ * Mesa 3-D graphics library
+ *
+ * Copyright (C) 1999-2000  Brian Paul   All Rights Reserved.
+ * Copyright (C) 2014, 2018, 2021  D. R. Commander   All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+
+/* xfonts.c -- glXUseXFont() for Mesa written by
+ * Copyright (C) 1995 Thorsten.Ohl @ Physik.TH-Darmstadt.de
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <X11/Xutil.h>
+#include <GL/gl.h>
+
+
+int
+Fake_glXUseXFont(Display * dpy, Window win, Font font, int first, int count,
+                 int listbase);
+
+
+#define THROW(m) { \
+   fprintf(stderr, "ERROR in line %d:\n%s\n", __LINE__,  m); \
+   retval = -1; \
+   goto bailout; \
+}
+
+
+/* Implementation.  */
+
+/* Fill a BITMAP with a character C from thew current font
+   in the graphics context GC.  WIDTH is the width in bytes
+   and HEIGHT is the height in bits.
+
+   Note that the generated bitmaps must be used with
+
+        glPixelStorei (GL_UNPACK_SWAP_BYTES, GL_FALSE);
+        glPixelStorei (GL_UNPACK_LSB_FIRST, GL_FALSE);
+        glPixelStorei (GL_UNPACK_ROW_LENGTH, 0);
+        glPixelStorei (GL_UNPACK_SKIP_ROWS, 0);
+        glPixelStorei (GL_UNPACK_SKIP_PIXELS, 0);
+        glPixelStorei (GL_UNPACK_ALIGNMENT, 1);
+
+   Possible optimizations:
+
+     * use only one reusable pixmap with the maximum dimensions.
+     * draw the entire font into a single pixmap (careful with
+       proportional fonts!).
+*/
+
+
+/*
+ * Generate OpenGL-compatible bitmap.
+ */
+static void
+fill_bitmap(Display * dpy, Window win, GC gc,
+            unsigned int width, unsigned int height,
+            int x0, int y0, unsigned int c, GLubyte * bitmap)
+{
+   XImage *image;
+   unsigned int x, y;
+   Pixmap pixmap;
+   XChar2b char2b;
+
+   pixmap = XCreatePixmap(dpy, win, 8 * width, height, 1);
+   XSetForeground(dpy, gc, 0);
+   XFillRectangle(dpy, pixmap, gc, 0, 0, 8 * width, height);
+   XSetForeground(dpy, gc, 1);
+
+   char2b.byte1 = (c >> 8) & 0xff;
+   char2b.byte2 = (c & 0xff);
+
+   XDrawString16(dpy, pixmap, gc, x0, y0, &char2b, 1);
+
+   image = XGetImage(dpy, pixmap, 0, 0, 8 * width, height, 1, XYPixmap);
+   if (image) {
+      /* Fill the bitmap (X11 and OpenGL are upside down wrt each other).  */
+      for (y = 0; y < height; y++)
+         for (x = 0; x < 8 * width; x++)
+            if (XGetPixel(image, x, y))
+               bitmap[width * (height - y - 1) + x / 8] |=
+                  (1 << (7 - (x % 8)));
+      XDestroyImage(image);
+   }
+
+   XFreePixmap(dpy, pixmap);
+}
+
+/*
+ * determine if a given glyph is valid and return the
+ * corresponding XCharStruct.
+ */
+static XCharStruct *
+isvalid(XFontStruct * fs, unsigned int which)
+{
+   unsigned int rows, pages;
+   unsigned int byte1 = 0, byte2 = 0;
+   int i, valid = 1;
+
+   rows = fs->max_byte1 - fs->min_byte1 + 1;
+   pages = fs->max_char_or_byte2 - fs->min_char_or_byte2 + 1;
+
+   if (rows == 1) {
+      /* "linear" fonts */
+      if ((fs->min_char_or_byte2 > which) || (fs->max_char_or_byte2 < which))
+         valid = 0;
+   }
+   else {
+      /* "matrix" fonts */
+      byte2 = which & 0xff;
+      byte1 = which >> 8;
+      if ((fs->min_char_or_byte2 > byte2) ||
+          (fs->max_char_or_byte2 < byte2) ||
+          (fs->min_byte1 > byte1) || (fs->max_byte1 < byte1))
+         valid = 0;
+   }
+
+   if (valid) {
+      if (fs->per_char) {
+         if (rows == 1) {
+            /* "linear" fonts */
+            return (fs->per_char + (which - fs->min_char_or_byte2));
+         }
+         else {
+            /* "matrix" fonts */
+            i = ((byte1 - fs->min_byte1) * pages) +
+               (byte2 - fs->min_char_or_byte2);
+            return (fs->per_char + i);
+         }
+      }
+      else {
+         return (&fs->min_bounds);
+      }
+   }
+   return (NULL);
+}
+
+
+int
+Fake_glXUseXFont(Display * dpy, Window win, Font font, int first, int count,
+                 int listbase)
+{
+   Pixmap pixmap;
+   GC gc = 0;
+   XGCValues values;
+   unsigned long valuemask;
+   XFontStruct *fs = NULL;
+   GLint swapbytes, lsbfirst, rowlength;
+   GLint skiprows, skippixels, alignment;
+   unsigned int max_width, max_height, max_bm_width, max_bm_height;
+   GLubyte *bm = NULL;
+   int i, retval = 0;
+   GLboolean packingModesSet = GL_FALSE;
+
+   fs = XQueryFont(dpy, font);
+   if (!fs) THROW("Couldn't get font structure information");
+
+   /* Allocate a bitmap that can fit all characters.  */
+   max_width = fs->max_bounds.rbearing - fs->min_bounds.lbearing;
+   max_height = fs->max_bounds.ascent + fs->max_bounds.descent;
+   max_bm_width = (max_width + 7) / 8;
+   max_bm_height = max_height;
+
+   bm = malloc((max_bm_width * max_bm_height) * sizeof(GLubyte));
+   if (!bm) THROW("Couldn't allocate bitmap in glXUseXFont()");
+
+   /* Save the current packing mode for bitmaps.  */
+   glGetIntegerv(GL_UNPACK_SWAP_BYTES, &swapbytes);
+   glGetIntegerv(GL_UNPACK_LSB_FIRST, &lsbfirst);
+   glGetIntegerv(GL_UNPACK_ROW_LENGTH, &rowlength);
+   glGetIntegerv(GL_UNPACK_SKIP_ROWS, &skiprows);
+   glGetIntegerv(GL_UNPACK_SKIP_PIXELS, &skippixels);
+   glGetIntegerv(GL_UNPACK_ALIGNMENT, &alignment);
+
+   /* Enforce a standard packing mode which is compatible with
+      fill_bitmap() from above.  This is actually the default mode,
+      except for the (non)alignment.  */
+   glPixelStorei(GL_UNPACK_SWAP_BYTES, GL_FALSE);
+   glPixelStorei(GL_UNPACK_LSB_FIRST, GL_FALSE);
+   glPixelStorei(GL_UNPACK_ROW_LENGTH, 0);
+   glPixelStorei(GL_UNPACK_SKIP_ROWS, 0);
+   glPixelStorei(GL_UNPACK_SKIP_PIXELS, 0);
+   glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+   packingModesSet = GL_TRUE;
+
+   pixmap = XCreatePixmap(dpy, win, 10, 10, 1);
+   values.foreground = BlackPixel(dpy, DefaultScreen(dpy));
+   values.background = WhitePixel(dpy, DefaultScreen(dpy));
+   values.font = fs->fid;
+   valuemask = GCForeground | GCBackground | GCFont;
+   gc = XCreateGC(dpy, pixmap, valuemask, &values);
+   XFreePixmap(dpy, pixmap);
+
+   for (i = 0; i < count; i++) {
+      unsigned int width, height, bm_width, bm_height;
+      GLfloat x0, y0, dx, dy;
+      XCharStruct *ch;
+      int x, y;
+      unsigned int c = first + i;
+      int list = listbase + i;
+      int valid;
+
+      /* check on index validity and get the bounds */
+      ch = isvalid(fs, c);
+      if (!ch) {
+         ch = &fs->max_bounds;
+         valid = 0;
+      }
+      else {
+         valid = 1;
+      }
+
+      /* glBitmap()' parameters:
+         straight from the glXUseXFont(3) manpage.  */
+      width = ch->rbearing - ch->lbearing;
+      height = ch->ascent + ch->descent;
+      x0 = -ch->lbearing;
+      y0 = ch->descent - 0;     /* XXX used to subtract 1 here */
+      /* but that caused a conformace failure */
+      dx = ch->width;
+      dy = 0;
+
+      /* X11's starting point.  */
+      x = -ch->lbearing;
+      y = ch->ascent;
+
+      /* Round the width to a multiple of eight.  We will use this also
+         for the pixmap for capturing the X11 font.  This is slightly
+         inefficient, but it makes the OpenGL part real easy.  */
+      bm_width = (width + 7) / 8;
+      bm_height = height;
+
+      glNewList(list, GL_COMPILE);
+      if (valid && (bm_width > 0) && (bm_height > 0)) {
+
+         memset(bm, '\0', bm_width * bm_height);
+         fill_bitmap(dpy, win, gc, bm_width, bm_height, x, y, c, bm);
+
+         glBitmap(width, height, x0, y0, dx, dy, bm);
+      }
+      else {
+         glBitmap(0, 0, 0.0, 0.0, dx, dy, NULL);
+      }
+      glEndList();
+   }
+
+   bailout:
+
+   free(bm);
+   if (fs) XFreeFontInfo(NULL, fs, 1);
+   if (gc) XFreeGC(dpy, gc);
+
+   /* Restore saved packing modes.  */
+   if (packingModesSet) {
+      glPixelStorei(GL_UNPACK_SWAP_BYTES, swapbytes);
+      glPixelStorei(GL_UNPACK_LSB_FIRST, lsbfirst);
+      glPixelStorei(GL_UNPACK_ROW_LENGTH, rowlength);
+      glPixelStorei(GL_UNPACK_SKIP_ROWS, skiprows);
+      glPixelStorei(GL_UNPACK_SKIP_PIXELS, skippixels);
+      glPixelStorei(GL_UNPACK_ALIGNMENT, alignment);
+   }
+
+   return retval;
+}

doc/index.html
@@ -3,7 +3,7 @@
 <head>
 <meta http-equiv="content-type" content="text/html; charset=ISO-8859-1">
 <meta name="language" content="en">
-<meta name="date" content="2021-11-22T14:39:09">
+<meta name="date" content="2021-11-22T17:27:53">
 <meta name="generator" content="deplate.rb 0.8.5">
 <title>User&rsquo;s Guide for VirtualGL 3.1</title>
 <link rel="start" href="index.html" title="Frontpage">
@@ -3154,13 +3154,20 @@ Image transport plugins are free to handle or ignore any configuration option as
     <dt class="Description-1 Description">Description</dt>
     <dd class="Description-1 Description">
         Normally VirtualGL will call the <code>eglGetProcAddress()</code> 
-        function in the EGL library against which it was linked (usually 
-        <strong class="filename">libEGL.so.1</strong>, in the system library 
-        path), and VGL will use that function to load any other EGL functions 
-        that it needs to call.  You can use the <code>VGL_EGLLIB</code> 
-        environment variable to specify the path of a dynamic library from which 
-        VirtualGL should load EGL functions. <br /><br /> You shouldn&rsquo;t 
-        need to change this unless something doesn&rsquo;t work.
+        function in the EGL library against which it or the 3D application was 
+        linked (usually <strong class="filename">libEGL.so.1</strong>, in the 
+        system library path), and VGL will use that function to load any other 
+        &ldquo;real&rdquo; EGL functions that it needs to call 
+        (&ldquo;real&rdquo; as opposed to the &ldquo;fake&rdquo;, or 
+        &ldquo;interposed&rdquo;, versions of those functions that VirtualGL 
+        provides, which often modify the arguments or perform other operations 
+        before calling the &ldquo;real&rdquo; functions.)  You can use the 
+        <code>VGL_EGLLIB</code> environment variable to specify the path of a 
+        dynamic library from which VirtualGL should load &ldquo;real&rdquo; EGL 
+        functions. <br /><br /> You shouldn&rsquo;t need to change this unless 
+        something doesn&rsquo;t work.  However, setting this environment 
+        variable is potentially useful if one wishes to insert another EGL 
+        interposer between VirtualGL and the system&rsquo;s EGL library.
     </dd>
 </dl>
 

release/rpm.spec.in
@@ -208,6 +208,7 @@ rm %{_tmppath}/%{name}-%{version}-%{release}-find-requires
 %{bindir}/cpustat
 %if %{eglbackend}
 	%{bindir}/eglinfo
+	%{bindir}/eglxinfo
 %endif
 %{bindir}/glxinfo
 %{bindir}/vglclient
@@ -221,6 +222,13 @@ rm %{_tmppath}/%{name}-%{version}-%{release}-find-requires
 	%{bindir}/glxspheres64
 %else
 	%{bindir}/glxspheres
+%endif
+%if %{eglbackend}
+	%if "%{_bits}" == "64"
+		%{bindir}/eglspheres64
+	%else
+		%{bindir}/eglspheres
+	%endif
 %endif
 %if "%{bindir}" != "%{_bindir}" && %{binsymlinks}
 	%{_bindir}/vglclient

commit 37e4efd4dd1af2d0c1291a630eb728a81c06b5c0

Faker: Fix quad-buffered stereo detection
(regression introduced by 21cc496a678aed60e7a9886d08dfe5d23eb9ae37)

Logic is a little tweeting bird, chirping in a meadow.
Logic is a wreath of pretty flowers that smell bad.

server/VirtualWin.cpp
@@ -1,6 +1,6 @@
 // Copyright (C)2004 Landmark Graphics Corporation
 // Copyright (C)2005, 2006 Sun Microsystems, Inc.
-// Copyright (C)2009-2015, 2017-2021 D. R. Commander
+// Copyright (C)2009-2015, 2017-2021, 2025 D. R. Commander
 //
 // This library is free software and may be redistributed and/or modified under
 // the terms of the wxWindows Library License, Version 3.1 or (at your option)
@@ -81,7 +81,7 @@ VirtualWin::VirtualWin(Display *dpy_, Window win) :
 				win);
 	}
 	stereoVisual = false;
-	if(edpy != EGL_NO_DISPLAY)
+	if(edpy == EGL_NO_DISPLAY)
 		stereoVisual = glxvisual::visAttrib(dpy, DefaultScreen(dpy),
 			xwa.visual->visualid, GLX_STEREO);
 }